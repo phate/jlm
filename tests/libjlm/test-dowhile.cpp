800e3cfee03223c8f205da4f8140cf566fa10f6c
Nico Reißmann <nico.reissmann@gmail.com>
2015-03-19 10:41:29 +0100

    tests: activate test-dowhile again

--- tests/libjlm/test-dowhile.cpp
+++ tests/libjlm/test-dowhile.cpp
@@ -1,16 +1,31 @@
 /*
- * Copyright 2014 Nico Reißmann <nico.reissmann@gmail.com>
+ * Copyright 2014 2015 Nico Reißmann <nico.reissmann@gmail.com>
  * See COPYING for terms of redistribution.
  */
 
 #include "test-registry.hpp"
+
+#include <jive/evaluator/eval.h>
+#include <jive/evaluator/literal.h>
 
 #include <assert.h>
 
 static int
 verify(const jive_graph * graph)
 {
-	/*FIXME*/
+	using namespace jive::evaluator;
+
+	memliteral state;
+	bitliteral xl(jive::bits::value_repr(32, 1));
+	bitliteral yl(jive::bits::value_repr(32, 2));
+
+	std::unique_ptr<const literal> result;
+	result = std::move(eval(graph, "test_dowhile", {&xl, &yl, &state})->copy());
+
+	const fctliteral * fctlit = dynamic_cast<const fctliteral*>(result.get());
+	assert(fctlit->nresults() == 2);
+	assert(dynamic_cast<const bitliteral*>(&fctlit->result(0))->value_repr() == 1);
+
 	return 0;
 }
 
