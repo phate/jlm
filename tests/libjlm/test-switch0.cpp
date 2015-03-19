57b18b4509be5ad3b81f71bed290021a8efe6e97
Nico Reißmann <nico.reissmann@gmail.com>
2015-03-19 15:41:11 +0100

    tests: activate test-switch0 again

--- tests/libjlm/test-switch0.cpp
+++ tests/libjlm/test-switch0.cpp
@@ -5,12 +5,45 @@
 
 #include "test-registry.hpp"
 
+#include <jive/evaluator/eval.h>
+#include <jive/evaluator/literal.h>
+
 #include <assert.h>
 
 static int
 verify(const jive_graph * graph)
 {
-	/*FIXME*/
+	using namespace jive::evaluator;
+
+	/* case 7 */
+	memliteral state;
+	bitliteral xl(jive::bits::value_repr(32, 7));
+
+	std::unique_ptr<const literal> result;
+	result = std::move(eval(graph, "test_switch0", {&xl, &state})->copy());
+
+	const fctliteral * fctlit = dynamic_cast<const fctliteral*>(result.get());
+	assert(fctlit->nresults() == 2);
+	assert(dynamic_cast<const bitliteral*>(&fctlit->result(0))->value_repr() == 5);
+
+
+	/* case 8 */
+	bitliteral yl(jive::bits::value_repr(32, 8));
+	result = std::move(eval(graph, "test_switch0", {&yl, &state})->copy());
+
+	fctlit = dynamic_cast<const fctliteral*>(result.get());
+	assert(fctlit->nresults() == 2);
+	assert(dynamic_cast<const bitliteral*>(&fctlit->result(0))->value_repr() == 6);
+
+
+	/* default case */
+	bitliteral zl(jive::bits::value_repr(32, 0));
+	result = std::move(eval(graph, "test_switch0", {&zl, &state})->copy());
+
+	fctlit = dynamic_cast<const fctliteral*>(result.get());
+	assert(fctlit->nresults() == 2);
+	assert(dynamic_cast<const bitliteral*>(&fctlit->result(0))->value_repr() == 11);
+
 	return 0;
 }
 
--- tests/libjlm/test-switch0.ll
+++ tests/libjlm/test-switch0.ll
@@ -1,16 +1,13 @@
-; ModuleID = 'test-switch0'
+; ModuleID = 'main.c'
 target datalayout = "e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v64:64:64-v128:128:128-a0:0:64-s0:64:64-f80:128:128-n8:16:32:64-S128"
 target triple = "x86_64-unknown-linux-gnu"
 
-define i32 @switch0(i32 %a) nounwind uwtable {
+define i32 @test_switch0(i32 %n) nounwind uwtable readnone {
 entry:
-  switch i32 %a, label %sw.default [
-    i32 0, label %sw.bb
-    i32 1, label %sw.bb1
+  switch i32 %n, label %sw.default [
+    i32 7, label %sw.epilog
+    i32 8, label %sw.bb1
   ]
-
-sw.bb:                                            ; preds = %entry
-  br label %sw.epilog
 
 sw.bb1:                                           ; preds = %entry
   br label %sw.epilog
@@ -18,6 +15,7 @@ sw.bb1:                                           ; preds = %entry
 sw.default:                                       ; preds = %entry
   br label %sw.epilog
 
-sw.epilog:                                        ; preds = %sw.default, %sw.bb1, %sw.bb
-  ret i32 0
+sw.epilog:                                        ; preds = %entry, %sw.default, %sw.bb1
+  %x.0 = phi i32 [ 11, %sw.default ], [ 6, %sw.bb1 ], [ 5, %entry ]
+  ret i32 %x.0
 }
