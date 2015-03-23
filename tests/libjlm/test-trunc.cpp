eb5858be8f785a7a3b15e110e721362aab0595a4
Nico Reißmann <nico.reissmann@gmail.com>
2015-03-23 18:49:14 +0100

    tests: activate test-trunc again

--- tests/libjlm/test-trunc.cpp
+++ tests/libjlm/test-trunc.cpp
@@ -1,19 +1,30 @@
 /*
- * Copyright 2014 Nico Reißmann <nico.reissmann@gmail.com>
+ * Copyright 2014 2015 Nico Reißmann <nico.reissmann@gmail.com>
  * See COPYING for terms of redistribution.
  */
 
 #include "test-registry.hpp"
 
-#include <jive/types/bitstring/slice.h>
-#include <jive/types/bitstring/type.h>
+#include <jive/evaluator/eval.h>
+#include <jive/evaluator/literal.h>
 
 #include <assert.h>
 
 static int
 verify(const jive_graph * graph)
 {
-	/*FIXME*/
+	using namespace jive::evaluator;
+
+	memliteral state;
+	bitliteral xl(jive::bits::value_repr(64, 0xFFFFFFFF0000000F));
+
+	std::unique_ptr<const literal> result;
+	result = std::move(eval(graph, "test_trunc", {&xl, &state})->copy());
+
+	const fctliteral * fctlit = dynamic_cast<const fctliteral*>(result.get());
+	assert(fctlit->nresults() == 2);
+	assert(dynamic_cast<const bitliteral*>(&fctlit->result(0))->value_repr() == 0x0000000F);
+
 	return 0;
 }
 
