3a13e42e59a3ba6a3a8780e9750db628b13f1950
Nico Reißmann <nico.reissmann@gmail.com>
2015-03-28 18:38:30 +0100

    tests: activate test-switch_fallthrough again

--- tests/libjlm/test-switch_fallthrough.cpp
+++ tests/libjlm/test-switch_fallthrough.cpp
@@ -5,12 +5,43 @@
 
 #include "test-registry.hpp"
 
+#include <jive/evaluator/eval.h>
+#include <jive/evaluator/literal.h>
+
 #include <assert.h>
 
 static int
 verify(const jive_graph * graph)
 {
-	/*FIXME*/
+	using namespace jive::evaluator;
+
+	/* test_switch_fallthrough(2) */
+	memliteral state;
+	bitliteral xl(jive::bits::value_repr(32, 2));
+
+	std::unique_ptr<const literal> result;
+	result = std::move(eval(graph, "test_switch_fallthrough", {&xl, &state})->copy());
+
+	const fctliteral * fctlit = dynamic_cast<const fctliteral*>(result.get());
+	assert(fctlit->nresults() == 2);
+	assert(dynamic_cast<const bitliteral*>(&fctlit->result(0))->value_repr() == 2);
+
+	/* test_switch_fallthrough(3) */
+	xl = jive::bits::value_repr(32, 3);
+	result = std::move(eval(graph, "test_switch_fallthrough", {&xl, &state})->copy());
+
+	fctlit = dynamic_cast<const fctliteral*>(result.get());
+	assert(fctlit->nresults() == 2);
+	assert(dynamic_cast<const bitliteral*>(&fctlit->result(0))->value_repr() == 7);
+
+	/* test_switch_fallthrough(10) */
+	xl = jive::bits::value_repr(32, 10);
+	result = std::move(eval(graph, "test_switch_fallthrough", {&xl, &state})->copy());
+
+	fctlit = dynamic_cast<const fctliteral*>(result.get());
+	assert(fctlit->nresults() == 2);
+	assert(dynamic_cast<const bitliteral*>(&fctlit->result(0))->value_repr() == 4);
+
 	return 0;
 }
 
--- tests/libjlm/test-switch_fallthrough.ll
+++ tests/libjlm/test-switch_fallthrough.ll
@@ -1,20 +1,22 @@
-; ModuleID = 'test-switch1'
+; ModuleID = 'main.c'
 target datalayout = "e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v64:64:64-v128:128:128-a0:0:64-s0:64:64-f80:128:128-n8:16:32:64-S128"
 target triple = "x86_64-unknown-linux-gnu"
 
-define i32 @switch1(i32 %a) nounwind uwtable {
+define i32 @test_switch_fallthrough(i32 %n) nounwind uwtable readnone {
 entry:
-  switch i32 %a, label %sw.epilog [
-    i32 0, label %sw.bb
-    i32 1, label %sw.bb1
+  switch i32 %n, label %sw.default [
+    i32 2, label %sw.epilog
+    i32 3, label %sw.bb1
   ]
 
-sw.bb:                                            ; preds = %entry
-  br label %sw.epilog
-
 sw.bb1:                                           ; preds = %entry
+  br label %sw.default
+
+sw.default:                                       ; preds = %entry, %sw.bb1
+  %r.0 = phi i32 [ 4, %entry ], [ 7, %sw.bb1 ]
   br label %sw.epilog
 
-sw.epilog:                                        ; preds = %entry, %sw.bb1, %sw.bb
-  ret i32 0
+sw.epilog:                                        ; preds = %entry, %sw.default
+  %r.1 = phi i32 [ %r.0, %sw.default ], [ 2, %entry ]
+  ret i32 %r.1
 }
