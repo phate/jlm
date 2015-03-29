20f44e5af81229eefe3a7478397786eb130d5f7b
Nico Reißmann <nico.reissmann@gmail.com>
2015-03-29 20:01:14 +0200

    tests: activate test-getelementptr again

--- tests/libjlm/test-getelementptr.cpp
+++ tests/libjlm/test-getelementptr.cpp
@@ -1,19 +1,47 @@
 /*
- * Copyright 2014 Nico Reißmann <nico.reissmann@gmail.com>
+ * Copyright 2014 2015 Nico Reißmann <nico.reissmann@gmail.com>
  * See COPYING for terms of redistribution.
  */
 
 #include "test-registry.hpp"
 
-#include <jive/arch/address.h>
-#include <jive/types/bitstring/constant.h>
+#include <jive/arch/address-transform.h>
+#include <jive/arch/memlayout-simple.h>
+#include <jive/evaluator/eval.h>
+#include <jive/evaluator/literal.h>
+
+#include <jive/view.h>
 
 #include <assert.h>
 
 static int
 verify(const jive_graph * graph)
 {
-	/*FIXME*/
+	/* FIXME: remove when the evaluator understands the address type */
+	setlocale(LC_ALL, "");
+
+	jive_memlayout_mapper_simple mapper;
+	jive_memlayout_mapper_simple_init(&mapper, 64);
+	jive_graph_address_transform(const_cast<jive_graph*>(graph), &mapper.base.base);
+	jive_memlayout_mapper_simple_fini(&mapper);
+
+	jive_graph_normalize(const_cast<jive_graph*>(graph));
+	jive_graph_prune(const_cast<jive_graph*>(graph));
+	jive_view(const_cast<jive_graph*>(graph), stdout);
+
+	using namespace jive::evaluator;
+
+	uint64_t array[] = {3, 4};
+	memliteral state;
+	bitliteral xl(jive::bits::value_repr(64, (uint64_t)array));
+
+	std::unique_ptr<const literal> result;
+	result = std::move(eval(graph, "test_getelementptr", {&xl, &state})->copy());
+
+	const fctliteral * fctlit = dynamic_cast<const fctliteral*>(result.get());
+	assert(fctlit->nresults() == 2);
+	assert(dynamic_cast<const bitliteral*>(&fctlit->result(0))->value_repr() == 7);
+
 	return 0;
 }
 
--- tests/libjlm/test-getelementptr.ll
+++ tests/libjlm/test-getelementptr.ll
@@ -1,12 +1,16 @@
-; ModuleID = 'dummy.c'
+; ModuleID = 'main.c'
 target datalayout = "e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v64:64:64-v128:128:128-a0:0:64-s0:64:64-f80:128:128-n8:16:32:64-S128"
 target triple = "x86_64-unknown-linux-gnu"
 
-%struct.ST = type { i32, double, %struct.RT }
-%struct.RT = type { i8, [10 x [20 x i32]], i8 }
-
-define i32 @test_getelementptr(%struct.ST* %s) nounwind uwtable readnone optsize {
+define i32 @test_getelementptr(i32* nocapture %p) nounwind uwtable readonly {
 entry:
-  %arrayidx2 = getelementptr inbounds %struct.ST* %s, i64 1, i32 2, i32 1, i64 5, i64 13
-  ret i32 0
+  %0 = load i32* %p, align 4, !tbaa !0
+  %arrayidx1 = getelementptr inbounds i32* %p, i64 1
+  %1 = load i32* %arrayidx1, align 4, !tbaa !0
+  %add = add i32 %1, %0
+  ret i32 %add
 }
+
+!0 = metadata !{metadata !"int", metadata !1}
+!1 = metadata !{metadata !"omnipotent char", metadata !2}
+!2 = metadata !{metadata !"Simple C/C++ TBAA"}
