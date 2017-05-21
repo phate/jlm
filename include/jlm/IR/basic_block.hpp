5a6f4a3465435e6925cd919c7ba5601662a7bed7
Nico Reissmann <nico.reissmann@gmail.com>
2017-05-21 16:14:22 +0200

    IR: add basic block attribute class

--- include/jlm/IR/basic_block.hpp
+++ include/jlm/IR/basic_block.hpp
@@ -18,6 +18,85 @@ namespace jlm {
 class expr;
 class tac;
 
+class basic_block_attribute final : public attribute {
+public:
+	virtual
+	~basic_block_attribute();
+
+	inline
+	basic_block_attribute()
+	: attribute()
+	{}
+
+	inline
+	basic_block_attribute(const basic_block_attribute & other)
+	: tacs_(other.tacs_)
+	{}
+
+	basic_block_attribute(basic_block_attribute &&) = delete;
+
+	inline basic_block_attribute &
+	operator=(const basic_block_attribute & other)
+	{
+		if (this == &other)
+			return *this;
+
+		tacs_ = other.tacs_;
+	}
+
+	basic_block_attribute &
+	operator=(basic_block_attribute &&) = delete;
+
+	const tac *
+	append(
+		const jive::operation & operation,
+		const std::vector<const variable*> & operands,
+		const std::vector<const variable*> & results);
+
+	const tac *
+	append(
+		jlm::cfg * cfg,
+		const jive::operation & operation,
+		const std::vector<const variable*> & operands);
+
+	const variable *
+	append(jlm::cfg * cfg, const expr & e, const variable * v);
+
+	const variable *
+	append(jlm::cfg * cfg, const expr & e);
+
+	/*
+		FIXME: add accessor functions
+	*/
+	inline std::list<const tac*> &
+	tacs() noexcept
+	{
+		return tacs_;
+	}
+
+	inline const std::list<const tac*> &
+	tacs() const noexcept
+	{
+		return tacs_;
+	}
+
+	virtual std::string
+	debug_string() const noexcept override;
+
+	virtual std::unique_ptr<attribute>
+	copy() const override;
+
+private:
+	std::list<const tac*> tacs_;
+};
+
+static inline basic_block_attribute &
+create_basic_block(jlm::cfg & cfg)
+{
+	basic_block_attribute attr;
+	return *static_cast<basic_block_attribute*>(&cfg.create_node(attr)->attribute());
+}
+
 class basic_block final : public cfg_node {
 public:
 	virtual ~basic_block();
--- src/IR/basic_block.cpp
+++ src/IR/basic_block.cpp
@@ -13,6 +13,81 @@
 
 namespace jlm {
 
+/* basic block attribute */
+
+basic_block_attribute::~basic_block_attribute()
+{
+	for (const auto & tac : tacs_)
+		delete tac;
+}
+
+std::string
+basic_block_attribute::debug_string() const noexcept
+{
+	std::stringstream sstrm;
+
+	sstrm << this << "\\n";
+	for (auto tac : tacs_)
+		sstrm << tac->debug_string() << "\\n";
+
+	return sstrm.str();
+}
+
+std::unique_ptr<attribute>
+basic_block_attribute::copy() const
+{
+	return std::make_unique<basic_block_attribute>(*this);
+}
+
+const tac *
+basic_block_attribute::append(
+	const jive::operation & operation,
+	const std::vector<const variable*> & operands,
+	const std::vector<const variable*> & results)
+{
+	auto tac = new jlm::tac(operation, operands, results);
+	tacs_.push_back(tac);
+	return tac;
+}
+
+const tac *
+basic_block_attribute::append(
+	jlm::cfg * cfg,
+	const jive::operation & operation,
+	const std::vector<const variable*> & operands)
+{
+	std::vector<const variable*> results;
+	for (size_t n = 0; n < operation.nresults(); n++)
+		results.push_back(cfg->create_variable(operation.result_type(n)));
+
+	return append(operation, operands, results);
+}
+
+const variable *
+basic_block_attribute::append(
+	jlm::cfg * cfg,
+	const expr & e,
+	const variable * result)
+{
+	std::vector<const variable *> operands;
+	for (size_t n = 0; n < e.noperands(); n++) {
+		auto opv = cfg->create_variable(e.operand(n).type());
+		operands.push_back(append(cfg, e.operand(n), opv));
+	}
+
+	auto tac = new jlm::tac(e.operation(), operands, {result});
+	tacs_.push_back(tac);
+	return tac->output(0);
+}
+
+const variable *
+basic_block_attribute::append(jlm::cfg * cfg, const expr & e)
+{
+	return append(cfg, e, cfg->create_variable(e.type()));
+}
+
+/* basic block */
+
 basic_block::~basic_block()
 {
 	for (auto tac : tacs_)
