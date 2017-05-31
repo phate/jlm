2513ea0cd991b24c6df2fcc88084b08ba5d3ecfa
Nico Reissmann <nico.reissmann@gmail.com>
2017-05-31 09:54:44 +0200

    util: add strfmt function

---
+++ include/jlm/util/strfmt.hpp
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2017 Nico Reißmann <nico.reissmann@gmail.com>
+ * See COPYING for terms of redistribution.
+ */
+
+#ifndef JLM_UTIL_STRFMT_HPP
+#define JLM_UTIL_STRFMT_HPP
+
+#include <sstream>
+
+template<typename ...Args>
+static inline void
+format_to_stream(std::ostream & os, Args... args);
+
+template<typename Arg>
+static inline void
+format_to_stream(std::ostream & os, const Arg & arg)
+{
+	os << arg;
+}
+
+template<typename Arg, typename... Args>
+static inline void
+format_to_stream(std::ostream & os, const Arg & arg, Args... args)
+{
+	os << arg;
+	format_to_stream(os, args...);
+}
+
+template<typename ...Args>
+static inline std::string
+strfmt(Args... args)
+{
+	std::ostringstream os;
+	format_to_stream(os, args...);
+	return os.str();
+}
+
+#endif
