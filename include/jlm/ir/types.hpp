ad30395612985e95a4272f45873aec143f07dc04
Nico Reissmann <nico.reissmann@gmail.com>
2017-06-16 15:57:13 +0200

    ir: add array type

--- include/jlm/ir/types.hpp
+++ include/jlm/ir/types.hpp
@@ -10,6 +10,8 @@
 
 namespace jlm {
 
+/* pointer type */
+
 class ptrtype : public jive::value::type {
 public:
 	virtual
@@ -71,6 +73,84 @@ create_ptrtype(const jive::base::type & vtype)
 	if (!vt) throw std::logic_error("Expected value type.");
 
 	return std::unique_ptr<jive::base::type>(new jlm::ptrtype(*vt));
+}
+
+/* array type */
+
+class arraytype final : public jive::value::type {
+public:
+	virtual
+	~arraytype();
+
+	inline
+	arraytype(const jive::value::type & type, size_t nelements)
+	: jive::value::type()
+	, nelements_(nelements)
+	, type_(type.copy())
+	{
+		if (nelements == 0)
+			throw std::logic_error("Expected at least one element.");
+	}
+
+	inline
+	arraytype(const jlm::arraytype & other)
+	: jive::value::type(other)
+	, nelements_(other.nelements_)
+	, type_(std::move(other.type_->copy()))
+	{}
+
+	inline
+	arraytype(jlm::arraytype && other)
+	: jive::value::type(other)
+	, nelements_(other.nelements_)
+	, type_(std::move(other.type_))
+	{}
+
+	inline arraytype &
+	operator=(const jlm::arraytype &) = delete;
+
+	inline arraytype &
+	operator=(jlm::arraytype &&) = delete;
+
+	virtual std::string
+	debug_string() const override;
+
+	virtual bool
+	operator==(const jive::base::type & other) const noexcept override;
+
+	virtual std::unique_ptr<jive::base::type>
+	copy() const override;
+
+	inline size_t
+	nelements() const noexcept
+	{
+		return nelements_;
+	}
+
+	inline const jive::value::type &
+	element_type() const noexcept
+	{
+		return *static_cast<const jive::value::type*>(type_.get());
+	}
+
+private:
+	size_t nelements_;
+	std::unique_ptr<jive::base::type> type_;
+};
+
+static inline bool
+is_arraytype(const jive::base::type & type)
+{
+	return dynamic_cast<const jlm::arraytype*>(&type) != nullptr;
+}
+
+static inline std::unique_ptr<jive::base::type>
+create_arraytype(const jive::base::type & type, size_t nelements)
+{
+	auto vt = dynamic_cast<const jive::value::type*>(&type);
+	if (!vt) throw std::logic_error("Expected value type.");
+
+	return std::unique_ptr<jive::base::type>(new arraytype(*vt, nelements));
 }
 
 }
--- src/ir/types.cpp
+++ src/ir/types.cpp
@@ -4,8 +4,11 @@
  */
 
 #include <jlm/ir/types.hpp>
+#include <jlm/util/strfmt.hpp>
 
 namespace jlm {
+
+/* pointer type */
 
 ptrtype::~ptrtype()
 {}
@@ -27,6 +30,30 @@ std::unique_ptr<jive::base::type>
 ptrtype::copy() const
 {
 	return std::unique_ptr<jive::base::type>(new ptrtype(*this));
+}
+
+/* array type */
+
+arraytype::~arraytype()
+{}
+
+std::string
+arraytype::debug_string() const
+{
+	return strfmt("[ ", nelements(), " x ", type_->debug_string(), " ]");
+}
+
+bool
+arraytype::operator==(const jive::base::type & other) const noexcept
+{
+	auto type = dynamic_cast<const jlm::arraytype*>(&other);
+	return type && type->element_type() == element_type() && type->nelements() == nelements();
+}
+
+std::unique_ptr<jive::base::type>
+arraytype::copy() const
+{
+	return std::unique_ptr<jive::base::type>(new arraytype(*this));
 }
 
 }
