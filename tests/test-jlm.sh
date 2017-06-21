fd5c8ab246581a9a2a46e7b972e9ee77ecf49a9f
Nico Reissmann <nico.reissmann@gmail.com>
2017-06-21 18:30:27 +0200

    tests: add environment for C tests

---
+++ tests/test-jlm.sh
@@ -0,0 +1,21 @@
+#!	/bin/bash
+
+if [ $# -eq 0 ] ; then
+	echo "ERROR: No file supplied."
+	exit 1
+fi
+
+root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+base=$(basename $1)
+file="/tmp/${base%.*}"
+clang_out="${file}.ll"
+jlm_out="${file}-jlm.ll"
+llc_out="${file}-jlm.o"
+gcc_out="${file}-jlm"
+
+clang-3.7 -O0 -S -emit-llvm $1 -o ${clang_out}
+${root}/../jlm -llvm ${clang_out} 2> ${jlm_out}
+llc-3.7 -O0 -filetype=obj -o ${llc_out} ${jlm_out}
+clang-3.7 -O0 ${llc_out} -o ${gcc_out}
+bash -c "${gcc_out}"
