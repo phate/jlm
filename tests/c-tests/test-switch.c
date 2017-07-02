9f895939fe0c60dc00f74862cf8b6f84f86dc519
Nico Reissmann <nico.reissmann@gmail.com>
2017-07-02 12:42:47 +0200

    tests: extend switch test

--- tests/c-tests/test-switch.c
+++ tests/c-tests/test-switch.c
@@ -1,6 +1,19 @@
 #include <assert.h>
 
 unsigned int
+test_switch_fallthrough(unsigned int x)
+{
+	switch(x) {
+		case 1:
+		case 2:
+		case 3:
+			return 4;
+		default:
+			return 42;
+	}
+}
+
+unsigned int
 test_switch(unsigned int x)
 {
 	switch(x) {
@@ -18,5 +31,10 @@ main()
 	assert(test_switch(2) == 2);
 	assert(test_switch(3) == 3);
 	assert(test_switch(11) == 42);
+
+	assert(test_switch_fallthrough(1) == 4);
+	assert(test_switch_fallthrough(2) == 4);
+	assert(test_switch_fallthrough(3) == 4);
+	assert(test_switch_fallthrough(11) == 42);
 	return 0;
 }
