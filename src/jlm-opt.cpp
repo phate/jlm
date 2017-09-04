2a89f1f783a1320ec582a9d5186f7a633469f460
Nico Reissmann <nico.reissmann@gmail.com>
2017-09-04 15:22:49 +0200

    jlm-opt: add support for dne pass

--- src/jlm-opt.cpp
+++ src/jlm-opt.cpp
@@ -8,6 +8,7 @@
 #include <jlm/jlm2rvsdg/module.hpp>
 #include <jlm/jlm2llvm/jlm2llvm.hpp>
 #include <jlm/llvm2jlm/module.hpp>
+#include <jlm/opt/dne.hpp>
 #include <jlm/rvsdg2jlm/rvsdg2jlm.hpp>
 
 #include <llvm/IR/LLVMContext.h>
@@ -18,26 +19,62 @@
 
 #include <iostream>
 
+enum class opt {dne};
+
 static void
 print_usage(const std::string & app)
 {
-	std::cerr << "Usage: " << app << " FILE\n";
+	std::cerr << "Usage: " << app << " [OPTIONS] FILE\n";
+	std::cerr << "OPTIONS:\n";
+	std::cerr << "--dne: Perform dead node elimination.\n";
 }
 
-int
-main(int argc, char ** argv)
+static std::string
+parse_cmdflags(int argc, char ** argv, std::vector<opt> & passes)
 {
-	if (argc != 2) {
+	if (argc < 2) {
 		std::cerr << "Expected LLVM IR file as input.\n";
 		print_usage(argv[0]);
 		exit(1);
 	}
 
+	for (int n = 1; n < argc-1; n++) {
+		std::string flag(argv[n]);
+
+		if (flag == "--dne") {
+			passes.push_back(opt::dne);
+			continue;
+		}
+	}
+
+	return std::string(argv[argc-1]);
+}
+
+static void
+perform_optimizations(jive::graph * graph, const std::vector<opt> & opts)
+{
+	for (const auto & opt : opts) {
+		if (opt == opt::dne) {
+			jlm::dne(*graph);
+			continue;
+		}
+	}
+}
+
+int
+main(int argc, char ** argv)
+{
+	std::vector<opt> optimizations;
+	auto file = parse_cmdflags(argc, argv, optimizations);
+
 	llvm::SMDiagnostic d;
-	auto lm = llvm::parseIRFile(argv[1], d, llvm::getGlobalContext());
+	auto lm = llvm::parseIRFile(file, d, llvm::getGlobalContext());
 
 	auto jm = jlm::convert_module(*lm);
 	auto rvsdg = jlm::construct_rvsdg(*jm);
+
+	perform_optimizations(rvsdg->graph(), optimizations);
+
 	jm = jlm::rvsdg2jlm::rvsdg2jlm(*rvsdg);
 	lm = jlm::jlm2llvm::convert(*jm, llvm::getGlobalContext());
 
