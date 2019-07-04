608962d52eda115d424cd732eb9c821aa613a477
Nico Reissmann <nico.reissmann@gmail.com>
2019-07-04 12:21:17 +0200

    libjlm: add timer class

---
+++ libjlm/include/jlm/util/time.hpp
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2019 Nico Reißmann <nico.reissmann@gmail.com>
+ * See COPYING for terms of redistribution.
+ */
+
+#ifndef JLM_UTIL_TIME_HPP
+#define JLM_UTIL_TIME_HPP
+
+#include <chrono>
+
+namespace jlm {
+
+class timer final {
+public:
+	constexpr timer()
+	{}
+
+	void
+	start() noexcept
+	{
+		start_ = std::chrono::high_resolution_clock::now();
+	}
+
+	void
+	stop() noexcept
+	{
+		end_ = std::chrono::high_resolution_clock::now();
+	}
+
+	size_t
+	ns()
+	{
+		return std::chrono::duration_cast<std::chrono::nanoseconds>(end_-start_).count();
+	}
+
+private:
+	std::chrono::time_point<std::chrono::system_clock> start_;
+	std::chrono::time_point<std::chrono::system_clock> end_;
+};
+
+}
+
+#endif
