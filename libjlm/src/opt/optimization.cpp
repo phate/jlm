8abede2276ee8befbdd94504662e918d1ff1b7f2
Nico Reissmann <nico.reissmann@gmail.com>
2020-01-12 20:53:46 +0100

    libjlm/opt: add optimization_stat for printing statistics

--- libjlm/src/opt/optimization.cpp
+++ libjlm/src/opt/optimization.cpp
@@ -17,11 +17,51 @@
 #include <jlm/opt/unroll.hpp>
 
 #include <jlm/util/stats.hpp>
+#include <jlm/util/strfmt.hpp>
 #include <jlm/util/time.hpp>
 
 #include <unordered_map>
 
 namespace jlm {
+
+class optimization_stat final : public stat {
+public:
+	virtual
+	~optimization_stat()
+	{}
+
+	optimization_stat(const jlm::filepath & filename)
+	: filename_(filename)
+	, nnodes_before_(0)
+	, nnodes_after_(0)
+	{}
+
+	void
+	start(const jive::graph & graph) noexcept
+	{
+		nnodes_before_ = jive::nnodes(graph.root());
+		timer_.start();
+	}
+
+	void
+	end(const jive::graph & graph) noexcept
+	{
+		timer_.stop();
+		nnodes_after_ = jive::nnodes(graph.root());
+	}
+
+	virtual std::string
+	to_str() const override
+	{
+		return strfmt("RVSDGOPTIMIZATION ", filename_.to_str(), " ",
+			nnodes_before_, " ", nnodes_after_, " ", timer_.ns());
+	}
+
+private:
+	jlm::timer timer_;
+	jlm::filepath filename_;
+	size_t nnodes_before_, nnodes_after_;
+};
 
 static void
 unroll(rvsdg_module & rm, const stats_descriptor & sd)
@@ -55,23 +95,15 @@ optimize(
 	const stats_descriptor & sd,
 	const std::vector<optimization> & opts)
 {
-	jlm::timer timer;
-	size_t nnodes_before = 0;
-	if (sd.print_rvsdg_optimization) {
-		nnodes_before = jive::nnodes(rm.graph()->root());
-		timer.start();
-	}
+	optimization_stat stat(rm.source_filename());
 
+	stat.start(*rm.graph());
 	for (const auto & opt : opts)
 		optimize(rm, sd, opt);
+	stat.end(*rm.graph());
 
-	if (sd.print_rvsdg_optimization) {
-		timer.stop();
-		size_t nnodes_after = jive::nnodes(rm.graph()->root());
-		fprintf(sd.file().fd(),
-			"RVSDGOPTIMIZATION %s %zu %zu %zu\n", rm.source_filename().to_str().c_str(),
-				nnodes_before, nnodes_after, timer.ns());
-	}
+	if (sd.print_rvsdg_optimization)
+		sd.print_stat(stat);
 }
 
 }
