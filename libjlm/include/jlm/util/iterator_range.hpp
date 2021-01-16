fa659eee012097d0c7d11986bdf2bab2a2b96632
Nico Reissmann <nico.reissmann@gmail.com>
2021-01-16 23:29:03 +0100

    libjlm/util: add iterator_range class

---
+++ libjlm/include/jlm/util/iterator_range.hpp
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2021 Nico Reißmann <nico.reissmann@gmail.com>
+ * See COPYING for terms of redistribution.
+ */
+
+#ifndef JLM_UTIL_ITERATOR_RANGE_HPP
+#define JLM_UTIL_ITERATOR_RANGE_HPP
+
+namespace jlm {
+
+/** \brief Iterator Range
+*
+* A range-compatible interface wrapping a pair of iterators.
+*/
+template<typename T>
+class iterator_range {
+public:
+	iterator_range(T begin, T end)
+	: begin_(std::move(begin))
+	, end_(std::move(end))
+	{}
+
+	template<typename container_t>
+	iterator_range(container_t && c)
+	: begin_(c.begin())
+	, end_(c.end())
+	{}
+
+	T
+	begin() const
+	{
+		return begin_;
+	}
+
+	T
+	end() const
+	{
+		return end_;
+	}
+
+private:
+	T begin_, end_;
+};
+
+}
+
+#endif
