33cfa8bc98c9c4224ae7fae0bd26c84e86235f02
Nico Reissmann <nico.reissmann@gmail.com>
2021-02-13 13:40:57 +0100

    libjlm/ir: replace create_fp2ui_tac function with creator

--- libjlm/include/jlm/ir/operators/operators.hpp
+++ libjlm/include/jlm/ir/operators/operators.hpp
@@ -263,20 +263,22 @@ public:
 	reduce_operand(
 		jive_unop_reduction_path_t path,
 		jive::output * output) const override;
+
+	static std::unique_ptr<jlm::tac>
+	create(
+		const variable * operand,
+		variable * result)
+	{
+		auto st = dynamic_cast<const fptype*>(&operand->type());
+		if (!st) throw jlm::error("expected floating point type.");
+
+		auto dt = dynamic_cast<const jive::bittype*>(&result->type());
+		if (!dt) throw jlm::error("expected bitstring type.");
+
+		fp2ui_op op(st->size(), *dt);
+		return tac::create(op, {operand}, {result});
+	}
 };
-
-static inline std::unique_ptr<jlm::tac>
-create_fp2ui_tac(const variable * operand, jlm::variable * result)
-{
-	auto st = dynamic_cast<const fptype*>(&operand->type());
-	if (!st) throw jlm::error("expected floating point type.");
-
-	auto dt = dynamic_cast<const jive::bittype*>(&result->type());
-	if (!dt) throw jlm::error("expected bitstring type.");
-
-	fp2ui_op op(st->size(), *dt);
-	return tac::create(op, {operand}, {result});
-}
 
 /* fp2si operator */
 
