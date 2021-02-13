8a3f7458619374a06d0bf8ab9a3d404b9cba925a
Nico Reissmann <nico.reissmann@gmail.com>
2021-02-13 14:42:34 +0100

    libjlm/ir: replace create_fpext_tac function with creator

--- libjlm/include/jlm/ir/operators/operators.hpp
+++ libjlm/include/jlm/ir/operators/operators.hpp
@@ -1103,20 +1103,22 @@ public:
 	{
 		return static_cast<const jlm::fptype*>(&result(0).type())->size();
 	}
+
+	static std::unique_ptr<jlm::tac>
+	create(
+		const variable * operand,
+		variable * result)
+	{
+		auto st = dynamic_cast<const jlm::fptype*>(&operand->type());
+		if (!st) throw jlm::error("expected floating point type.");
+
+		auto dt = dynamic_cast<const jlm::fptype*>(&result->type());
+		if (!dt) throw jlm::error("expected floating point type.");
+
+		jlm::fpext_op op(st->size(), dt->size());
+		return tac::create(op, {operand}, {result});
+	}
 };
-
-static inline std::unique_ptr<jlm::tac>
-create_fpext_tac(const variable * operand, jlm::variable * result)
-{
-	auto st = dynamic_cast<const jlm::fptype*>(&operand->type());
-	if (!st) throw jlm::error("expected floating point type.");
-
-	auto dt = dynamic_cast<const jlm::fptype*>(&result->type());
-	if (!dt) throw jlm::error("expected floating point type.");
-
-	jlm::fpext_op op(st->size(), dt->size());
-	return tac::create(op, {operand}, {result});
-}
 
 /* fpneg operator */
 
