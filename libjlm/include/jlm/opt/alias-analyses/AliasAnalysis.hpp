004a8d603a751f37e1b23b685ea62480d7a1461f
Nico Reissmann <nico.reissmann@gmail.com>
2021-06-06 11:08:54 +0200

    libjlm/opt: add alias analysis interface

---
+++ libjlm/include/jlm/opt/alias-analyses/AliasAnalysis.hpp
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2021 Nico Reißmann <nico.reissmann@gmail.com>
+ * See COPYING for terms of redistribution.
+ */
+
+#ifndef JLM_OPT_ALIAS_ANALYSES_ALIASANALYSIS_HPP
+#define JLM_OPT_ALIAS_ANALYSES_ALIASANALYSIS_HPP
+
+#include <memory>
+
+namespace jlm {
+
+class rvsdg_module;
+
+namespace aa {
+
+class ptg;
+
+/**
+* \brief Alias Analysis Interface
+*/
+class AliasAnalysis {
+public:
+	virtual
+	~AliasAnalysis() = default;
+
+	/**
+	* \brief Analyze RVSDG module
+	*
+	* \param module RVSDG module the analysis is performed on.
+	*
+	* \return A PointsTo graph.
+	*/
+	virtual std::unique_ptr<ptg>
+	Analyze(const rvsdg_module & module) = 0;
+};
+
+}}
+
+#endif
