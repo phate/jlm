46bbb1a25a8ee10aef384c2d30c2b6086fa01f0b
Nico Reissmann <nico.reissmann@gmail.com>
2021-06-18 05:41:47 +0200

    libjlm/jlm-llvm: fix conversion of free operation

--- libjlm/src/backend/llvm/jlm2llvm/instruction.cpp
+++ libjlm/src/backend/llvm/jlm2llvm/instruction.cpp
@@ -788,7 +788,7 @@ convert(
 {
 	auto & llvmmod = ctx.llvm_module();
 
-	auto fcttype = convert_type(op.fcttype(), ctx);
+	auto fcttype = convert_type(jive::fcttype({&op.argument(0).type()}, {}), ctx);
 	auto function = llvmmod.getOrInsertFunction("free", fcttype);
 	auto operands = std::vector<llvm::Value*>(1, ctx.value(args[0]));
 	return builder.CreateCall(function, operands);
