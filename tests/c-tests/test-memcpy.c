bc246dda32821a7302677d341c75fc189b2a031d
Nico Reissmann <nico.reissmann@gmail.com>
2021-07-18 13:53:06 +0200

    TODO

---
+++ tests/c-tests/test-memcpy.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+
+static const char *
+Create()
+{
+	return malloc(200);
+}
+
+static void
+Copy(const char** pp)
+{
+	const char * p = *pp;
+
+	char tmp[200];
+	memcpy(tmp, p, 200);
+}
+
+int
+main()
+{
+	const char * p = Create();
+	Copy(&p);
+	return 0;
+}
