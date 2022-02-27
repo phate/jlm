8a90c6c7e1e141f8d3db75e1ae2492c35b867e5b
Nico Reissmann <nico.reissmann@gmail.com>
2022-02-27 14:45:52 +0100

    libjlm/frontend: remove unused convert_type functions

--- libjlm/include/jlm/frontend/llvm/LlvmTypeConversion.hpp
+++ libjlm/include/jlm/frontend/llvm/LlvmTypeConversion.hpp
@@ -36,36 +36,12 @@ convert_fpsize(const llvm::Type * type);
 std::unique_ptr<jive::valuetype>
 ConvertType(const llvm::Type * type, context & ctx);
 
-static inline std::unique_ptr<jlm::arraytype>
-convert_arraytype(const llvm::ArrayType * type, context & ctx)
-{
-	auto t = ConvertType(llvm::cast<llvm::Type>(type), ctx);
-	JLM_ASSERT(jive::is<arraytype>(*t));
-	return std::unique_ptr<jlm::arraytype>(static_cast<jlm::arraytype*>(t.release()));
-}
-
 static inline std::unique_ptr<FunctionType>
 ConvertFunctionType(const llvm::FunctionType * type, context & ctx)
 {
 	auto t = ConvertType(llvm::cast<llvm::Type>(type), ctx);
 	JLM_ASSERT(dynamic_cast<const FunctionType*>(t.get()));
 	return std::unique_ptr<FunctionType>(static_cast<FunctionType*>(t.release()));
-}
-
-static inline std::unique_ptr<structtype>
-convert_type(const llvm::StructType * type, context & ctx)
-{
-	auto t = ConvertType(llvm::cast<llvm::Type>(type), ctx);
-	JLM_ASSERT(dynamic_cast<const structtype*>(t.get()));
-	return std::unique_ptr<structtype>(static_cast<structtype*>(t.release()));
-}
-
-static inline std::unique_ptr<vectortype>
-convert_type(const llvm::VectorType * type, context & ctx)
-{
-	auto t = ConvertType(llvm::cast<llvm::Type>(type), ctx);
-	JLM_ASSERT(dynamic_cast<const vectortype*>(t.get()));
-	return std::unique_ptr<vectortype>(static_cast<vectortype*>(t.release()));
 }
 
 static inline std::unique_ptr<ptrtype>
