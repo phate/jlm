1407811dc1f6ba3c6329766c1cfe416007494072
Nico Reissmann <nico.reissmann@gmail.com>
2022-05-15 11:23:55 +0200

    libjlm/aa: add documentation to MemoryStateEncoder class

--- libjlm/include/jlm/opt/alias-analyses/MemoryStateEncoder.hpp
+++ libjlm/include/jlm/opt/alias-analyses/MemoryStateEncoder.hpp
@@ -35,8 +35,19 @@ namespace aa {
 
 class PointsToGraph;
 
-/** \brief Encodes a Points-To graph in the RVSDG.
-*/
+/** \brief Memory State Encoder Interface
+ *
+ * A memory state encoder encodes a points-to graph in the RVSDG. The basic idea is that there exists a one-to-one
+ * correspondence between memory nodes in the points-to graph and memory states in the RVSDG, i.e., for each memory
+ * node in the points-to graph, there exists a memory state edge in the RVSDG. A memory state encoder routes these
+ * state edges through the RVSDG's structural nodes and ensures that simple nodes operating on a
+ * memory location represented by a corresponding memory node in the points-to graph are sequentialized with the
+ * respective memory state edge. For example, a store node that modifies a global variable needs to have the respective
+ * state edge that corresponds to its memory location routed through it, i.e., the store node
+ * is sequentialized by this state edge. Such an encoding ensures that the ordering of side-effecting operations
+ * touching on the same memory locations is preserved, while rendering operations independent that are not operating on
+ * the same memory locations.
+ */
 class MemoryStateEncoder {
 public:
 	virtual
