af7a47118fa14f13eb38a5da416b4acdedd49f7b
Magnus Sjalander <work@sjalander.com>
2022-05-16 15:56:51 +0200

    jhls: Tool for compiling and synthesising C-code
    jhls can be passed any number of C-files containing the
    application. The function to be accelerated is defined by
    --hls-function=<function_name>.
    The output is an executable that simulates the application and the
    synthesised function using verilator.

---
+++ jhls/include/jhls/command.hpp
@@ -0,0 +1,679 @@
+/*
+ * Copyright 2018 Nico Reißmann <nico.reissmann@gmail.com>
+ * See COPYING for terms of redistribution.
+ */
+
+#ifndef JLM_JHLS_COMMAND_HPP
+#define JLM_JHLS_COMMAND_HPP
+
+#include <jhls/cmdline.hpp>
+#include <jlm/driver/passgraph.hpp>
+
+#include <memory>
+#include <string>
+#include <vector>
+#include <unistd.h>
+
+namespace jlm {
+
+/* parser command */
+
+class prscmd final : public command {
+public:
+	virtual
+	~prscmd();
+
+	prscmd(
+		const jlm::filepath & ifile,
+		const jlm::filepath & dependencyFile,
+		const jlm::filepath & tmpfolder,
+		const std::vector<std::string> & Ipaths,
+		const std::vector<std::string> & Dmacros,
+		const std::vector<std::string> & Wwarnings,
+		const std::vector<std::string> & flags,
+		bool verbose,
+		bool rdynamic,
+		bool suppress,
+		bool pthread,
+		bool MD,
+		bool hls,
+		const std::string & mT,
+		const standard & std)
+	: std_(std)
+	, ifile_(ifile)
+	, tmpfolder_(tmpfolder)
+	, Ipaths_(Ipaths)
+	, Dmacros_(Dmacros)
+	, Wwarnings_(Wwarnings)
+	, flags_(flags)
+	, verbose_(verbose)
+	, rdynamic_(rdynamic)
+	, suppress_(suppress)
+	, pthread_(pthread)
+	, MD_(MD)
+	, hls_(hls)
+	, mT_(mT)
+	, dependencyFile_(dependencyFile)
+	{}
+
+	virtual std::string
+	to_str() const override;
+
+	jlm::filepath
+	ofile() const;
+
+	virtual void
+	run() const override;
+
+	static passgraph_node *
+	create(
+		passgraph * pgraph,
+		const jlm::filepath & ifile,
+		const jlm::filepath & dependencyFile,
+		const jlm::filepath & tmpfolder,
+		const std::vector<std::string> & Ipaths,
+		const std::vector<std::string> & Dmacros,
+		const std::vector<std::string> & Wwarnings,
+		const std::vector<std::string> & flags,
+		bool verbose,
+		bool rdynamic,
+		bool suppress,
+		bool pthread,
+		bool MD,
+		bool hls,
+		const std::string & mT,
+		const standard & std)
+	{
+		std::unique_ptr<prscmd> cmd(new prscmd(
+			ifile,
+			dependencyFile,
+			tmpfolder,
+			Ipaths,
+			Dmacros,
+			Wwarnings,
+			flags,
+			verbose,
+			rdynamic,
+			suppress,
+			pthread,
+			MD,
+			hls,
+			mT,
+			std));
+
+		return passgraph_node::create(pgraph, std::move(cmd));
+	}
+
+private:
+	static std::string
+	replace_all(std::string str, const std::string& from, const std::string& to);
+
+	standard std_;
+	jlm::filepath ifile_;
+	jlm::filepath tmpfolder_;
+	std::vector<std::string> Ipaths_;
+	std::vector<std::string> Dmacros_;
+	std::vector<std::string> Wwarnings_;
+	std::vector<std::string> flags_;
+	bool verbose_;
+	bool rdynamic_;
+	bool suppress_;
+	bool pthread_;
+	bool MD_;
+	bool hls_;
+	std::string mT_;
+	jlm::filepath dependencyFile_;
+};
+
+/* optimization command */
+
+class optcmd final : public command {
+public:
+	virtual
+	~optcmd();
+
+	optcmd(
+		const jlm::filepath & ifile,
+		const jlm::filepath & tmpfolder,
+		const std::vector<std::string> & jlmopts,
+		const optlvl & ol)
+	: ifile_(ifile)
+	, tmpfolder_(tmpfolder)
+	, jlmopts_(jlmopts)
+	, ol_(ol)
+	{}
+
+	virtual std::string
+	to_str() const override;
+
+	virtual void
+	run() const override;
+
+	static passgraph_node *
+	create(
+		passgraph * pgraph,
+		const jlm::filepath & ifile,
+		const jlm::filepath & tmpfolder,
+		const std::vector<std::string> & jlmopts,
+		const optlvl & ol)
+	{
+		return passgraph_node::create(pgraph, std::make_unique<optcmd>(ifile, tmpfolder, jlmopts, ol));
+	}
+
+private:
+	jlm::filepath ifile_;
+	jlm::filepath tmpfolder_;
+	std::vector<std::string> jlmopts_;
+	optlvl ol_;
+};
+
+/* code generator command */
+
+class cgencmd final : public command {
+public:
+	virtual
+	~cgencmd();
+
+	cgencmd(
+		const jlm::filepath & ifile,
+		const jlm::filepath & ofile,
+		const jlm::filepath & tmpfolder,
+		const bool hls,
+		const optlvl & ol)
+	: ol_(ol)
+	, ifile_(ifile)
+	, ofile_(ofile)
+	, tmpfolder_(tmpfolder)
+	, hls_(hls)
+	{}
+
+	virtual std::string
+	to_str() const override;
+
+	virtual void
+	run() const override;
+
+	inline const jlm::filepath &
+	ofile() const noexcept
+	{
+		return ofile_;
+	}
+
+	static passgraph_node *
+	create(
+		passgraph * pgraph,
+		const jlm::filepath & ifile,
+		const jlm::filepath & ofile,
+		const jlm::filepath & tmpfolder,
+		const bool hls,
+		const optlvl & ol)
+	{
+		return passgraph_node::create(pgraph, std::make_unique<cgencmd>(ifile, ofile, tmpfolder, hls, ol));
+	}
+
+private:
+	optlvl ol_;
+	jlm::filepath ifile_;
+	jlm::filepath ofile_;
+	jlm::filepath tmpfolder_;
+	bool hls_;
+};
+
+/* linker command */
+
+class lnkcmd final : public command {
+public:
+	virtual
+	~lnkcmd();
+
+	lnkcmd(
+		const std::vector<jlm::filepath> & ifiles,
+		const jlm::filepath & ofile,
+		const std::vector<std::string> & Lpaths,
+		const std::vector<std::string> & libs,
+		bool pthread)
+	: ofile_(ofile)
+	, libs_(libs)
+	, ifiles_(ifiles)
+	, Lpaths_(Lpaths)
+	, pthread_(pthread)
+	{}
+
+	virtual std::string
+	to_str() const override;
+
+	virtual void
+	run() const override;
+
+	inline const jlm::filepath &
+	ofile() const noexcept
+	{
+		return ofile_;
+	}
+
+	inline const std::vector<jlm::filepath> &
+	ifiles() const noexcept
+	{
+		return ifiles_;
+	}
+
+	static passgraph_node *
+	create(
+		passgraph * pgraph,
+		const std::vector<jlm::filepath> & ifiles,
+		const jlm::filepath & ofile,
+		const std::vector<std::string> & Lpaths,
+		const std::vector<std::string> & libs,
+		bool pthread)
+	{
+		std::unique_ptr<lnkcmd> cmd(new lnkcmd(ifiles, ofile, Lpaths, libs, pthread));
+		return passgraph_node::create(pgraph, std::move(cmd));
+	}
+
+private:
+	jlm::filepath ofile_;
+	std::vector<std::string> libs_;
+	std::vector<jlm::filepath> ifiles_;
+	std::vector<std::string> Lpaths_;
+	bool pthread_;
+};
+
+/* print command */
+
+class printcmd final : public command {
+public:
+	virtual
+	~printcmd();
+
+	printcmd(
+		std::unique_ptr<passgraph> pgraph)
+	: pgraph_(std::move(pgraph))
+	{}
+
+	printcmd(const printcmd&) = delete;
+
+	printcmd(printcmd&&) = delete;
+
+	printcmd &
+	operator=(const printcmd&) = delete;
+
+	printcmd &
+	operator=(printcmd&&)	= delete;
+
+	virtual std::string
+	to_str() const override;
+
+	virtual void
+	run() const override;
+
+	static passgraph_node *
+	create(
+		passgraph * pgraph,
+		std::unique_ptr<passgraph> pg)
+	{
+		return passgraph_node::create(pgraph, std::make_unique<printcmd>(std::move(pg)));
+	}
+
+private:
+	std::unique_ptr<passgraph> pgraph_;
+};
+
+class m2rcmd final : public command {
+public:
+	virtual
+	~m2rcmd(){}
+
+	m2rcmd(
+		const jlm::filepath & ifile,
+		const jlm::filepath & ofile)
+	: ifile_(ifile),
+	  ofile_(ofile)
+	{}
+
+	virtual std::string
+	to_str() const override;
+
+	jlm::filepath
+	ofile() const;
+
+	virtual void
+	run() const override;
+
+	static passgraph_node *
+	create(
+		passgraph * pgraph,
+		const jlm::filepath & ifile,
+		const jlm::filepath & ofile)
+	{
+		std::unique_ptr<m2rcmd> cmd(new m2rcmd(ifile, ofile));
+		return passgraph_node::create(pgraph, std::move(cmd));
+	}
+
+private:
+	jlm::filepath ifile_;
+	jlm::filepath ofile_;
+};
+
+class mkdircmd final : public command {
+public:
+	virtual
+	~mkdircmd(){}
+
+    mkdircmd(
+		const jlm::filepath & path)
+	: path_(path)
+	{}
+
+	virtual std::string
+	to_str() const override;
+
+	virtual void
+	run() const override;
+
+	static passgraph_node *
+	create(
+		passgraph * pgraph,
+		const jlm::filepath & path)
+	{
+		std::unique_ptr<mkdircmd> cmd(new mkdircmd(path));
+		return passgraph_node::create(pgraph, std::move(cmd));
+	}
+
+private:
+	jlm::filepath path_;
+};
+
+class verilatorcmd final : public command {
+public:
+	virtual
+	~verilatorcmd(){}
+
+	verilatorcmd(
+			const jlm::filepath & vfile,
+			const std::vector<jlm::filepath> & lfiles,
+			const jlm::filepath & hfile,
+			const jlm::filepath & ofile,
+            const jlm::filepath & tmpfolder,
+			const std::vector<std::string> & Lpaths,
+			const std::vector<std::string> & libs)
+	: ofile_(ofile)
+	, vfile_(vfile)
+	, hfile_(hfile)
+	, tmpfolder_(tmpfolder)
+	, libs_(libs)
+	, lfiles_(lfiles)
+	, Lpaths_(Lpaths)
+	{}
+
+	virtual std::string
+	to_str() const override;
+
+	virtual void
+	run() const override;
+
+	inline const jlm::filepath &
+	vfile() const noexcept
+	{
+		return vfile_;
+	}
+
+	inline const jlm::filepath &
+	hfile() const noexcept
+	{
+		return hfile_;
+	}
+
+	inline const jlm::filepath &
+	ofile() const noexcept
+	{
+		return ofile_;
+	}
+
+	inline const std::vector<jlm::filepath> &
+	lfiles() const noexcept
+	{
+		return lfiles_;
+	}
+
+	static passgraph_node *
+	create(
+		passgraph * pgraph,
+		const jlm::filepath & vfile,
+		const std::vector<jlm::filepath> & lfiles,
+		const jlm::filepath & hfile,
+		const jlm::filepath & ofile,
+		const jlm::filepath & tmpfolder,
+		const std::vector<std::string> & Lpaths,
+		const std::vector<std::string> & libs)
+	{
+		std::unique_ptr<verilatorcmd> cmd(new verilatorcmd(vfile, lfiles, hfile, ofile, tmpfolder, Lpaths, libs));
+		return passgraph_node::create(pgraph, std::move(cmd));
+	}
+
+private:
+	jlm::filepath ofile_;
+	jlm::filepath vfile_;
+	jlm::filepath hfile_;
+	jlm::filepath tmpfolder_;
+	std::vector<std::string> libs_;
+	std::vector<jlm::filepath> lfiles_;
+	std::vector<std::string> Lpaths_;
+};
+
+class lllnkcmd final : public command {
+public:
+	virtual
+	~lllnkcmd(){}
+
+	lllnkcmd(
+		const std::vector<jlm::filepath> & ifiles,
+		const jlm::filepath & ofile)
+	: ofile_(ofile)
+	, ifiles_(ifiles)
+	{}
+
+	virtual std::string
+	to_str() const override;
+
+	virtual void
+	run() const override;
+
+	inline const jlm::filepath &
+	ofile() const noexcept
+	{
+		return ofile_;
+	}
+
+	inline const std::vector<jlm::filepath> &
+	ifiles() const noexcept
+	{
+		return ifiles_;
+	}
+
+	static passgraph_node *
+	create(
+		passgraph * pgraph,
+		const std::vector<jlm::filepath> & ifiles,
+		const jlm::filepath & ofile)
+	{
+		std::unique_ptr<lllnkcmd> cmd(new lllnkcmd(ifiles, ofile));
+		return passgraph_node::create(pgraph, std::move(cmd));
+	}
+
+private:
+	jlm::filepath ofile_;
+	std::vector<jlm::filepath> ifiles_;
+};
+
+class firrtlcmd final : public command {
+public:
+	virtual
+	~firrtlcmd(){}
+
+	firrtlcmd(
+		const jlm::filepath & ifile,
+		const jlm::filepath & ofile)
+	: ofile_(ofile)
+	, ifile_(ifile)
+	{}
+
+	virtual std::string
+	to_str() const override;
+
+	virtual void
+	run() const override;
+
+	inline const jlm::filepath &
+	ofile() const noexcept
+	{
+		return ofile_;
+	}
+
+	inline const jlm::filepath &
+	ifile() const noexcept
+	{
+		return ifile_;
+	}
+
+	static passgraph_node *
+	create(
+		passgraph * pgraph,
+		const jlm::filepath & ifile,
+		const jlm::filepath & ofile)
+	{
+		std::unique_ptr<firrtlcmd> cmd(new firrtlcmd(ifile, ofile));
+		return passgraph_node::create(pgraph, std::move(cmd));
+	}
+
+private:
+	jlm::filepath ofile_;
+	jlm::filepath ifile_;
+};
+
+std::basic_string<char>
+gcd();
+
+class hlscmd final : public command {
+public:
+	virtual
+	~hlscmd(){}
+
+	hlscmd(
+		const jlm::filepath & ifile,
+		const std::string & outfolder,
+	        const bool &circt)
+	: ifile_(ifile)
+	, outfolder_(outfolder)
+	, circt_(circt)
+	{}
+
+	virtual std::string
+	to_str() const override;
+
+	virtual void
+	run() const override;
+
+	inline const jlm::filepath
+	firfile() const noexcept
+	{
+		return jlm::filepath(outfolder_+"jlm_hls.fir");
+	}
+
+	inline const jlm::filepath
+	llfile() const noexcept
+	{
+		return jlm::filepath(outfolder_+"jlm_hls_rest.ll");
+	}
+
+	inline const jlm::filepath
+	harnessfile() const noexcept
+	{
+		return jlm::filepath(outfolder_+"jlm_hls_harness.cpp");
+	}
+
+	inline const jlm::filepath &
+	ifile() const noexcept
+	{
+		return ifile_;
+	}
+
+	static passgraph_node *
+	create(
+		passgraph * pgraph,
+		const jlm::filepath & ifile,
+		const std::string & outfolder,
+	        const bool &circt)
+	{
+		std::unique_ptr<hlscmd> cmd(new hlscmd(ifile, outfolder, circt));
+		return passgraph_node::create(pgraph, std::move(cmd));
+	}
+
+private:
+	jlm::filepath ifile_;
+	std::string outfolder_;
+	bool circt_;
+};
+
+class extractcmd final : public command {
+public:
+	virtual
+	~extractcmd(){}
+
+    extractcmd(
+		const jlm::filepath & ifile,
+		const std::string & function,
+		const std::string & outfolder)
+	: ifile_(ifile)
+	, function_(function)
+	, outfolder_(outfolder)
+	{}
+
+	virtual std::string
+	to_str() const override;
+
+	virtual void
+	run() const override;
+
+	inline const jlm::filepath
+	functionfile() const noexcept
+	{
+		return jlm::filepath(outfolder_+"jlm_hls_function.ll");
+	}
+
+	inline const jlm::filepath
+	llfile() const noexcept
+	{
+		return jlm::filepath(outfolder_+"jlm_hls_rest.ll");
+	}
+
+	inline const jlm::filepath &
+	ifile() const noexcept
+	{
+		return ifile_;
+	}
+	inline const std::string &
+	function() const noexcept
+	{
+		return function_;
+	}
+
+	static passgraph_node *
+	create(
+		passgraph * pgraph,
+		const jlm::filepath & ifile,
+		const std::string & function,
+		const std::string & outfolder)
+	{
+		std::unique_ptr<extractcmd> cmd(new extractcmd(ifile, function, outfolder));
+		return passgraph_node::create(pgraph, std::move(cmd));
+	}
+
+private:
+	jlm::filepath ifile_;
+	std::string function_;
+	std::string outfolder_;
+};
+
+}
+
+#endif
---
+++ jhls/src/command.cpp
@@ -0,0 +1,467 @@
+/*
+ * Copyright 2018 Nico Reißmann <nico.reissmann@gmail.com>
+ * See COPYING for terms of redistribution.
+ */
+
+#include <jlc/llvmpaths.hpp>
+#include <jhls/command.hpp>
+#include <jhls/toolpaths.hpp>
+#include <jlm/util/strfmt.hpp>
+
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <algorithm>
+#include <sys/stat.h>
+
+namespace jlm {
+
+
+/* parser command */
+
+static std::string
+create_prscmd_ofile(const std::string & ifile)
+{
+	return strfmt("tmp-", ifile, "-clang-out.ll");
+}
+
+prscmd::~prscmd()
+{}
+
+jlm::filepath
+prscmd::ofile() const
+{
+	auto f = ifile_.base();
+	return tmpfolder_.to_str()+create_prscmd_ofile(f);
+}
+
+std::string
+prscmd::replace_all(std::string str, const std::string& from, const std::string& to) {
+    size_t start_pos = 0;
+    while((start_pos = str.find(from, start_pos)) != std::string::npos) {
+        str.replace(start_pos, from.length(), to);
+        start_pos += to.length();
+    }
+    return str;
+}
+
+std::string
+prscmd::to_str() const
+{
+	auto f = ifile_.base();
+
+	std::string Ipaths;
+	for (const auto & Ipath : Ipaths_)
+		Ipaths += "-I" + Ipath + " ";
+
+	std::string Dmacros;
+	for (const auto & Dmacro : Dmacros_)
+		Dmacros += "-D" + Dmacro + " ";
+
+	std::string Wwarnings;
+	for (const auto & Wwarning : Wwarnings_)
+		Wwarnings += "-W" + Wwarning + " ";
+
+	std::string flags;
+	for (const auto & flag : flags_)
+		flags += "-f" + flag + " ";
+
+	std::string arguments;
+	if (verbose_)
+	  arguments += "-v ";
+
+	if (rdynamic_)
+	  arguments += "-rdynamic ";
+
+	if (suppress_)
+	  arguments += "-w ";
+
+	if (pthread_)
+	  arguments += "-pthread ";
+
+	if (MD_) {
+		arguments += "-MD ";
+		arguments += "-MF " + dependencyFile_.to_str() + " ";
+		arguments += "-MT " + mT_ + " ";
+	}
+
+	if (hls_) {
+		return strfmt(
+		clangpath.to_str() + " "
+		, arguments, " "
+		, Wwarnings, " "
+		, flags, " "
+		, std_ != standard::none ? "-std="+jlm::to_str(std_)+" " : ""
+		, replace_all(Dmacros, std::string("\""), std::string("\\\"")), " "
+		, Ipaths, " "
+		, "-S -emit-llvm -Xclang -disable-O0-optnone "
+		, "-o ", tmpfolder_.to_str()+create_prscmd_ofile(f), " "
+		, ifile_.to_str()
+		);
+	}
+
+	return strfmt(
+	  clangpath.to_str() + " "
+	, arguments, " "
+	, Wwarnings, " "
+	, flags, " "
+	, std_ != standard::none ? "-std="+jlm::to_str(std_)+" " : ""
+	, replace_all(Dmacros, std::string("\""), std::string("\\\"")), " "
+	, Ipaths, " "
+	, "-S -emit-llvm "
+	, "-o ", tmpfolder_.to_str()+create_prscmd_ofile(f), " "
+	, ifile_.to_str()
+	);
+}
+
+void
+prscmd::run() const
+{
+	if (system(to_str().c_str()))
+		exit(EXIT_FAILURE);
+}
+
+/* optimization command */
+
+static std::string
+create_optcmd_ofile(const std::string & ifile)
+{
+	return strfmt("tmp-", ifile, "-jlm-opt-out.ll");
+}
+
+optcmd::~optcmd()
+{}
+
+std::string
+optcmd::to_str() const
+{
+	auto f = ifile_.base();
+
+	std::string jlmopts;
+	for (const auto & jlmopt : jlmopts_)
+		jlmopts += "--" + jlmopt + " ";
+
+	/*
+		If a default optimization level has been specified (-O) and no specific jlm-options 
+		have been specified (-J) then use a default set of optimizations.
+	 */
+	if (jlmopts.empty()) {
+		/*
+			Only -O3 sets default optimizations
+		*/
+		if (ol_ == optlvl::O3) {
+			jlmopts  = "--iln --inv --red --dne --ivt --inv --dne --psh --inv --dne ";
+			jlmopts += "--red --cne --dne --pll --inv --dne --url --inv ";
+		}
+	}
+
+	return strfmt(
+	  "jlm-opt "
+	, "--llvm "
+	, jlmopts
+	, tmpfolder_.to_str(), create_prscmd_ofile(f), " > "
+	, tmpfolder_.to_str(), create_optcmd_ofile(f)
+	);
+}
+
+void
+optcmd::run() const
+{
+	if (system(to_str().c_str()))
+		exit(EXIT_FAILURE);
+}
+
+/* code generator command */
+
+cgencmd::~cgencmd()
+{}
+
+std::string
+cgencmd::to_str() const
+{
+	if (hls_) {
+		return strfmt(
+		llcpath.to_str() + " "
+		, "-", jlm::to_str(ol_), " "
+		, "--relocation-model=pic "
+		, "-filetype=obj "
+		, "-o ", ofile_.to_str()
+		, " ", ifile_.to_str()
+		);
+
+	}
+
+	return strfmt(
+	llcpath.to_str() + " "
+	, "-", jlm::to_str(ol_), " "
+	, "-filetype=obj "
+	, "-o ", ofile_.to_str()
+	, " ", tmpfolder_.to_str(), create_optcmd_ofile(ifile_.base())
+	);
+}
+
+void
+cgencmd::run() const
+{
+	if (system(to_str().c_str()))
+		exit(EXIT_FAILURE);
+}
+
+/* linker command */
+
+lnkcmd::~lnkcmd()
+{}
+
+std::string
+lnkcmd::to_str() const
+{
+	std::string ifiles;
+	for (const auto & ifile : ifiles_)
+		ifiles += ifile.to_str() + " ";
+
+	std::string Lpaths;
+	for (const auto & Lpath : Lpaths_)
+		Lpaths += "-L" + Lpath + " ";
+
+	std::string libs;
+	for (const auto & lib : libs_)
+		libs += "-l" + lib + " ";
+
+	std::string arguments;
+	if (pthread_)
+	  arguments += "-pthread ";
+
+	return strfmt(
+	  clangpath.to_str() + " "
+	, "-no-pie -O0 "
+        , arguments
+	, ifiles
+	, "-o ", ofile_.to_str(), " "
+	, Lpaths
+	, libs
+	);
+}
+
+void
+lnkcmd::run() const
+{
+	if (system(to_str().c_str()))
+		exit(EXIT_FAILURE);
+}
+
+/* print command */
+
+printcmd::~printcmd()
+{}
+
+std::string
+printcmd::to_str() const
+{
+	return "PRINTCMD";
+}
+
+void
+printcmd::run() const
+{
+	for (const auto & node : topsort(pgraph_.get())) {
+		if (node != pgraph_->entry() && node != pgraph_->exit())
+			std::cout << node->cmd().to_str() << "\n";
+	}
+}
+
+/* HLS */
+
+std::string
+lllnkcmd::to_str() const {
+
+	auto llvm_link = clangpath.path() + "llvm-link";
+	std::string ifiles;
+	for (const auto & ifile : ifiles_)
+		ifiles += ifile.to_str() + " ";
+	return strfmt(
+			llvm_link, " "
+			, "-S -v "
+			, "-o ", ofile_.to_str(), " "
+			, ifiles
+	);
+}
+
+void
+lllnkcmd::run() const {
+	if (system(to_str().c_str()))
+		exit(EXIT_FAILURE);
+}
+
+void
+hlscmd::run() const {
+	if (system(to_str().c_str()))
+		exit(EXIT_FAILURE);
+}
+
+std::string
+hlscmd::to_str() const {
+	if (circt_)
+		return strfmt(
+			      "jlm-hls"
+			      , " -o ", outfolder_
+			      , " --circt "
+			      , ifile().to_str()
+			      //, " --hls-file ", ifile().to_str()
+			      );
+	else
+		return strfmt(
+			      "jlm-hls"
+			      , " -o ", outfolder_, " "
+			      , ifile().to_str()
+			      //, " --hls-file ", ifile().to_str()
+			      );
+}
+
+void
+extractcmd::run() const {
+	if (system(to_str().c_str()))
+		exit(EXIT_FAILURE);
+}
+
+std::string
+extractcmd::to_str() const {
+        return strfmt(
+		      "jlm-hls"
+		      , " --extract"
+		      , " --hls-function ", function()
+		      , " -o ", outfolder_, " "
+		      , ifile().to_str()
+		      //, " --hls-file ", ifile().to_str()
+		      );
+}
+
+void
+firrtlcmd::run() const {
+	if (system(to_str().c_str()))
+		exit(EXIT_FAILURE);
+}
+
+std::string
+firrtlcmd::to_str() const {
+	return strfmt(
+		      firtoolpath.to_str()
+		      , " -format=fir --verilog "
+		      , ifile().to_str()
+		      , " > ", ofile().to_str()
+		      );
+}
+
+void
+verilatorcmd::run() const {
+	if (system(to_str().c_str()))
+		exit(EXIT_FAILURE);
+}
+
+std::string
+gcd() {
+	char tmp[256];
+	getcwd(tmp, 256);
+	auto cd = std::string(tmp);
+	return cd;
+}
+
+std::string
+verilatorcmd::to_str() const {
+	std::string lfiles;
+	for (const auto & ifile : lfiles_)
+		lfiles += ifile.to_str() + " ";
+
+	std::string Lpaths;
+	for (const auto & Lpath : Lpaths_)
+		Lpaths += "-L" + Lpath + " ";
+
+	std::string libs;
+	for (const auto & lib : libs_)
+		libs += "-l" + lib + " ";
+
+	std::string cflags;
+//	if(!libs.empty()||!Lpaths.empty()){
+	cflags = " -CFLAGS \"" + libs + Lpaths + " -fPIC\"";
+//	}
+
+	std::string ofile = ofile_.to_str();
+	if(ofile.at(0)!='/'){
+		ofile = gcd() + "/" + ofile;
+	}
+        std::string verilator_root;
+        if(verilatorrootpath.to_str().size()){
+		verilator_root = strfmt(
+					"VERILATOR_ROOT="
+					, verilatorrootpath.to_str()
+					, " "
+					);
+        }
+	return strfmt(
+		      verilator_root
+		      ,verilatorpath.to_str()
+		      , " --cc"
+		      , " --build"
+		      , " --exe"
+#ifndef HLS_USE_VCD
+		      , " --trace-fst"
+#else
+		      , " --trace"
+#endif
+		      , " -Wno-WIDTH" //divisions otherwise cause errors
+		      , " -j"
+		      , " -Mdir ", tmpfolder_.to_str(), "verilator/"
+		      , " -MAKEFLAGS CXX=g++"
+		      , " -CFLAGS -g" // TODO: switch for this
+		      , " --assert"
+		      , cflags
+		      , " -o ", ofile
+		      , " ", vfile().to_str()
+		      , " ", hfile().to_str()
+		      , " ", lfiles
+		      );
+}
+
+jlm::filepath
+m2rcmd::ofile() const
+{
+	return ofile_;
+}
+
+std::string
+m2rcmd::to_str() const
+{
+	auto opt = clangpath.path() + "opt";
+
+	return strfmt(
+		      opt + " "
+		      , "-mem2reg -S "
+		      , "-o ", ofile().to_str(), " "
+		      , ifile_.to_str()
+		      );
+}
+
+void
+m2rcmd::run() const
+{
+	if (system(to_str().c_str()))
+		exit(EXIT_FAILURE);
+}
+
+std::string
+mkdircmd::to_str() const
+{
+	return strfmt(
+		      "mkdir "
+		      , path_.to_str()
+		      );
+}
+
+void
+mkdircmd::run() const
+{
+	if (mkdir(path_.to_str().c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH)!=0)
+		throw jlm::error("mkdir failed: "+path_.to_str());
+}
+
+} // jlm
