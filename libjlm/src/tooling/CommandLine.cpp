867b9e40fe1145c50cee59cccb518715b4ed0209
Nico Reissmann <nico.reissmann@gmail.com>
2022-05-29 09:29:03 +0200

    libjlm/tooling: add CommandLineOptions interface

---
+++ libjlm/src/tooling/CommandLine.cpp
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2022 Nico Reißmann <nico.reissmann@gmail.com>
+ * See COPYING for terms of redistribution.
+ */
+
+#include <jlm/tooling/CommandLine.hpp>
+
+namespace jlm
+{
+
+CommandLineOptions::~CommandLineOptions()
+= default;
+
+}
