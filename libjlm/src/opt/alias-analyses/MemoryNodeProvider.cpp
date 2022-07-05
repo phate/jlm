25d8af2faf80b9b80b713cc4ebfb8f20850db49a
Nico Reissmann <nico.reissmann@gmail.com>
2022-07-05 14:56:13 +0200

    libjlm/aa: add MemoryNodeProvider interface

---
+++ libjlm/src/opt/alias-analyses/MemoryNodeProvider.cpp
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2022 Nico Reißmann <nico.reissmann@gmail.com>
+ * See COPYING for terms of redistribution.
+ */
+
+#include <jlm/opt/alias-analyses/MemoryNodeProvider.hpp>
+
+namespace jlm::aa
+{
+
+MemoryNodeProvider::~MemoryNodeProvider() noexcept
+= default;
+
+}
