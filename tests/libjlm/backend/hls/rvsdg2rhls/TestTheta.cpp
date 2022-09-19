d7831d41ac7c9c09fcc4ddd8da6f6eb5b6651262
Magnus Sjalander <work@sjalander.com>
2022-09-19 12:00:51 +0200

    tests: Added units tests for HLS

---
+++ tests/libjlm/backend/hls/rvsdg2rhls/TestTheta.cpp
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2022 Magnus Sjalander <work@sjalander.com>
+ * See COPYING for terms of redistribution.
+ */
+
+#include "test-registry.hpp"
+
+#include <jive/view.hpp>
+#include <jive/rvsdg/theta.hpp>
+
+#include <jlm/backend/hls/rvsdg2rhls/rvsdg2rhls.hpp>
+#include <jlm/ir/operators.hpp>
+#include <jlm/ir/hls/hls.hpp>
+#include <jlm/ir/RvsdgModule.hpp>
+
+static inline void
+TestUnknownBoundaries()
+{
+	using namespace jlm;
+
+	auto b32 = jive::bit32;
+	FunctionType ft({&b32, &b32, &b32}, {&b32, &b32, &b32});
+
+	RvsdgModule rm(filepath(""), "", "");
+	auto nf = rm.Rvsdg().node_normal_form(typeid(jive::operation));
+	nf->set_mutable(false);
+
+	/* Setup graph */
+
+	auto lambda = lambda::node::create(rm.Rvsdg().root(), ft, "f", linkage::external_linkage);
+
+	jive::bitult_op ult(32);
+	jive::bitsgt_op sgt(32);
+	jive::bitadd_op add(32);
+	jive::bitsub_op sub(32);
+
+	auto theta = jive::theta_node::create(lambda->subregion());
+	auto subregion = theta->subregion();
+	auto idv = theta->add_loopvar(lambda->fctargument(0));
+	auto lvs = theta->add_loopvar(lambda->fctargument(1));
+	auto lve = theta->add_loopvar(lambda->fctargument(2));
+
+	auto arm = jive::simple_node::create_normalized(subregion, add, {idv->argument(), lvs->argument()})[0];
+	auto cmp = jive::simple_node::create_normalized(subregion, ult, {arm, lve->argument()})[0];
+	auto match = jive::match(1, {{1, 1}}, 0, 2, cmp);
+
+	idv->result()->divert_to(arm);
+	theta->set_predicate(match);
+
+	auto f = lambda->finalize({theta->output(0), theta->output(1), theta->output(2)});
+	rm.Rvsdg().add_export(f, {f->type(), ""});
+
+	jive::view(rm.Rvsdg(), stdout);
+
+	/* Convert graph to RHLS */
+
+	hls::rvsdg2rhls(rm);
+	jive::view(rm.Rvsdg(), stdout);
+
+	/* Verify graph */
+
+	// The lambda node has been replaced so need to start from
+	// the root of the graph
+	auto root = rm.Rvsdg().root();
+	auto newLambda = dynamic_cast<const jlm::lambda::node *>(root->nodes.begin().ptr());
+	assert(jive::contains<jlm::hls::loop_op>(newLambda->subregion(), true));
+}
+
+static int
+Test()
+{
+	TestUnknownBoundaries();
+
+	return 0;
+}
+
+JLM_UNIT_TEST_REGISTER("libjlm/backend/hls/rvsdg2rhls/TestTheta", Test)
