3869fdb863e929f7ac7ad08b91d2605eff0b1c84
Nico Reissmann <nico.reissmann@gmail.com>
2022-12-12 07:32:41 +0100

    libjlm/aa: add AgnosticMemoryNodeProvisioning class

--- libjlm/src/opt/alias-analyses/AgnosticMemoryNodeProvider.cpp
+++ libjlm/src/opt/alias-analyses/AgnosticMemoryNodeProvider.cpp
@@ -8,34 +8,123 @@
 namespace jlm::aa
 {
 
+/** \brief Memory Node Provisioning of agnostic memory node provider
+ *
+ */
+class AgnosticMemoryNodeProvisioning final : public MemoryNodeProvisioning
+{
+  explicit
+  AgnosticMemoryNodeProvisioning(const PointsToGraph & pointsToGraph)
+    : PointsToGraph_(pointsToGraph)
+  {}
+
+public:
+  AgnosticMemoryNodeProvisioning(const AgnosticMemoryNodeProvisioning&) = delete;
+
+  AgnosticMemoryNodeProvisioning(AgnosticMemoryNodeProvisioning&&) = delete;
+
+  AgnosticMemoryNodeProvisioning &
+  operator=(const AgnosticMemoryNodeProvisioning&) = delete;
+
+  AgnosticMemoryNodeProvisioning &
+  operator=(AgnosticMemoryNodeProvisioning&&) = delete;
+
+  [[nodiscard]] const PointsToGraph &
+  GetPointsToGraph() const noexcept override
+  {
+    return PointsToGraph_;
+  }
+
+  [[nodiscard]] const HashSet<const PointsToGraph::MemoryNode*> &
+  GetRegionEntryNodes(const jive::region & region) const override
+  {
+    return MemoryNodes_;
+  }
+
+  [[nodiscard]] const HashSet<const PointsToGraph::MemoryNode*> &
+  GetRegionExitNodes(const jive::region & region) const override
+  {
+    return MemoryNodes_;
+  }
+
+  [[nodiscard]] const HashSet<const PointsToGraph::MemoryNode*> &
+  GetCallEntryNodes(const CallNode & callNode) const override
+  {
+    return MemoryNodes_;
+  }
+
+  [[nodiscard]] const HashSet<const PointsToGraph::MemoryNode*> &
+  GetCallExitNodes(const CallNode & callNode) const override
+  {
+    return MemoryNodes_;
+  }
+
+  [[nodiscard]] HashSet<const PointsToGraph::MemoryNode*>
+  GetOutputNodes(const jive::output & output) const override
+  {
+    JLM_ASSERT(is<PointerType>(output.type()));
+    auto & registerNode = PointsToGraph_.GetRegisterNode(output);
+
+    HashSet<const PointsToGraph::MemoryNode*> memoryNodes;
+    for (auto & memoryNode : registerNode.Targets())
+      memoryNodes.Insert(&memoryNode);
+
+    return memoryNodes;
+  }
+
+  void
+  SetMemoryNodes(HashSet<const PointsToGraph::MemoryNode*> memoryNodes)
+  {
+    MemoryNodes_ = std::move(memoryNodes);
+  }
+
+  static std::unique_ptr<AgnosticMemoryNodeProvisioning>
+  Create(const PointsToGraph & pointsToGraph)
+  {
+    return std::unique_ptr<AgnosticMemoryNodeProvisioning>(
+      new AgnosticMemoryNodeProvisioning(pointsToGraph));
+  }
+
+private:
+  const PointsToGraph & PointsToGraph_;
+  HashSet<const PointsToGraph::MemoryNode*> MemoryNodes_;
+};
+
 AgnosticMemoryNodeProvider::AgnosticMemoryNodeProvider(const PointsToGraph & pointsToGraph)
-  : PointsToGraph_(pointsToGraph)
+  : Provisioning_(AgnosticMemoryNodeProvisioning::Create(pointsToGraph))
 {}
+
+AgnosticMemoryNodeProvider::~AgnosticMemoryNodeProvider()
+= default;
 
 void
 AgnosticMemoryNodeProvider::ProvisionMemoryNodes(
   const RvsdgModule&,
   StatisticsCollector& statisticsCollector)
 {
-  auto statistics = Statistics::Create(statisticsCollector, PointsToGraph_);
+  auto & pointsToGraph = Provisioning_->GetPointsToGraph();
+
+  auto statistics = Statistics::Create(statisticsCollector, pointsToGraph);
 
   statistics->StartCollecting();
-  for (auto & allocaNode : PointsToGraph_.AllocaNodes())
-    MemoryNodes_.Insert(&allocaNode);
+  HashSet<const PointsToGraph::MemoryNode*> memoryNodes;
+  for (auto & allocaNode : pointsToGraph.AllocaNodes())
+    memoryNodes.Insert(&allocaNode);
 
-  for (auto & deltaNode : PointsToGraph_.DeltaNodes())
-    MemoryNodes_.Insert(&deltaNode);
+  for (auto & deltaNode : pointsToGraph.DeltaNodes())
+    memoryNodes.Insert(&deltaNode);
 
-  for (auto & lambdaNode : PointsToGraph_.LambdaNodes())
-    MemoryNodes_.Insert(&lambdaNode);
+  for (auto & lambdaNode : pointsToGraph.LambdaNodes())
+    memoryNodes.Insert(&lambdaNode);
 
-  for (auto & mallocNode : PointsToGraph_.MallocNodes())
-    MemoryNodes_.Insert(&mallocNode);
+  for (auto & mallocNode : pointsToGraph.MallocNodes())
+    memoryNodes.Insert(&mallocNode);
 
-  for (auto & importNode : PointsToGraph_.ImportNodes())
-    MemoryNodes_.Insert(&importNode);
+  for (auto & importNode : pointsToGraph.ImportNodes())
+    memoryNodes.Insert(&importNode);
 
-  MemoryNodes_.Insert(&PointsToGraph_.GetExternalMemoryNode());
+  memoryNodes.Insert(&pointsToGraph.GetExternalMemoryNode());
+  Provisioning_->SetMemoryNodes(std::move(memoryNodes));
   statistics->StopCollecting();
 
   statisticsCollector.CollectDemandedStatistics(std::move(statistics));
@@ -65,38 +154,38 @@ AgnosticMemoryNodeProvider::Create(
 const PointsToGraph &
 AgnosticMemoryNodeProvider::GetPointsToGraph() const noexcept
 {
-  return PointsToGraph_;
+  return Provisioning_->GetPointsToGraph();
 }
 
 const HashSet<const PointsToGraph::MemoryNode*> &
-AgnosticMemoryNodeProvider::GetRegionEntryNodes(const jive::region&) const
+AgnosticMemoryNodeProvider::GetRegionEntryNodes(const jive::region & region) const
 {
-  return MemoryNodes_;
+  return Provisioning_->GetRegionEntryNodes(region);
 }
 
 const HashSet<const PointsToGraph::MemoryNode*> &
-AgnosticMemoryNodeProvider::GetRegionExitNodes(const jive::region&) const
+AgnosticMemoryNodeProvider::GetRegionExitNodes(const jive::region & region) const
 {
-  return MemoryNodes_;
+  return Provisioning_->GetRegionExitNodes(region);
 }
 
 const HashSet<const PointsToGraph::MemoryNode*> &
-AgnosticMemoryNodeProvider::GetCallEntryNodes(const CallNode&) const
+AgnosticMemoryNodeProvider::GetCallEntryNodes(const CallNode & callNode) const
 {
-  return MemoryNodes_;
+  return Provisioning_->GetCallEntryNodes(callNode);
 }
 
 const HashSet<const PointsToGraph::MemoryNode*> &
-AgnosticMemoryNodeProvider::GetCallExitNodes(const CallNode&) const
+AgnosticMemoryNodeProvider::GetCallExitNodes(const CallNode & callNode) const
 {
-  return MemoryNodes_;
+  return Provisioning_->GetCallExitNodes(callNode);
 }
 
 HashSet<const PointsToGraph::MemoryNode*>
 AgnosticMemoryNodeProvider::GetOutputNodes(const jive::output & output) const
 {
   JLM_ASSERT(is<PointerType>(output.type()));
-  auto & registerNode = PointsToGraph_.GetRegisterNode(output);
+  auto & registerNode = Provisioning_->GetPointsToGraph().GetRegisterNode(output);
 
   HashSet<const PointsToGraph::MemoryNode*> memoryNodes;
   for (auto & memoryNode : registerNode.Targets())
