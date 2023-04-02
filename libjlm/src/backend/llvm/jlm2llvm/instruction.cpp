86d4bd84ddcb83e3f0bcc6410ccb197cd57906dc
Nico Reissmann <nico.reissmann@gmail.com>
2023-04-02 20:40:49 +0200

    libjlm/backend: avoid PointerType::GetElementType() method invocations in jlm-llvm pass

--- libjlm/src/backend/llvm/jlm2llvm/instruction.cpp
+++ libjlm/src/backend/llvm/jlm2llvm/instruction.cpp
@@ -286,10 +286,10 @@ convert(
 	llvm::IRBuilder<> & builder,
 	context & ctx)
 {
-  auto type = convert_type(operation.GetPointerType().GetElementType(), ctx);
-	auto loadInstruction = builder.CreateLoad(type, ctx.value(args[0]));
-	loadInstruction->setAlignment(llvm::Align(operation.GetAlignment()));
-	return loadInstruction;
+    auto type = convert_type(operation.GetLoadedType(), ctx);
+    auto loadInstruction = builder.CreateLoad(type, ctx.value(args[0]));
+    loadInstruction->setAlignment(llvm::Align(operation.GetAlignment()));
+    return loadInstruction;
 }
 
 static inline llvm::Value *
