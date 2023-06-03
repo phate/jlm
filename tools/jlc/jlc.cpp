34a79d874f0d468b321fe1db0a5c19d107c0ada7
Nico Reissmann <nico.reissmann@gmail.com>
2023-06-03 10:50:01 +0200

    catch CommandLineParser exceptions in jlc

--- tools/jlc/jlc.cpp
+++ tools/jlc/jlc.cpp
@@ -12,9 +12,18 @@ main(int argc, char ** argv)
   using namespace jlm::tooling;
 
   JlcCommandLineParser commandLineParser;
-  auto & commandLineOptions = commandLineParser.ParseCommandLineArguments(argc, argv);
+  const JlcCommandLineOptions* commandLineOptions;
+  try
+  {
+    commandLineOptions = &commandLineParser.ParseCommandLineArguments(argc, argv);
+  }
+  catch(const CommandLineParser::Exception& e)
+  {
+    std::cerr << e.what() << std::endl;
+    exit(EXIT_FAILURE);
+  }
 
-  auto commandGraph = JlcCommandGraphGenerator::Generate(commandLineOptions);
+  auto commandGraph = JlcCommandGraphGenerator::Generate(*commandLineOptions);
   commandGraph->Run();
 
   return 0;
