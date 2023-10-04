c7654bd64bfca498b689fe9bcde4453daeb11ad6
Nico Reissmann <nico.reissmann@gmail.com>
2023-10-04 06:11:54 +0200

    Add documentation to RedirectUnknownMemoryNodeSources

--- jlm/llvm/opt/alias-analyses/Steensgaard.hpp
+++ jlm/llvm/opt/alias-analyses/Steensgaard.hpp
@@ -118,6 +118,24 @@ private:
   static std::unique_ptr<PointsToGraph>
   ConstructPointsToGraph(const LocationSet & locationSets);
 
+  /**
+   * Resolves all points-to graph nodes that were marked throughout the analysis as pointing towards unknown memory
+   * locations, i.e., we have no idea where these marked nodes point to. The resolving of these nodes simply happens by
+   * adding edges from them to \b all existing points-to graph memory nodes. Note, that this strategy of resolving such
+   * nodes is extremely expensive for points-to graphs with a lot of nodes marked as pointing towards unknown memory
+   * locations as it leads to a quadratic number of edges.
+   *
+   * FIXME: We can do better than resolving these nodes by simply adding edges to all points-to graph memory nodes.
+   * The requirement is that the users of these marked nodes, i.e,, stores, loads, calls, etc., need to stay in the
+   * same place as dictated by the memory state given by the original program. In other words, a store referencing an
+   * address that is marked as unknown is not allowed to move before or after any other memory referencing operation.
+   * However, this does not necessarily require that the address needs to point to all points-to graph memory nodes in
+   * order to fulfill this requirement, it is just the simplest to go about it. We simply speculate on the fact that
+   * unknown marked points-to graph nodes are rare. If this turns out to be false, then we might want to invest in a
+   * better strategy.
+   *
+   * @param pointsToGraph The PointsToGraph constructed by ConstructPointsToGraph.
+   */
   static void
   RedirectUnknownMemoryNodeSources(PointsToGraph & pointsToGraph);
 
