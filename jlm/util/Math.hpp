6a142d392fc702e1b56461d750fd9058d7c32d61
haved <krogstie.havard@gmail.com>
2023-10-24 10:17:43 +0200

    add tests of the bit-width operations in util/Math.cpp

--- jlm/util/Math.hpp
+++ jlm/util/Math.hpp
@@ -12,7 +12,8 @@ namespace jlm::util
 {
 
 /**
- * Log2 for integers, rounding down when value is not a perfect power of two
+ * Log2 for integers, rounding down when value is not a perfect power of two.
+ * Any value less than 1 becomes -1.
  *
  * Examples:
  * Log2floor(4) = 2
@@ -20,17 +21,18 @@ namespace jlm::util
  * Log2floor(8) = 3
  */
 template<class T>
-static inline constexpr int Log2floor(T value) {
-  if (value <= 1)
-    return 0;
+static inline constexpr int Log2Floor(T value) {
+  if (value < 1)
+    return -1;
 
-  return 1 + Log2floor(value >> 1);
+  return 1 + Log2Floor(value >> 1);
 }
 
 /**
  * The number of bits needed to hold the given value
  *
  * Examples:
+ * BitsRequiredToRepresent(0)     = 0
  * BitsRequiredToRepresent(0b1)   = 1
  * BitsRequiredToRepresent(0b10)  = 2
  * BitsRequiredToRepresent(0b100) = 3
@@ -38,21 +40,23 @@ static inline constexpr int Log2floor(T value) {
  */
 template<class T>
 static inline constexpr int BitsRequiredToRepresent(T value) {
-  return Log2floor(value) + 1;
+  using UnsignedT = std::make_unsigned_t<T>;
+  return Log2Floor(static_cast<UnsignedT>(value)) + 1;
 }
 
 /**
  * Calculates the number of bits needed to hold the integer representation of all enum values.
- * Takes a sentinel end value as parameter, requires all other enum values to have a lower integer value.
+ * @param endValue the largest enum value, requires all other enum values to have a lower integer value.
  */
 template<class T>
-static inline constexpr int BitWidthOfEnum(T sentinelEndValue) {
+static inline constexpr int BitWidthOfEnum(T endValue) {
   static_assert(std::is_enum<T>::value, "BitWidthOfEnum only takes enums");
 
   using UnderlyingT = std::underlying_type_t<T>;
 
-  // To appease gcc warnings, the returned bit width is large enough to hold the sentinel value as well
-  return BitsRequiredToRepresent(static_cast<UnderlyingT>(sentinelEndValue));
+  // To appease gcc warnings, the returned bit width is large enough to hold the endValue as well,
+  // even if it is just a sentinel COUNT value
+  return BitsRequiredToRepresent(static_cast<UnderlyingT>(endValue));
 }
 
 }
