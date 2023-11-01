72fdcd4b5b458fcc495bd095652fab8f986b46c5
Nico Reissmann <nico.reissmann@gmail.com>
2023-11-01 06:20:06 +0100

    Add structural node unit test

---
+++ tests/jlm/rvsdg/TestStructuralNode.cpp
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2023 Nico Reißmann <nico.reissmann@gmail.com>
+ * See COPYING for terms of redistribution.
+ */
+
+#include <test-registry.hpp>
+#include <test-operation.hpp>
+#include <test-types.hpp>
+
+#include <cassert>
+
+static void
+TestOutputRemoval()
+{
+  using namespace jlm;
+
+  // Arrange
+  rvsdg::graph rvsdg;
+  tests::valuetype valueType;
+
+  auto structuralNode = tests::structural_node::create(rvsdg.root(), 1);
+  auto output0 = rvsdg::structural_output::create(structuralNode, {valueType});
+  auto output1 = rvsdg::structural_output::create(structuralNode, {valueType});
+  auto output2 = rvsdg::structural_output::create(structuralNode, {valueType});
+  auto output3 = rvsdg::structural_output::create(structuralNode, {valueType});
+  auto output4 = rvsdg::structural_output::create(structuralNode, {valueType});
+
+  // Act & Assert
+  assert(structuralNode->noutputs() == 5);
+  assert(output0->index() == 0);
+  assert(output1->index() == 1);
+  assert(output2->index() == 2);
+  assert(output3->index() == 3);
+  assert(output4->index() == 4);
+
+  structuralNode->remove_output(2);
+  assert(structuralNode->noutputs() == 4);
+  assert(output0->index() == 0);
+  assert(output1->index() == 1);
+  assert(output3->index() == 2);
+  assert(output4->index() == 3);
+
+  structuralNode->remove_output(3);
+  assert(structuralNode->noutputs() == 3);
+  assert(output0->index() == 0);
+  assert(output1->index() == 1);
+  assert(output3->index() == 2);
+
+}
+
+static int
+TestStructuralNode()
+{
+  TestOutputRemoval();
+
+  return 0;
+}
+
+JLM_UNIT_TEST_REGISTER("jlm/rvsdg/TestStructuralNode", TestStructuralNode)
