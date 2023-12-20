de30ebaeb19b02295ff4446200ff7fce73ddcab9
Nico Reissmann <nico.reissmann@gmail.com>
2023-12-20 05:40:19 +0100

    Add unit test

--- tests/jlm/util/TestFile.cpp
+++ tests/jlm/util/TestFile.cpp
@@ -9,6 +9,26 @@
 
 #include <cassert>
 
+// FIXME: This was shamelessly copied from util/file.hpp.
+// We should introduce our own string wrapper where we can have such methods.
+static std::string
+CreateRandomString(std::size_t length)
+{
+  const std::string characterSet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+  std::random_device random_device;
+  std::mt19937 generator(random_device());
+  std::uniform_int_distribution<> distribution(0, characterSet.size() - 1);
+
+  std::string result;
+  for (std::size_t i = 0; i < length; ++i)
+  {
+    result += characterSet[distribution(generator)];
+  }
+
+  return result;
+}
+
 static void
 TestFilePathMethods()
 {
@@ -21,10 +41,25 @@ TestFilePathMethods()
   assert(f.path() == "/tmp/");
 }
 
+static void
+TestCreateUniqueFileName()
+{
+  // Arrange
+  auto randomString = CreateRandomString(6);
+  auto tmpDirectory = std::filesystem::temp_directory_path().string() + "/" + randomString;
+
+  // Act
+  auto filePath = jlm::util::filepath::CreateUniqueFileName(tmpDirectory, "myPrefix", "mySuffix");
+
+  // Assert
+  assert(filePath.path() == (tmpDirectory + "/"));
+}
+
 static int
 TestFile()
 {
   TestFilePathMethods();
+  TestCreateUniqueFileName();
 
   return 0;
 }
