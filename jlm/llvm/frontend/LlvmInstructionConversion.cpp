561005789845a722eb68caf5568050fe8c18ee85
Nico Reissmann <nico.reissmann@gmail.com>
2023-12-23 06:20:26 +0100

    fix frontend bug

--- jlm/llvm/frontend/LlvmInstructionConversion.cpp
+++ jlm/llvm/frontend/LlvmInstructionConversion.cpp
@@ -682,7 +682,10 @@ convert_free_call(const ::llvm::CallInst * i, tacsvector_t & tacs, context & ctx
   auto pointer = ConvertValue(i->getArgOperand(0), tacs, ctx);
 
   tacs.push_back(FreeOperation::Create(pointer, { memstate }, iostate));
-  tacs.push_back(assignment_op::create(tacs.back()->result(0), memstate));
+  auto & freeThreeAddressCode = *tacs.back().get();
+
+  tacs.push_back(assignment_op::create(freeThreeAddressCode.result(0), memstate));
+  tacs.push_back(assignment_op::create(freeThreeAddressCode.result(1), iostate));
 
   return nullptr;
 }
