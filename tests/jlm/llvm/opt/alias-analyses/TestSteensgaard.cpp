b4f02a45d4c66ac1468f043ce0b5a0b837384c92
Nico Reissmann <nico.reissmann@gmail.com>
2024-02-01 06:26:30 +0100

    Fix handling of points-to flags for memcpy in Steensgaard (#373)
    1. Fixes the handling of flags for memcpy in Steensgaard. The method
    ignored flags so far.
    2. Added a unit test that exposes the problem
    3. Some minor adjustments for debugging Steensgaard
    
    Closes #334

--- tests/jlm/llvm/opt/alias-analyses/TestSteensgaard.cpp
+++ tests/jlm/llvm/opt/alias-analyses/TestSteensgaard.cpp
@@ -9,6 +9,7 @@
 
 #include <jlm/llvm/opt/alias-analyses/PointsToGraph.hpp>
 #include <jlm/llvm/opt/alias-analyses/Steensgaard.hpp>
+#include <jlm/rvsdg/view.hpp>
 #include <jlm/util/Statistics.hpp>
 
 static std::unique_ptr<jlm::llvm::aa::PointsToGraph>
@@ -1064,8 +1065,8 @@ TestMemcpy()
     auto lambdaF = &pointsToGraph.GetLambdaNode(test.LambdaF());
     auto lambdaG = &pointsToGraph.GetLambdaNode(test.LambdaG());
 
-    assertTargets(memCpyDest, { globalArray });
-    assertTargets(memCpySrc, { localArray });
+    assertTargets(memCpyDest, { globalArray, localArray });
+    assertTargets(memCpySrc, { localArray, globalArray });
 
     jlm::util::HashSet<const jlm::llvm::aa::PointsToGraph::MemoryNode *> expectedEscapedMemoryNodes(
         { globalArray, localArray, lambdaF, lambdaG });
@@ -1073,13 +1074,14 @@ TestMemcpy()
   };
 
   jlm::tests::MemcpyTest test;
-  // jlm::rvsdg::view(test.graph().root(), stdout);
+  std::unordered_map<const jlm::rvsdg::output *, std::string> outputMap;
+  std::cout << jlm::rvsdg::view(test.graph().root(), outputMap) << std::flush;
 
   /*
    * Act
    */
   auto pointsToGraph = RunSteensgaard(test.module());
-  // std::cout << jlm::llvm::aa::PointsToGraph::ToDot(*pointsToGraph);
+  std::cout << jlm::llvm::aa::PointsToGraph::ToDot(*pointsToGraph, outputMap);
 
   /*
    * Assert
@@ -1122,6 +1124,39 @@ TestMemcpy2()
 
   assertTargets(memcpyOperand0, { &lambdaFNode, &externalMemoryNode });
   assertTargets(memcpyOperand1, { &lambdaFNode, &externalMemoryNode });
+}
+
+static void
+TestMemcpy3()
+{
+  // Arrange
+  jlm::tests::MemcpyTest3 test;
+  std::unordered_map<const jlm::rvsdg::output *, std::string> outputMap;
+  std::cout << jlm::rvsdg::view(test.graph().root(), outputMap) << std::flush;
+
+  // Act
+  auto pointsToGraph = RunSteensgaard(test.module());
+  std::cout << jlm::llvm::aa::PointsToGraph::ToDot(*pointsToGraph, outputMap) << std::flush;
+
+  // Assert
+  assert(pointsToGraph->NumRegisterNodes() == 4);
+  assert(pointsToGraph->NumLambdaNodes() == 1);
+  assert(pointsToGraph->NumAllocaNodes() == 1);
+
+  auto & lambdaNode = pointsToGraph->GetLambdaNode(test.Lambda());
+  auto & lambdaArgument0 = pointsToGraph->GetRegisterNode(*test.Lambda().fctargument(0));
+
+  auto & allocaNode = pointsToGraph->GetAllocaNode(test.Alloca());
+
+  auto & memcpyOperand0 = pointsToGraph->GetRegisterNode(*test.Memcpy().input(0)->origin());
+  auto & memcpyOperand1 = pointsToGraph->GetRegisterNode(*test.Memcpy().input(1)->origin());
+
+  auto & externalMemoryNode = pointsToGraph->GetExternalMemoryNode();
+
+  assertTargets(lambdaArgument0, { &lambdaNode, &allocaNode, &externalMemoryNode });
+
+  assertTargets(memcpyOperand0, { &lambdaNode, &allocaNode, &externalMemoryNode });
+  assertTargets(memcpyOperand1, { &allocaNode });
 }
 
 static void
@@ -1226,6 +1261,8 @@ TestSteensgaardAnalysis()
 
   TestMemcpy();
   TestMemcpy2();
+
+  TestMemcpy3();
 
   TestLinkedList();
 
