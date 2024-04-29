1b6f8a57b19e1af043ad196aad12013859b44d0e
Nico Reissmann <nico.reissmann@gmail.com>
2024-04-29 07:26:14 +0200

    add frontend support

---
+++ tests/jlm/llvm/frontend/llvm/StoreTests.cpp
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2024 Nico Reißmann <nico.reissmann@gmail.com>
+ * See COPYING for terms of redistribution.
+ */
+
+#include <test-registry.hpp>
+#include <test-util.hpp>
+
+#include <jlm/llvm/frontend/LlvmModuleConversion.hpp>
+#include <jlm/llvm/ir/operators/operators.hpp>
+#include <jlm/llvm/ir/operators/store.hpp>
+#include <jlm/llvm/ir/operators/StoreVolatile.hpp>
+#include <jlm/llvm/ir/print.hpp>
+
+#include <llvm/IR/BasicBlock.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/Module.h>
+
+static int
+StoreConversion()
+{
+  using namespace llvm;
+
+  // Arrange
+  llvm::LLVMContext context;
+  std::unique_ptr<Module> llvmModule(new Module("module", context));
+
+  auto int64Type = Type::getInt64Ty(context);
+  auto pointerType = Type::getInt64PtrTy(context);
+  auto voidType = Type::getVoidTy(context);
+
+  auto functionType =
+      FunctionType::get(voidType, ArrayRef<Type *>({ int64Type, pointerType }), false);
+  auto function =
+      Function::Create(functionType, GlobalValue::ExternalLinkage, "f", llvmModule.get());
+  auto valueArgument = function->getArg(0);
+  auto addressArgument = function->getArg(1);
+
+  auto basicBlock = BasicBlock::Create(context, "BasicBlock", function);
+
+  IRBuilder<> builder(basicBlock);
+  builder.CreateStore(valueArgument, addressArgument, true);
+  builder.CreateStore(valueArgument, addressArgument, false);
+  builder.CreateStore(valueArgument, addressArgument, true);
+  builder.CreateRetVoid();
+
+  jlm::tests::print(*llvmModule);
+
+  // Act
+  auto ipgModule = jlm::llvm::ConvertLlvmModule(*llvmModule);
+  print(*ipgModule, stdout);
+
+  // Assert
+  {
+    using namespace jlm::llvm;
+
+    auto controlFlowGraph =
+        dynamic_cast<const function_node *>(ipgModule->ipgraph().find("f"))->cfg();
+    auto basicBlock =
+        dynamic_cast<const basic_block *>(controlFlowGraph->entry()->outedge(0)->sink());
+
+    size_t numStoreThreeAddressCodes = 0;
+    size_t numStoreVolatileThreeAddressCodes = 0;
+    for (auto it = basicBlock->begin(); it != basicBlock->end(); it++)
+    {
+      if (is<StoreVolatileOperation>(*it))
+      {
+        numStoreVolatileThreeAddressCodes++;
+        auto ioStateAssignment = *std::next(it);
+        auto memoryStateAssignment = *std::next(it, 2);
+
+        assert(is<assignment_op>(ioStateAssignment->operation()));
+        assert(is<iostatetype>(ioStateAssignment->operand(0)->type()));
+
+        assert(is<assignment_op>(memoryStateAssignment->operation()));
+        assert(is<MemoryStateType>(memoryStateAssignment->operand(0)->type()));
+      }
+      else if (is<StoreOperation>(*it))
+      {
+        numStoreThreeAddressCodes++;
+        auto memoryStateAssignment = *std::next(it, 1);
+
+        assert(is<assignment_op>(memoryStateAssignment->operation()));
+        assert(is<MemoryStateType>(memoryStateAssignment->operand(0)->type()));
+      }
+    }
+
+    assert(numStoreThreeAddressCodes == 1);
+    assert(numStoreVolatileThreeAddressCodes == 2);
+  }
+
+  return 0;
+}
+
+JLM_UNIT_TEST_REGISTER("jlm/llvm/frontend/llvm/StoreTests-StoreConversion", StoreConversion)
