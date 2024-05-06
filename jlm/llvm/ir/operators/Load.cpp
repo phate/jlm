024905f842f7f5df263296109c5b5b6c52aa937a
Nico Reissmann <nico.reissmann@gmail.com>
2024-05-07 00:47:11 +0200

    Move all store related classes to Store.[ch]pp (#468)
    Moves all store related classes into a single header and implementation
    file.

--- jlm/llvm/ir/operators/Load.cpp
+++ jlm/llvm/ir/operators/Load.cpp
@@ -6,7 +6,7 @@
 #include <jlm/llvm/ir/operators/alloca.hpp>
 #include <jlm/llvm/ir/operators/Load.hpp>
 #include <jlm/llvm/ir/operators/operators.hpp>
-#include <jlm/llvm/ir/operators/StoreNonVolatile.hpp>
+#include <jlm/llvm/ir/operators/Store.hpp>
 
 namespace jlm::llvm
 {
