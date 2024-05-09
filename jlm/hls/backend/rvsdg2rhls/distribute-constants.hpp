44704aa94043e93a2eb2e266c09068447f91b45f
Magnus Sjalander <magnus.sjalander@ntnu.no>
2024-05-09 08:54:33 +0200

    Added distribute constant files

---
+++ jlm/hls/backend/rvsdg2rhls/distribute-constants.hpp
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2021 David Metz <david.c.metz@ntnu.no>
+ * See COPYING for terms of redistribution.
+ */
+
+#ifndef JLM_LIBJLM_SRC_BACKEND_HLS_RVSDG2RHLS_DISTRIBUTE_CONSTANTS_HPP
+#define JLM_LIBJLM_SRC_BACKEND_HLS_RVSDG2RHLS_DISTRIBUTE_CONSTANTS_HPP
+
+#include <jlm/rvsdg/region.hpp>
+#include <jlm/llvm/ir/operators/lambda.hpp>
+#include <jlm/llvm/ir/RvsdgModule.hpp>
+
+namespace jlm {
+	namespace hls {
+		void
+		distribute_constants(rvsdg::region *region);
+
+		void
+		distribute_constants(llvm::RvsdgModule &rm);
+	}
+}
+#endif //JLM_LIBJLM_SRC_BACKEND_HLS_RVSDG2RHLS_DISTRIBUTE_CONSTANTS_HPP
