4bac08270bd2bbeb81c9d41b291b8c94dc9acccb
Magnus Sjalander <magnus.sjalander@ntnu.no>
2024-05-11 09:38:31 +0200

    Variable name updated

--- jlm/hls/backend/rhls2firrtl/RhlsToFirrtlConverter.cpp
+++ jlm/hls/backend/rhls2firrtl/RhlsToFirrtlConverter.cpp
@@ -467,7 +467,7 @@ circt::firrtl::FModuleOp
 RhlsToFirrtlConverter::MlirGenFork(const jlm::rvsdg::simple_node * node)
 {
   auto op = dynamic_cast<const jlm::hls::fork_op *>(&node->operation());
-  bool constant = op->IsConstant();
+  bool isConstant = op->IsConstant();
   // Create the module and its input/output ports
   auto module = nodeToModule(node);
   auto body = module.getBodyBlock();
@@ -484,7 +484,7 @@ RhlsToFirrtlConverter::MlirGenFork(const jlm::rvsdg::simple_node * node)
   //
   // Output registers
   //
-  if (constant)
+  if (isConstant)
   {
     Connect(body, inReady, oneBitValue);
     for (size_t i = 0; i < node->noutputs(); ++i)
