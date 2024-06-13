a86f01cb1a3fb91aab4f246705253d114daab86d
Magnus Sjalander <magnus.sjalander@ntnu.no>
2024-06-13 11:25:34 +0200

    Script for running the polybench suite and updated CI

---
+++ scripts/run-polybench.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -eu
+
+# URL to the benchmark git repository and the commit to be used
+GIT_REPOSITORY=https://github.com/phate/polybench-jlm.git
+GIT_COMMIT=6d43f31b4790e180c9d3672bf77afba39414f8b2
+
+# Get the absolute path to this script and set default JLM paths
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+JLM_ROOT_DIR=${SCRIPT_DIR}/..
+JLM_BIN_DIR=${JLM_ROOT_DIR}/build
+
+# Set default path for where the benchmark will be cloned and make target for running it
+BENCHMARK_DIR=${JLM_ROOT_DIR}/usr/polybench
+BENCHMARK_RUN_TARGET=check
+
+function commit()
+{
+	echo ${GIT_COMMIT}
+}
+
+function usage()
+{
+	echo "Usage: ./run-polybench.sh [OPTION] [VAR=VALUE]"
+	echo ""
+	echo "  --benchmark-path PATH The path where to place the polybench suite."
+	echo "                        [${BENCHMARK_DIR}]"
+	echo "  --get-commit-hash     Prints the commit hash used for the build."
+	echo "  --help                Prints this message and stops."
+}
+
+while [[ "$#" -ge 1 ]] ; do
+	case "$1" in
+		--build-path)
+			shift
+			BUILD_ROOT=$(readlink -m "$1")
+			shift
+			;;
+		--get-commit-hash)
+			commit >&2
+			exit 1
+			;;
+		--help)
+			usage >&2
+			exit 1
+			;;
+	esac
+done
+
+if [ ! -d "$BENCHMARK_DIR" ] ;
+then
+	git clone ${GIT_REPOSITORY} ${BENCHMARK_DIR}
+fi
+
+export PATH=${JLM_BIN_DIR}:${PATH}
+cd ${BENCHMARK_DIR}
+make clean
+make -j `nproc` -O ${BENCHMARK_RUN_TARGET}
