d71a4321f2462d87974d073bc83d1f5ec6ea3d5c
Magnus Sjalander <work@sjalander.com>
2024-07-14 21:53:23 +0200

    jhls links llvm-IR files and creates single object file (#545)
    This simplifies how HLS benchmarks are compiled and reduce their
    dependencies to only jlm binaries (jhls and jlm-hls) and Verilator.

--- jlm/tooling/CommandGraphGenerator.cpp
+++ jlm/tooling/CommandGraphGenerator.cpp
@@ -360,12 +360,19 @@ JhlsCommandGraphGenerator::GenerateCommandGraph(const JhlsCommandLineOptions & c
       commandLineOptions.UseCirct_);
   m2r2.AddEdge(hls);
 
-  std::vector<util::filepath> lnkifiles;
-  for (const auto & c : commandLineOptions.Compilations_)
-  {
-    if (c.RequiresLinking())
-      lnkifiles.push_back(c.OutputFile());
-  }
+  auto linkerInputFiles = util::filepath(commandLineOptions.OutputFile_.to_str() + ".re*.ll");
+  auto mergedFile = util::filepath(commandLineOptions.OutputFile_.to_str() + ".merged.ll");
+  auto & llvmLink =
+      LlvmLinkCommand::Create(*commandGraph, { linkerInputFiles }, mergedFile, true, false);
+  hls.AddEdge(llvmLink);
+
+  auto & compileMerged = LlcCommand::Create(
+      *commandGraph,
+      mergedFile,
+      util::filepath(commandLineOptions.OutputFile_.to_str() + ".o"),
+      LlcCommand::OptimizationLevel::O3,
+      LlcCommand::RelocationModel::Pic);
+  llvmLink.AddEdge(compileMerged);
 
   for (const auto & leave : leaves)
     leave->AddEdge(commandGraph->GetExitNode());
