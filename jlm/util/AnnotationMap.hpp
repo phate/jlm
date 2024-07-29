ad7d5016a9943e15f98cbc63ca7075a9c88278e6
Nico Reissmann <nico.reissmann@gmail.com>
2024-07-29 07:19:58 +0200

    Add annotation map

---
+++ jlm/util/AnnotationMap.hpp
@@ -0,0 +1,219 @@
+/*
+ * Copyright 2024 Nico Reißmann <nico.reissmann@gmail.com>
+ * See COPYING for terms of redistribution.
+ */
+
+#include <jlm/util/common.hpp>
+#include <jlm/util/iterator_range.hpp>
+
+#include <cstdint>
+#include <string_view>
+#include <unordered_map>
+#include <variant>
+
+namespace jlm::util
+{
+
+/**
+ * Represents a simple key-value pair with a label and a value of type std::string_view, int64_t,
+ * uint64_t, or double.
+ */
+class Annotation final
+{
+  using AnnotationValue = std::variant<std::string_view, int64_t, uint64_t, double>;
+
+public:
+  Annotation(std::string_view label, std::string_view value)
+      : Label_(std::move(label)),
+        Value_(std::move(value))
+  {}
+
+  Annotation(std::string_view label, int64_t value)
+      : Label_(std::move(label)),
+        Value_(std::move(value))
+  {}
+
+  Annotation(std::string_view label, uint64_t value)
+      : Label_(std::move(label)),
+        Value_(std::move(value))
+  {}
+
+  Annotation(std::string_view label, double value)
+      : Label_(std::move(label)),
+        Value_(std::move(value))
+  {}
+
+  /**
+   * Gets the label of the annotation.
+   */
+  [[nodiscard]] const std::string_view &
+  Label() const noexcept
+  {
+    return Label_;
+  }
+
+  /**
+   * Gets the value of the annotation. Requires the annotation value to be of type \p TValue.
+   */
+  template<typename TValue>
+  [[nodiscard]] const TValue &
+  Value() const
+  {
+    return std::get<TValue>(Value_);
+  }
+
+  /**
+   * Checks if the type of the annotation value is equivalent to \p TValue.
+   *
+   * @return True if the value type if equivalent to \p TValue, otherwise false.
+   */
+  template<typename TValue>
+  [[nodiscard]] bool
+  HasValueType() const noexcept
+  {
+    return std::get_if<TValue>(&Value_) != nullptr;
+  }
+
+  bool
+  operator==(Annotation & other) const noexcept
+  {
+    return Label_ == other.Label_ && Value_ == other.Value_;
+  }
+
+  bool
+  operator!=(Annotation & other) const noexcept
+  {
+    return !(*this == other);
+  }
+
+private:
+  std::string_view Label_ = {};
+  AnnotationValue Value_ = {};
+};
+
+/**
+ * Represents a simple map that associates pointers with Annotation%s.
+ */
+class AnnotationMap final
+{
+  using AnnotationMapType = std::unordered_map<const void *, Annotation>;
+
+  class ConstIterator final
+  {
+  public:
+    using iterator_category = std::forward_iterator_tag;
+    using value_type = Annotation;
+    using difference_type = std::ptrdiff_t;
+    using pointer = Annotation *;
+    using reference = Annotation &;
+
+  private:
+    friend AnnotationMap;
+
+    explicit ConstIterator(const typename AnnotationMapType::const_iterator & it)
+        : It_(it)
+    {}
+
+  public:
+    [[nodiscard]] const jlm::util::Annotation *
+    GetAnnotation() const noexcept
+    {
+      return &It_.operator->()->second;
+    }
+
+    const jlm::util::Annotation &
+    operator*() const
+    {
+      return *GetAnnotation();
+    }
+
+    const jlm::util::Annotation *
+    operator->() const
+    {
+      return GetAnnotation();
+    }
+
+    ConstIterator &
+    operator++()
+    {
+      ++It_;
+      return *this;
+    }
+
+    ConstIterator
+    operator++(int)
+    {
+      ConstIterator tmp = *this;
+      ++*this;
+      return tmp;
+    }
+
+    bool
+    operator==(const ConstIterator & other) const
+    {
+      return It_ == other.It_;
+    }
+
+    bool
+    operator!=(const ConstIterator & other) const
+    {
+      return !operator==(other);
+    }
+
+  private:
+    typename AnnotationMapType::const_iterator It_ = {};
+  };
+
+  using AnnotationRange = iterator_range<AnnotationMap::ConstIterator>;
+
+public:
+  /**
+   * Retrieves all annotations.
+   *
+   * @return An iterator_range of all the annotations.
+   */
+  [[nodiscard]] AnnotationRange
+  Annotations() const
+  {
+    return { ConstIterator(Map_.begin()), ConstIterator(Map_.end()) };
+  }
+
+  /**
+   * Checks if an annotation with the given \p key exists.
+   *
+   * @return True if the annotation exists, otherwise false.
+   */
+  [[nodiscard]] bool
+  HasAnnotation(const void * key) const noexcept
+  {
+    return Map_.find(key) != Map_.end();
+  }
+
+  /**
+   * Retrieves the annotation for the given \p key. The key must exist.
+   *
+   * @return A reference to an instance of Annotation.
+   */
+  [[nodiscard]] const Annotation &
+  GetAnnotation(const void * key) const noexcept
+  {
+    JLM_ASSERT(HasAnnotation(key));
+    return Map_.at(key);
+  }
+
+  /**
+   * Adds \p annotation with the given \p key to the map. Requires that \p key is not yet
+   * present.
+   */
+  void
+  AddAnnotation(const void * key, Annotation annotation)
+  {
+    JLM_ASSERT(!HasAnnotation(key));
+    Map_.emplace(key, annotation);
+  }
+
+private:
+  AnnotationMapType Map_ = {};
+};
+
+}
