ff8d5d89b7480de2e30b7cb74c47f63442d1b407
haved <krogstie.havard@gmail.com>
2024-08-07 11:21:14 +0200

    modify the CIRCT and MLIR jlm dialect's build scripts to work more generally

--- scripts/build-circt.sh
+++ scripts/build-circt.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-set -eu
+set -eux
 
 GIT_COMMIT=debf1ed774c2bbdbfc8e7bc987a21f72e8f08f65
 
 # Get the absolute path to this script and set default build and install paths
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
-JLM_ROOT_DIR=${SCRIPT_DIR}/..
-CIRCT_BUILD=${JLM_ROOT_DIR}/build-circt
-CIRCT_INSTALL=${JLM_ROOT_DIR}/usr
+JLM_ROOT_DIR="$(realpath "${SCRIPT_DIR}/..")"
+CIRCT_BUILD="${JLM_ROOT_DIR}/build-circt"
+CIRCT_INSTALL="${JLM_ROOT_DIR}/usr"
 LLVM_LIT_PATH=/usr/local/bin/lit
 
 LLVM_VERSION=17
-LLVM_CONFIG=llvm-config-${LLVM_VERSION}
-LLVM_ROOT=$(${LLVM_CONFIG} --prefix)
-LLVM_LIB=$(${LLVM_CONFIG} --libdir)
+LLVM_CONFIG_BIN=llvm-config-${LLVM_VERSION}
 
 function commit()
 {
@@ -24,6 +22,8 @@ function usage()
 {
 	echo "Usage: ./build-circt.sh [OPTION] [VAR=VALUE]"
 	echo ""
+	echo "  --llvm-config PATH    The llvm-config script used to determine up llvm"
+	echo "                        build dependencies. [${LLVM_CONFIG_BIN}]"
 	echo "  --build-path PATH     The path where to build CIRCT."
 	echo "                        [${CIRCT_BUILD}]"
 	echo "  --install-path PATH   The path where to install CIRCT."
@@ -36,6 +36,11 @@ function usage()
 
 while [[ "$#" -ge 1 ]] ; do
 	case "$1" in
+		--llvm-config)
+			shift
+			LLVM_CONFIG_BIN="$1"
+			shift
+			;;
 		--build-path)
 			shift
 			CIRCT_BUILD=$(readlink -m "$1")
@@ -62,6 +67,9 @@ while [[ "$#" -ge 1 ]] ; do
 	esac
 done
 
+LLVM_BINDIR=$(${LLVM_CONFIG_BIN} --bindir)
+LLVM_CMAKEDIR=$(${LLVM_CONFIG_BIN} --cmakedir)
+
 CIRCT_GIT_DIR=${CIRCT_BUILD}/circt.git
 CIRCT_BUILD_DIR=${CIRCT_BUILD}/build
 
@@ -69,16 +77,15 @@ if [ ! -d "$CIRCT_GIT_DIR" ] ;
 then
 	git clone https://github.com/EECS-NTNU/circt.git ${CIRCT_GIT_DIR}
 fi
-cd ${CIRCT_GIT_DIR}
-git checkout ${GIT_COMMIT}
+git -C ${CIRCT_GIT_DIR} checkout ${GIT_COMMIT}
 cmake -G Ninja \
 	${CIRCT_GIT_DIR} \
 	-B ${CIRCT_BUILD_DIR} \
-	-DCMAKE_C_COMPILER=clang-${LLVM_VERSION} \
-	-DCMAKE_CXX_COMPILER=clang++-${LLVM_VERSION} \
+	-DCMAKE_C_COMPILER=${LLVM_BINDIR}/clang \
+	-DCMAKE_CXX_COMPILER=${LLVM_BINDIR}/clang++ \
 	-DCMAKE_BUILD_TYPE=RelWithDebInfo \
-	-DLLVM_DIR=${LLVM_ROOT}/cmake/ \
-	-DMLIR_DIR=${LLVM_LIB}/cmake/mlir \
+	-DLLVM_DIR=${LLVM_CMAKEDIR} \
+	-DMLIR_DIR=${LLVM_CMAKEDIR}/../mlir \
 	-DLLVM_EXTERNAL_LIT="${LLVM_LIT_PATH}" \
 	-DLLVM_LIT_ARGS="-v --show-unsupported" \
 	-DVERILATOR_DISABLE=ON \
