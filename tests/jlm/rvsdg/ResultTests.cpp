e82ce46c34a088b53679ac1a43cd3c4f091cc9f6
Nico Reissmann <nico.reissmann@gmail.com>
2024-08-13 02:37:50 +0200

    add unit test

--- tests/jlm/rvsdg/ResultTests.cpp
+++ tests/jlm/rvsdg/ResultTests.cpp
@@ -49,3 +49,43 @@ ResultNodeMismatch()
 }
 
 JLM_UNIT_TEST_REGISTER("jlm/rvsdg/ResultTests-ResultNodeMismatch", ResultNodeMismatch)
+
+static int
+ResultInputTypeMismatch()
+{
+  using namespace jlm::tests;
+  using namespace jlm::util;
+
+  // Arrange
+  auto valueType = jlm::tests::valuetype::Create();
+  auto stateType = jlm::tests::statetype::Create();
+
+  jlm::rvsdg::graph rvsdg;
+
+  auto structuralNode = structural_node::create(rvsdg.root(), 1);
+  auto structuralOutput = jlm::rvsdg::structural_output::create(structuralNode, valueType);
+
+  // Act & Assert
+  bool exceptionWasCaught = false;
+  try
+  {
+    auto simpleNode = test_op::create(structuralNode->subregion(0), {}, { stateType });
+    jlm::rvsdg::result::create(
+        structuralNode->subregion(0),
+        simpleNode->output(0),
+        structuralOutput,
+        stateType);
+    // The line below should not be executed as the line above is expected to throw an exception.
+    assert(false);
+  }
+  catch (type_error &)
+  {
+    exceptionWasCaught = true;
+  }
+
+  assert(exceptionWasCaught);
+
+  return 0;
+}
+
+JLM_UNIT_TEST_REGISTER("jlm/rvsdg/ResultTests-ResultInputTypeMismatch", ResultInputTypeMismatch)
