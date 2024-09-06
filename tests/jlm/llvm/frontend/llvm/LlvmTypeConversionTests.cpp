0fd2a1dd3e3b8a1895cb6b9e91bfa99866cfac5b
Halvor Linder <halvor_lh@hotmail.no>
2024-09-06 13:07:37 +0200

    update type conversion test to only test type conversion

--- tests/jlm/llvm/frontend/llvm/LlvmTypeConversionTests.cpp
+++ tests/jlm/llvm/frontend/llvm/LlvmTypeConversionTests.cpp
@@ -6,101 +6,39 @@
 #include <test-registry.hpp>
 #include <test-util.hpp>
 
-#include <jlm/llvm/frontend/LlvmModuleConversion.hpp>
-#include <jlm/llvm/ir/operators/Load.hpp>
-#include <jlm/llvm/ir/operators/operators.hpp>
-#include <jlm/llvm/ir/print.hpp>
-
-#include <llvm/IR/BasicBlock.h>
-#include <llvm/IR/IRBuilder.h>
-#include <llvm/IR/Module.h>
+#include <jlm/llvm/frontend/LlvmConversionContext.hpp>
+#include <jlm/llvm/frontend/LlvmTypeConversion.hpp>
 
 static void
 TestTypeConversion(
-    llvm::LLVMContext & context,
+    jlm::llvm::context & jlm_context,
     llvm::Type * llvm_type,
     jlm::llvm::fpsize jlm_type_size)
 {
   using namespace llvm;
 
-  std::unique_ptr<Module> llvmModule(new Module("module", context));
+  auto jlm_type = jlm::llvm::ConvertType(llvm_type, jlm_context);
+  auto floating_point_type = dynamic_cast<const jlm::llvm::fptype *>(jlm_type.get());
 
-  auto pointer_type = ::llvm::PointerType::getUnqual(llvm_type);
-
-  auto function_type = FunctionType::get(llvm_type, ArrayRef<Type *>({ pointer_type }), false);
-  auto function =
-      Function::Create(function_type, GlobalValue::ExternalLinkage, "f", llvmModule.get());
-
-  auto bb = BasicBlock::Create(context, "bb", function);
-
-  auto arg0 = function->getArg(0);
-
-  IRBuilder<> builder(bb);
-
-  auto loadedValue1 = builder.CreateLoad(llvm_type, arg0, false);
-  builder.CreateRet(loadedValue1);
-
-  // Act
-  auto ipgModule = jlm::llvm::ConvertLlvmModule(*llvmModule);
-  print(*ipgModule, stdout);
-
-  {
-    using namespace jlm::llvm;
-    auto controlFlowGraph =
-        dynamic_cast<const function_node *>(ipgModule->ipgraph().find("f"))->cfg();
-    auto basicBlock =
-        dynamic_cast<const basic_block *>(controlFlowGraph->entry()->outedge(0)->sink());
-
-    /*     Generated ipg bb looks something like this:
-        <double, iostate, mem> f <ptr, iostate, mem>
-        {
-        0xbd0675417b80:  _io_ _s_
-
-        0xbd0675418110:
-          _r_ = undef
-          tv0, tv1 = Load , _s_
-          ASSIGN _s_, tv1
-          ASSIGN _r_, tv0
-          [0xbd0675417c00]
-
-        0xbd0675417c00: _r_ _io_ _s_
-        } */
-
-    auto tac_it = basicBlock->begin();
-    auto return_declaration = *tac_it;
-    auto load = *std::next(tac_it);
-    auto return_assignment = *std::next(tac_it, 3);
-
-    std::cout << return_assignment->operation().debug_string() << std::endl;
-
-    assert(is<fptype>(return_declaration->result(0)->type()));
-    auto t0 = dynamic_cast<const fptype &>(return_declaration->result(0)->type());
-    assert(t0.size() == jlm_type_size);
-
-    assert(is<fptype>(load->result(0)->type()));
-    auto t1 = dynamic_cast<const fptype &>(load->result(0)->type());
-    assert(t1.size() == jlm_type_size);
-
-    assert(is<fptype>(return_assignment->operand(0)->type()));
-    auto t2 = dynamic_cast<const fptype &>(return_assignment->operand(0)->type());
-    assert(t2.size() == jlm_type_size);
-
-    assert(is<fptype>(return_assignment->operand(1)->type()));
-    auto t3 = dynamic_cast<const fptype &>(return_assignment->operand(1)->type());
-    assert(t3.size() == jlm_type_size);
-  }
+  assert(floating_point_type && floating_point_type->size() == jlm_type_size);
 }
 
 static int
 TypeConversion()
 {
-  llvm::LLVMContext context;
+  using namespace jlm::llvm;
 
-  TestTypeConversion(context, ::llvm::Type::getHalfTy(context), jlm::llvm::fpsize::half);
-  TestTypeConversion(context, ::llvm::Type::getFloatTy(context), jlm::llvm::fpsize::flt);
-  TestTypeConversion(context, ::llvm::Type::getDoubleTy(context), jlm::llvm::fpsize::dbl);
-  TestTypeConversion(context, ::llvm::Type::getX86_FP80Ty(context), jlm::llvm::fpsize::x86fp80);
-  TestTypeConversion(context, ::llvm::Type::getFP128Ty(context), jlm::llvm::fpsize::fp128);
+  llvm::LLVMContext llvm_ctx;
+  llvm::Module lm("module", llvm_ctx);
+
+  ipgraph_module im(jlm::util::filepath(""), "", "");
+  auto jlm_ctx = context(im);
+
+  TestTypeConversion(jlm_ctx, ::llvm::Type::getHalfTy(llvm_ctx), jlm::llvm::fpsize::half);
+  TestTypeConversion(jlm_ctx, ::llvm::Type::getFloatTy(llvm_ctx), jlm::llvm::fpsize::flt);
+  TestTypeConversion(jlm_ctx, ::llvm::Type::getDoubleTy(llvm_ctx), jlm::llvm::fpsize::dbl);
+  TestTypeConversion(jlm_ctx, ::llvm::Type::getX86_FP80Ty(llvm_ctx), jlm::llvm::fpsize::x86fp80);
+  TestTypeConversion(jlm_ctx, ::llvm::Type::getFP128Ty(llvm_ctx), jlm::llvm::fpsize::fp128);
 
   return 0;
 }
