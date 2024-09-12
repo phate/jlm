d34a7845a0010ba6e684a775d921fa869d5414ad
Nico Reissmann <nico.reissmann@gmail.com>
2024-09-12 05:49:18 +0200

    Add NumMemoryStateInputsOutputs annotation to RvsdgTreePrinter

--- tests/jlm/llvm/opt/RvsdgTreePrinterTests.cpp
+++ tests/jlm/llvm/opt/RvsdgTreePrinterTests.cpp
@@ -8,10 +8,19 @@
 #include <test-types.hpp>
 
 #include <jlm/llvm/ir/operators/lambda.hpp>
-#include <jlm/llvm/ir/RvsdgModule.hpp>
 #include <jlm/llvm/opt/RvsdgTreePrinter.hpp>
 
 #include <fstream>
+
+static std::string
+ReadFile(const std::string & outputFilePath)
+{
+  std::ifstream file(outputFilePath);
+  std::stringstream buffer;
+  buffer << file.rdbuf();
+
+  return buffer.str();
+}
 
 static int
 PrintRvsdgTree()
@@ -41,13 +50,14 @@ PrintRvsdgTree()
   printer.run(*rvsdgModule);
 
   // Assert
-  auto outputFilePath = tempDirectory.string() + "/" + fileName + "-rvsdgTree-0";
+  auto tree = ReadFile(tempDirectory.string() + "/" + fileName + "-rvsdgTree-0");
+  std::cout << tree;
 
-  std::ifstream file(outputFilePath);
-  std::stringstream buffer;
-  buffer << file.rdbuf();
+  auto expectedTree = "RootRegion\n"
+                      "-LAMBDA[f]\n"
+                      "--Region[0]\n\n";
 
-  assert(buffer.str() == "RootRegion\n-LAMBDA[f]\n--Region[0]\n\n");
+  assert(tree == expectedTree);
 
   return 0;
 }
@@ -81,16 +91,15 @@ PrintNumRvsdgNodesAnnotation()
   printer.run(*rvsdgModule);
 
   // Assert
-  auto outputFilePath = tempDirectory.string() + "/" + fileName + "-rvsdgTree-0";
+  auto tree = ReadFile(tempDirectory.string() + "/" + fileName + "-rvsdgTree-0");
+  std::cout << tree;
 
-  std::ifstream file(outputFilePath);
-  std::stringstream buffer;
-  buffer << file.rdbuf();
+  auto expectedTree = "RootRegion NumRvsdgNodes:2\n"
+                      "-STRUCTURAL_TEST_NODE NumRvsdgNodes:2\n"
+                      "--Region[0] NumRvsdgNodes:1\n"
+                      "--Region[1] NumRvsdgNodes:1\n\n";
 
-  assert(
-      buffer.str()
-      == "RootRegion NumRvsdgNodes:2\n-STRUCTURAL_TEST_NODE NumRvsdgNodes:2\n--Region[0] "
-         "NumRvsdgNodes:1\n--Region[1] NumRvsdgNodes:1\n\n");
+  assert(tree == expectedTree);
 
   return 0;
 }
@@ -98,3 +107,58 @@ PrintNumRvsdgNodesAnnotation()
 JLM_UNIT_TEST_REGISTER(
     "jlm/llvm/opt/RvsdgTreePrinterTests-PrintNumRvsdgNodesAnnotation",
     PrintNumRvsdgNodesAnnotation)
+
+static int
+PrintNumMemoryStateInputsOutputsAnnotation()
+{
+  using namespace jlm::llvm;
+  using namespace jlm::util;
+
+  // Arrange
+  auto memoryStateType = MemoryStateType::Create();
+  auto valueType = jlm::tests::valuetype::Create();
+
+  std::string fileName = "PrintNumMemoryStateInputsOutputsAnnotationTest";
+  auto rvsdgModule = RvsdgModule::Create({ fileName }, "", "");
+  auto & rvsdg = rvsdgModule->Rvsdg();
+
+  auto & x = jlm::tests::GraphImport::Create(rvsdg, memoryStateType, "x");
+  auto & y = jlm::tests::GraphImport::Create(rvsdg, valueType, "y");
+
+  auto structuralNode = jlm::tests::structural_node::create(rvsdg.root(), 2);
+  auto & ix = structuralNode->AddInputWithArguments(x);
+  auto & iy = structuralNode->AddInputWithArguments(y);
+
+  auto & ox = structuralNode->AddOutputWithResults({ &ix.Argument(0), &ix.Argument(1) });
+  auto & oy = structuralNode->AddOutputWithResults({ &iy.Argument(0), &iy.Argument(1) });
+
+  jlm::tests::GraphExport::Create(ox, "x");
+  jlm::tests::GraphExport::Create(oy, "y");
+
+  auto tempDirectory = std::filesystem::temp_directory_path();
+  RvsdgTreePrinter::Configuration configuration(
+      { tempDirectory },
+      { RvsdgTreePrinter::Configuration::Annotation::NumMemoryStateInputsOutputs });
+  RvsdgTreePrinter printer(configuration);
+
+  // Act
+  printer.run(*rvsdgModule);
+
+  // Assert
+  auto tree = ReadFile(tempDirectory.string() + "/" + fileName + "-rvsdgTree-0");
+  std::cout << tree;
+
+  auto expectedTree =
+      "RootRegion NumMemoryStateTypeArguments:1 NumMemoryStateTypeResults:1\n"
+      "-STRUCTURAL_TEST_NODE NumMemoryStateTypeInputs:1 NumMemoryStateTypeOutputs:1\n"
+      "--Region[0] NumMemoryStateTypeArguments:1 NumMemoryStateTypeResults:1\n"
+      "--Region[1] NumMemoryStateTypeArguments:1 NumMemoryStateTypeResults:1\n\n";
+
+  assert(tree == expectedTree);
+
+  return 0;
+}
+
+JLM_UNIT_TEST_REGISTER(
+    "jlm/llvm/opt/RvsdgTreePrinterTests-PrintNumMemoryStateInputsOutputsAnnotation",
+    PrintNumMemoryStateInputsOutputsAnnotation)
