cc78b2192ed2e634785d02289898d202a9d6f6f5
Nico Reissmann <nico.reissmann@gmail.com>
2024-09-17 05:55:52 +0200

    more

--- jlm/hls/backend/rvsdg2rhls/add-forks.hpp
+++ jlm/hls/backend/rvsdg2rhls/add-forks.hpp
@@ -16,7 +16,7 @@ namespace jlm::hls
  * Adds a fork for every output that has multiple consumers (node inputs). The original output is
  * connected to the fork's input and each consumer is connected to one of the fork's outputs.
  *
- * /param region The Region for which to insert forks.
+ * /param region The region for which to insert forks.
  */
 void
 add_forks(rvsdg::Region * region);
