54449da8f4d71e3d634550763274b5b2aec7ec22
Nico Reissmann <nico.reissmann@gmail.com>
2024-09-28 07:49:39 +0200

    Move CFG conversion to ascii to cfg class (#631)
    The ascii output for the control flow graph is not easily readable. This
    is the first PR on improving it. It does the following:
    
    1. Moves the functions for converting a CFG to ascii to the cfg class
    2. Adds a unit test for the conversion of three address codes
    
    In order to add a unit test for the CFG conversion, I first need to make
    the output more deterministic. This will happen in a follow up PR. This
    PR is part of issue #586

--- jlm/llvm/ir/cfg.cpp
+++ jlm/llvm/ir/cfg.cpp
@@ -69,6 +69,111 @@ cfg::remove_node(basic_block * bb)
   return remove_node(it);
 }
 
+std::string
+cfg::ToAscii(const cfg & controlFlowGraph)
+{
+  std::string str;
+  auto nodes = breadth_first(controlFlowGraph);
+  for (const auto & node : nodes)
+  {
+    str += CreateLabel(*node) + ":";
+    str += (is<basic_block>(node) ? "\n" : " ");
+
+    if (auto entryNode = dynamic_cast<const entry_node *>(node))
+    {
+      str += ToAscii(*entryNode);
+    }
+    else if (auto exitNode = dynamic_cast<const exit_node *>(node))
+    {
+      str += ToAscii(*exitNode);
+    }
+    else if (auto basicBlock = dynamic_cast<const basic_block *>(node))
+    {
+      str += ToAscii(*basicBlock);
+    }
+    else
+    {
+      JLM_UNREACHABLE("Unhandled control flow graph node type!");
+    }
+  }
+
+  return str;
+}
+
+std::string
+cfg::ToAscii(const entry_node & entryNode)
+{
+  std::string str;
+  for (size_t n = 0; n < entryNode.narguments(); n++)
+  {
+    str += entryNode.argument(n)->debug_string() + " ";
+  }
+
+  return str + "\n";
+}
+
+std::string
+cfg::ToAscii(const exit_node & exitNode)
+{
+  std::string str;
+  for (size_t n = 0; n < exitNode.nresults(); n++)
+  {
+    str += exitNode.result(n)->debug_string() + " ";
+  }
+
+  return str;
+}
+
+std::string
+cfg::ToAscii(const basic_block & basicBlock)
+{
+  auto & threeAddressCodes = basicBlock.tacs();
+
+  std::string str;
+  for (const auto & tac : threeAddressCodes)
+  {
+    str += "\t" + tac::ToAscii(*tac);
+    if (tac != threeAddressCodes.last())
+      str += "\n";
+  }
+
+  if (threeAddressCodes.last())
+  {
+    if (is<branch_op>(threeAddressCodes.last()->operation()))
+      str += " " + CreateTargets(basicBlock);
+    else
+      str += "\n\t" + CreateTargets(basicBlock);
+  }
+  else
+  {
+    str += "\t" + CreateTargets(basicBlock);
+  }
+
+  return str + "\n";
+}
+
+std::string
+cfg::CreateTargets(const cfg_node & node)
+{
+  size_t n = 0;
+  std::string str("[");
+  for (auto it = node.begin_outedges(); it != node.end_outedges(); it++, n++)
+  {
+    str += CreateLabel(*it->sink());
+    if (n != node.noutedges() - 1)
+      str += ", ";
+  }
+  str += "]";
+
+  return str;
+}
+
+std::string
+cfg::CreateLabel(const cfg_node & node)
+{
+  return util::strfmt(&node);
+}
+
 /* supporting functions */
 
 std::vector<cfg_node *>
