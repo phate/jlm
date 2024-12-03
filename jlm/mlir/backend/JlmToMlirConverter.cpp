074ef29092ae6bd291f99e4e757400f70c5ad487
Halvor Linder <halvor_lh@hotmail.no>
2024-12-03 11:40:29 +0100

    constant data array, constant agg zero and vararg to mlir

--- jlm/mlir/backend/JlmToMlirConverter.cpp
+++ jlm/mlir/backend/JlmToMlirConverter.cpp
@@ -416,6 +416,11 @@ JlmToMlirConverter::ConvertSimpleNode(
     MlirOp =
         Builder_->create<::mlir::arith::ConstantFloatOp>(Builder_->getUnknownLoc(), value, size);
   }
+  else if (auto zeroOp = dynamic_cast<const llvm::ConstantAggregateZeroOperation *>(&operation))
+  {
+    auto type = ConvertType(*zeroOp->result(0));
+    MlirOp = Builder_->create<::mlir::LLVM::ZeroOp>(Builder_->getUnknownLoc(), type);
+  }
   else if (auto arrOp = dynamic_cast<const llvm::ConstantDataArray *>(&operation))
   {
     auto arrayType = ConvertType(*arrOp->result(0));
