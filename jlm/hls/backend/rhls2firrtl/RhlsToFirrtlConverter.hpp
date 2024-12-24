e59773a83e2bc38d44fafb741d270e7073e8ff86
HKrogstie <krogstie.havard@gmail.com>
2024-12-24 19:19:48 +0100

    Hls harness rework (#696)
    Creates a new cleaned up harness for calling Verilated hls kernels from
    C code.
    
    It defines a class for the memory queues, which simplifies the usage
    code a great deal.
    
    I eventually "cracked the code" regarding writing harnesses for
    hardware:
    Only read model outputs right before posedge, only write inputs right
    after posedge (and eval again).
    
    Example diff between the old and new `test_load.hls.harness.cpp` can be
    seen [here](https://www.diffchecker.com/1vnmcZ6H/). The diff is not
    particularly easy to read, as I have re-arranged functions. I would
    recommend just reading the new harness, as it includes comments.
    
    The new harness passes the tests, but some of the cycle counts have
    increased by one, due to the old harness allowing the model to "false
    start" when some computation does not depend on input. I have created a
    commit in `hls-test-suite` with the updated numbers.
    
    I also cleaned up some other code to use range-based for loops over
    `.Arguments()` and `.Results()` and similar, instead of doing C-style
    for loops.
    
    This PR is created partly to see how it fares in CI. It is definitely
    possible to write some tests for this code, particularly covering how
    the lambda's arguments, memory queues, and context variables are
    handled.

--- jlm/hls/backend/rhls2firrtl/RhlsToFirrtlConverter.hpp
+++ jlm/hls/backend/rhls2firrtl/RhlsToFirrtlConverter.hpp
@@ -43,7 +43,7 @@ class RhlsToFirrtlConverter : public BaseHLS
 
 public:
   std::string
-  get_text(llvm::RvsdgModule &) override
+  GetText(llvm::RvsdgModule &) override
   {
     return "MLIR/FIRRTL generator";
   }
