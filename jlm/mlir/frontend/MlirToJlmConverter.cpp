c02243775b94ce0c19529b27dd78d1a5d4d8fa85
Halvor Linder <halvor_lh@hotmail.no>
2025-02-05 10:05:30 +0100

    add error handling in gep conversion

--- jlm/mlir/frontend/MlirToJlmConverter.cpp
+++ jlm/mlir/frontend/MlirToJlmConverter.cpp
@@ -470,6 +470,9 @@ MlirToJlmConverter::ConvertOperation(
   {
     auto elemType = GepOp.getElemType();
     std::shared_ptr<jlm::rvsdg::Type> pointeeType = ConvertType(elemType);
+    if (!rvsdg::is<const rvsdg::ValueType>(pointeeType))
+      JLM_UNREACHABLE("Expected ValueType for GepOp operation pointee.");
+    
     auto pointeeValueType = std::dynamic_pointer_cast<const rvsdg::ValueType>(pointeeType);
 
     auto jlmGepOp = jlm::llvm::GetElementPtrOperation::Create(
