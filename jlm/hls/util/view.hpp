d3e4ea052a66347d75bffe6512274c9681253645
Magnus Sjalander <work@sjalander.com>
2025-03-29 15:41:36 +0100

    Add color to dot output and refactor of view HLS
    Written by: David Metz

--- jlm/hls/util/view.hpp
+++ jlm/hls/util/view.hpp
@@ -14,16 +14,47 @@ namespace jlm::hls
 {
 
 std::string
-region_to_dot(rvsdg::Region * region);
+region_to_dot(
+    rvsdg::Region * region,
+    std::unordered_map<rvsdg::output *, std::string> & o_color,
+    std::unordered_map<rvsdg::input *, std::string> & i_color);
 
 std::string
-to_dot(rvsdg::Region * region);
+to_dot(
+    rvsdg::Region * region,
+    std::unordered_map<rvsdg::output *, std::string> & o_color,
+    std::unordered_map<rvsdg::input *, std::string> & i_color);
 
 void
 view_dot(rvsdg::Region * region, FILE * out);
+void
+view_dot(
+    rvsdg::Region * region,
+    FILE * out,
+    std::unordered_map<rvsdg::output *, std::string> & o_color,
+    std::unordered_map<rvsdg::input *, std::string> & i_color);
 
 void
+dump_dot(
+    llvm::RvsdgModule & rvsdgModule,
+    const std::string & file_name,
+    std::unordered_map<rvsdg::output *, std::string> & o_color,
+    std::unordered_map<rvsdg::input *, std::string> & i_color);
+void
+
 dump_dot(llvm::RvsdgModule & rvsdgModule, const std::string & file_name);
+
+void
+dump_dot(
+    rvsdg::Region * region,
+    const std::string & file_name,
+    std::unordered_map<rvsdg::output *, std::string> & o_color,
+    std::unordered_map<rvsdg::input *, std::string> & i_color);
+void
+dump_dot(rvsdg::Region * region, const std::string & file_name);
+
+void
+dot_to_svg(const std::string & file_name);
 
 } // namespace jlm::hls
 
