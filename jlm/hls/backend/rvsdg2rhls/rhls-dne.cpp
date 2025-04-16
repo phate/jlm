3f286239fb6efce125b85534b306f289044e49c1
Magnus Sjalander <work@sjalander.com>
2025-04-16 07:50:39 +0200

    Fixed includes

--- jlm/hls/backend/rvsdg2rhls/rhls-dne.cpp
+++ jlm/hls/backend/rvsdg2rhls/rhls-dne.cpp
@@ -3,10 +3,10 @@
  * See COPYING for terms of redistribution.
  */
 
-#include "hls-function-util.hpp"
-#include "jlm/llvm/ir/operators/MemoryStateOperations.hpp"
+#include <jlm/hls/backend/rvsdg2rhls/hls-function-util.hpp>
 #include <jlm/hls/backend/rvsdg2rhls/rhls-dne.hpp>
 #include <jlm/llvm/ir/operators/lambda.hpp>
+#include <jlm/llvm/ir/operators/MemoryStateOperations.hpp>
 #include <jlm/rvsdg/traverser.hpp>
 
 namespace jlm::hls
