0dc43eb952a307198bdb2c2b74e007f919417dc3
caleridas <36173465+caleridas@users.noreply.github.com>
2025-04-27 21:42:15 +0200

    Introduce UnitType (#897)
    Introduce the UnitType to allow representing a type that holds no data
    (akin to the C "void" type). This can formally be used in places where
    "something" must be produced, but it has no content -- this will later
    be used in pattern matching over types without a content in generalized
    gamma constructs.
    
    Prepare places that currently balk on existency of this type so its
    usage can be introduced without breakage later.

---
+++ jlm/rvsdg/UnitType.hpp
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2025 Helge Bahmann <hcb@chaoticmind.net>
+ * See COPYING for terms of redistribution.
+ */
+
+#ifndef JLM_RVSDG_UNITTYPE_HPP
+#define JLM_RVSDG_UNITTYPE_HPP
+
+#include <jlm/rvsdg/type.hpp>
+
+namespace jlm::rvsdg
+{
+
+/**
+ * \brief Unit type (type carrying no information)
+ *
+ * Represents the "unit" type: A type which has only a single inhabitant
+ * without content and carries no information. This is used in places where
+ * a formal argument or result is needed, but no information is carried.
+ *
+ * This roughly corresponds to the "void" type in C/C++.
+ */
+class UnitType final : public ValueType
+{
+public:
+  ~UnitType() noexcept override;
+
+  std::string
+  debug_string() const override;
+
+  bool
+  operator==(const Type & other) const noexcept override;
+
+  std::size_t
+  ComputeHash() const noexcept override;
+
+  static std::shared_ptr<const UnitType>
+  Create();
+};
+
+}
+
+#endif
