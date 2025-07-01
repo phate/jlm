4147f9d4fea213ddf69fdc743049f58296d91655
Nico Reissmann <nico.reissmann@gmail.com>
2025-07-01 08:31:34 +0200

    TODO

---
+++ tests/jlm/hls/backend/rvsdg2rhls/SinkInsertion.cpp
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2025 Nico Reißmann <nico.reissmann@gmail.com>
+ * See COPYING for terms of redistribution.
+ */
+
+#include "test-registry.hpp"
+
+static void
+TestSinkInsertion()
+{
+  // Arrange
+  RvsdgModule rm(util::FilePath(""), "", "");
+}
