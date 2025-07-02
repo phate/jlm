d07c0b7ca3428faf978e4831e18ad9d9f7b35862
Nico Reissmann <nico.reissmann@gmail.com>
2025-07-02 07:13:43 +0200

    Rename tests::structural_op class to TestStructuralOperation

--- tests/test-operation.hpp
+++ tests/test-operation.hpp
@@ -222,12 +222,10 @@ private:
   enum BinaryOperation::flags flags_;
 };
 
-/* structural operation */
-
-class structural_op final : public rvsdg::StructuralOperation
+class TestStructuralOperation final : public rvsdg::StructuralOperation
 {
 public:
-  virtual ~structural_op() noexcept;
+  ~TestStructuralOperation() noexcept override;
 
   virtual std::string
   debug_string() const override;
@@ -251,7 +249,7 @@ private:
   {}
 
 public:
-  [[nodiscard]] const structural_op &
+  [[nodiscard]] const TestStructuralOperation &
   GetOperation() const noexcept override;
 
   StructuralNodeInput &
