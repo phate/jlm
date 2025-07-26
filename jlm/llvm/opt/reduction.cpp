d5e1a7a9a7faa6e4099021971bbbffe03ee7a179
Nico Reissmann <nico.reissmann@gmail.com>
2025-07-26 15:01:05 +0200

    Add LoadNonVolatileOperation::NormalizeIOBarrierAllocaAddress to NodeReduction pass

--- jlm/llvm/opt/reduction.cpp
+++ jlm/llvm/opt/reduction.cpp
@@ -240,7 +240,8 @@ NodeReduction::NormalizeLoadNode(
         LoadNonVolatileOperation::NormalizeLoadAlloca,
         LoadNonVolatileOperation::NormalizeDuplicateStates,
         LoadNonVolatileOperation::NormalizeLoadStoreState,
-        LoadNonVolatileOperation::NormalizeLoadLoadState });
+        LoadNonVolatileOperation::NormalizeLoadLoadState,
+        LoadNonVolatileOperation::NormalizeIOBarrierAllocaAddress });
 
   return rvsdg::NormalizeSequence<LoadNonVolatileOperation>(
       loadNodeNormalizations,
