69b93e64b4df2d2b61513165aa64d6e054a65006
Nico Reissmann <nico.reissmann@gmail.com>
2025-08-06 14:39:29 +0200

    Rename notifier class to Notifier (#1138)

--- jlm/rvsdg/notifiers.hpp
+++ jlm/rvsdg/notifiers.hpp
@@ -16,24 +16,24 @@ class Node;
 class Output;
 class Region;
 
-extern jlm::util::notifier<rvsdg::Region *> on_region_create;
-extern jlm::util::notifier<rvsdg::Region *> on_region_destroy;
+extern jlm::util::Notifier<rvsdg::Region *> on_region_create;
+extern jlm::util::Notifier<rvsdg::Region *> on_region_destroy;
 
-extern util::notifier<Node *> on_node_create;
-extern util::notifier<Node *> on_node_destroy;
-extern util::notifier<Node *, size_t> on_node_depth_change;
+extern util::Notifier<Node *> on_node_create;
+extern util::Notifier<Node *> on_node_destroy;
+extern util::Notifier<Node *, size_t> on_node_depth_change;
 
-extern jlm::util::notifier<jlm::rvsdg::Input *> on_input_create;
-extern jlm::util::notifier<
+extern jlm::util::Notifier<jlm::rvsdg::Input *> on_input_create;
+extern jlm::util::Notifier<
     jlm::rvsdg::Input *,
     jlm::rvsdg::Output *, /* old */
     jlm::rvsdg::Output *  /* new */
     >
     on_input_change;
-extern jlm::util::notifier<jlm::rvsdg::Input *> on_input_destroy;
+extern jlm::util::Notifier<jlm::rvsdg::Input *> on_input_destroy;
 
-extern jlm::util::notifier<jlm::rvsdg::Output *> on_output_create;
-extern jlm::util::notifier<jlm::rvsdg::Output *> on_output_destroy;
+extern jlm::util::Notifier<jlm::rvsdg::Output *> on_output_create;
+extern jlm::util::Notifier<jlm::rvsdg::Output *> on_output_destroy;
 
 }
 
