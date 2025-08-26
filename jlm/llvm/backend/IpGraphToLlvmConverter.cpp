705c92b476726e4b731d28aea385f9b1bba7400e
Nico Reissmann <nico.reissmann@gmail.com>
2025-08-26 08:19:05 +0200

    Support MemoryStateJoinOperation in IpGraphToLlvmConverter (#1185)

--- jlm/llvm/backend/IpGraphToLlvmConverter.cpp
+++ jlm/llvm/backend/IpGraphToLlvmConverter.cpp
@@ -1393,6 +1393,12 @@ IpGraphToLlvmConverter::convert_operation(
   {
     return convert<MemoryStateMergeOperation>(op, arguments, builder);
   }
+  if (is<MemoryStateJoinOperation>(op))
+  {
+    // This operation has no equivalent LLVM instruction.
+    // Nothing needs to be done.
+    return nullptr;
+  }
   if (is<MemoryStateSplitOperation>(op))
   {
     return convert<MemoryStateSplitOperation>(op, arguments, builder);
