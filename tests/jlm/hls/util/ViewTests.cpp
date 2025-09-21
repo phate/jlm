92b2f719ec5eb15d1e4949d29121af9fe72d4a77
Nico Reissmann <nico.reissmann@gmail.com>
2025-09-21 13:24:18 +0200

    HLS: Convert ThetaNodeConversion pass into a transformation (#1258)

--- tests/jlm/hls/util/ViewTests.cpp
+++ tests/jlm/hls/util/ViewTests.cpp
@@ -5,11 +5,10 @@
 
 #include "test-registry.hpp"
 
-#include <jlm/hls/backend/rvsdg2rhls/add-forks.hpp>
-#include <jlm/hls/backend/rvsdg2rhls/ThetaConversion.hpp>
 #include <jlm/hls/ir/hls.hpp>
 #include <jlm/hls/util/view.hpp>
 #include <jlm/llvm/ir/operators.hpp>
+#include <jlm/rvsdg/theta.hpp>
 #include <jlm/rvsdg/view.hpp>
 
 static void
