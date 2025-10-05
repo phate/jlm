964525a962419162ed33e45260b234d5186e8cc1
HKrogstie <krogstie.havard@gmail.com>
2025-10-05 17:24:22 +0200

    Update linkage.hpp

--- jlm/llvm/ir/linkage.hpp
+++ jlm/llvm/ir/linkage.hpp
@@ -16,7 +16,7 @@ namespace jlm::llvm
 
 /**
  * Types of linkage for global variables, constants and functions.
- * Based on LLVM's \ref ::llvm::GlobalValue::LinkageTypes
+ * Based on LLVM's ::llvm::GlobalValue::LinkageTypes
  */
 enum class linkage
 {
