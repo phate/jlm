0691042eb1edc5486f1784b73c2a68dba1d3cad5
Nico Reissmann <nico.reissmann@gmail.com>
2025-10-28 06:33:54 +0100

    add documentation

--- jlm/llvm/opt/PredicateCorrelation.hpp
+++ jlm/llvm/opt/PredicateCorrelation.hpp
@@ -30,6 +30,14 @@ class StatisticsCollector;
 namespace jlm::llvm
 {
 
+/**
+ * Predicate Correlation correlates the predicates between theta and gamma nodes, and
+ * redirects their respective predicates to match operation nodes.
+ *
+ * ### Theta-Gamma Predicate Correlation
+ * If a theta node's predicate originates from a gamma node with two control flow constants, then
+ * the theta node's predicate is redirected to the gamma node's predicate match node.
+ */
 class PredicateCorrelation final : public rvsdg::Transformation
 {
 public:
@@ -51,9 +59,23 @@ private:
   static void
   correlatePredicatesInRegion(rvsdg::Region & region);
 
+  /**
+   * Performs theta-gamma predicate correlation
+   *
+   * @param thetaNode The theta node for which to correlate.
+   */
   static void
   correlatePredicatesInTheta(rvsdg::ThetaNode & thetaNode);
 
+  /**
+   * Takes the output of a gamma node and if the output's respective branch results in every
+   * subregion originate from a control constant, then it returns a vector of the control constant
+   * alternatives.
+   *
+   * @param gammaOutput The output of a gamma node.
+   * @return The control constant alternatives for each of the gamma node's subregion, or
+   * std::nullopt;
+   */
   static std::optional<std::vector<size_t>>
   extractControlConstantAlternatives(const rvsdg::Output & gammaOutput);
 };
