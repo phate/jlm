9a9d0ce5488f15918fe09687508de0b934ab1ace
Nico Reissmann <nico.reissmann@gmail.com>
2025-12-29 21:27:46 +0100

    TestStructuralNode

--- tests/jlm/rvsdg/TestStructuralNode.cpp
+++ tests/jlm/rvsdg/TestStructuralNode.cpp
@@ -3,7 +3,7 @@
  * See COPYING for terms of redistribution.
  */
 
-#include <test-registry.hpp>
+#include <gtest/gtest.h>
 
 #include <jlm/rvsdg/graph.hpp>
 #include <jlm/rvsdg/TestNodes.hpp>
@@ -11,8 +11,7 @@
 
 #include <cassert>
 
-static void
-TestOutputRemoval()
+TEST(StructuralNodeTests, TestOutputRemoval)
 {
   using namespace jlm;
 
@@ -28,31 +27,23 @@ TestOutputRemoval()
   auto & output4 = structuralNode->addOutputOnly(valueType);
 
   // Act & Assert
-  assert(structuralNode->noutputs() == 5);
-  assert(output0.index() == 0);
-  assert(output1.index() == 1);
-  assert(output2.index() == 2);
-  assert(output3.index() == 3);
-  assert(output4.index() == 4);
+  EXPECT_EQ(structuralNode->noutputs(), 5);
+  EXPECT_EQ(output0.index(), 0);
+  EXPECT_EQ(output1.index(), 1);
+  EXPECT_EQ(output2.index(), 2);
+  EXPECT_EQ(output3.index(), 3);
+  EXPECT_EQ(output4.index(), 4);
 
   structuralNode->removeOutputAndResults(2);
-  assert(structuralNode->noutputs() == 4);
-  assert(output0.index() == 0);
-  assert(output1.index() == 1);
-  assert(output3.index() == 2);
-  assert(output4.index() == 3);
+  EXPECT_EQ(structuralNode->noutputs(), 4);
+  EXPECT_EQ(output0.index(), 0);
+  EXPECT_EQ(output1.index(), 1);
+  EXPECT_EQ(output3.index(), 2);
+  EXPECT_EQ(output4.index(), 3);
 
   structuralNode->removeOutputAndResults(3);
-  assert(structuralNode->noutputs() == 3);
-  assert(output0.index() == 0);
-  assert(output1.index() == 1);
-  assert(output3.index() == 2);
+  EXPECT_EQ(structuralNode->noutputs(), 3);
+  EXPECT_EQ(output0.index(), 0);
+  EXPECT_EQ(output1.index(), 1);
+  EXPECT_EQ(output3.index(), 2);
 }
-
-static void
-TestStructuralNode()
-{
-  TestOutputRemoval();
-}
-
-JLM_UNIT_TEST_REGISTER("jlm/rvsdg/TestStructuralNode", TestStructuralNode)
