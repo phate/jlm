7f489135e35e1b3aeae2f161bfd2edb9fde0316d
Nico Reissmann <nico.reissmann@gmail.com>
2026-01-03 09:33:42 +0100

    LoadTests.cpp

--- tests/jlm/llvm/frontend/llvm/LoadTests.cpp
+++ tests/jlm/llvm/frontend/llvm/LoadTests.cpp
@@ -3,7 +3,7 @@
  * See COPYING for terms of redistribution.
  */
 
-#include <test-registry.hpp>
+#include <gtest/gtest.h>
 
 #include <jlm/llvm/frontend/LlvmModuleConversion.hpp>
 #include <jlm/llvm/ir/operators/Load.hpp>
@@ -14,8 +14,7 @@
 #include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/Module.h>
 
-static void
-LoadConversion()
+TEST(LoadTests, LoadConversion)
 {
   using namespace llvm;
 
@@ -65,25 +64,23 @@ LoadConversion()
         auto ioStateAssignment = *std::next(it);
         auto memoryStateAssignment = *std::next(it, 2);
 
-        assert(is<AssignmentOperation>(ioStateAssignment->operation()));
-        assert(is<IOStateType>(ioStateAssignment->operand(0)->type()));
+        EXPECT_TRUE(is<AssignmentOperation>(ioStateAssignment->operation()));
+        EXPECT_TRUE(is<IOStateType>(ioStateAssignment->operand(0)->type()));
 
-        assert(is<AssignmentOperation>(memoryStateAssignment->operation()));
-        assert(is<MemoryStateType>(memoryStateAssignment->operand(0)->type()));
+        EXPECT_TRUE(is<AssignmentOperation>(memoryStateAssignment->operation()));
+        EXPECT_TRUE(is<MemoryStateType>(memoryStateAssignment->operand(0)->type()));
       }
       else if (is<LoadNonVolatileOperation>(*it))
       {
         numLoadThreeAddressCodes++;
         auto memoryStateAssignment = *std::next(it, 1);
 
-        assert(is<AssignmentOperation>(memoryStateAssignment->operation()));
-        assert(is<MemoryStateType>(memoryStateAssignment->operand(0)->type()));
+        EXPECT_TRUE(is<AssignmentOperation>(memoryStateAssignment->operation()));
+        EXPECT_TRUE(is<MemoryStateType>(memoryStateAssignment->operand(0)->type()));
       }
     }
 
-    assert(numLoadThreeAddressCodes == 1);
-    assert(numLoadVolatileThreeAddressCodes == 2);
+    EXPECT_EQ(numLoadThreeAddressCodes, 1);
+    EXPECT_EQ(numLoadVolatileThreeAddressCodes, 2);
   }
 }
-
-JLM_UNIT_TEST_REGISTER("jlm/llvm/frontend/llvm/LoadTests-LoadConversion", LoadConversion)
