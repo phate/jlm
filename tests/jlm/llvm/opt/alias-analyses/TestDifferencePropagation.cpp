796f9af8684276825d267692854902b7be27ceca
Nico Reissmann <nico.reissmann@gmail.com>
2026-01-03 23:13:31 +0100

    TestDifferencePropagation.cpp

--- tests/jlm/llvm/opt/alias-analyses/TestDifferencePropagation.cpp
+++ tests/jlm/llvm/opt/alias-analyses/TestDifferencePropagation.cpp
@@ -3,16 +3,13 @@
  * See COPYING for terms of redistribution.
  */
 
-#include <test-registry.hpp>
+#include <gtest/gtest.h>
 
 #include <jlm/llvm/opt/alias-analyses/DifferencePropagation.hpp>
 #include <jlm/llvm/opt/alias-analyses/PointerObjectSet.hpp>
 #include <jlm/llvm/TestRvsdgs.hpp>
 
-#include <cassert>
-
-static void
-TestTracksDifferences()
+TEST(DifferencePropagationTests, TestTracksDifferences)
 {
   using namespace jlm;
   using namespace jlm::llvm::aa;
@@ -38,21 +35,21 @@ TestTracksDifferences()
   differencePropagation.Initialize();
 
   // Assert
-  assert(differencePropagation.GetNewPointees(r0) == (util::HashSet<PointerObjectIndex>{ a0, a3 }));
+  EXPECT_EQ(differencePropagation.GetNewPointees(r0), (util::HashSet{ a0, a3 }));
 
   // Act 2 - add another pointer/pointee relation: r1 -> a1
   differencePropagation.AddToPointsToSet(r1, a1);
 
   // Assert that a1 is a new pointee of r1
-  assert(differencePropagation.GetNewPointees(r1) == (util::HashSet<PointerObjectIndex>{ a1 }));
+  EXPECT_EQ(differencePropagation.GetNewPointees(r1), util::HashSet{ a1 });
 
   // Act 3 - clear difference tracking for r1
   differencePropagation.ClearNewPointees(r1);
 
   // Assert r1 no longer has any new pointees
-  assert(differencePropagation.GetNewPointees(r1).IsEmpty());
+  EXPECT_TRUE(differencePropagation.GetNewPointees(r1).IsEmpty());
   // r0 still has new pointees
-  assert(!differencePropagation.GetNewPointees(r0).IsEmpty());
+  EXPECT_FALSE(differencePropagation.GetNewPointees(r0).IsEmpty());
 
   // Act 4 - add more pointees to r1,
   bool new0 = differencePropagation.AddToPointsToSet(r1, a0);
@@ -60,8 +57,8 @@ TestTracksDifferences()
   bool new2 = differencePropagation.AddToPointsToSet(r1, a2);
 
   // Assert that only a0 and a2 were new
-  assert(new0 && !new1 && new2);
-  assert(differencePropagation.GetNewPointees(r1) == (util::HashSet<PointerObjectIndex>{ a0, a2 }));
+  EXPECT_TRUE(new0 && !new1 && new2);
+  EXPECT_EQ(differencePropagation.GetNewPointees(r1), util::HashSet({ a0, a2 }));
 
   // Act 5 - make r0 point to a superset of r1, making r0 now point to a0, a1, a2, a3
   // First mark the existing pointees of r0 (a0 and a3) as seen
@@ -69,25 +66,25 @@ TestTracksDifferences()
   differencePropagation.MakePointsToSetSuperset(r0, r1);
 
   // Assert that only a1 and a2 are new to r0, as it has already marked a0 and a3 as seen
-  assert(differencePropagation.GetNewPointees(r0) == (util::HashSet<PointerObjectIndex>{ a1, a2 }));
+  EXPECT_EQ(differencePropagation.GetNewPointees(r0), util::HashSet({ a1, a2 }));
 
   // Act 6 - give nodes r0 and r1 flags
   set.MarkAsPointeesEscaping(r0);
   set.MarkAsPointingToExternal(r1);
 
   // Assert that the flags are new, but only if they actually have the flag
-  assert(differencePropagation.PointeesEscapeIsNew(r0));
-  assert(!differencePropagation.PointsToExternalIsNew(r0));
-  assert(!differencePropagation.PointeesEscapeIsNew(r1));
-  assert(differencePropagation.PointsToExternalIsNew(r1));
+  EXPECT_TRUE(differencePropagation.PointeesEscapeIsNew(r0));
+  EXPECT_FALSE(differencePropagation.PointsToExternalIsNew(r0));
+  EXPECT_FALSE(differencePropagation.PointeesEscapeIsNew(r1));
+  EXPECT_TRUE(differencePropagation.PointsToExternalIsNew(r1));
 
   // Act 7 - mark flags as seen
   differencePropagation.MarkPointeesEscapeAsHandled(r0);
   differencePropagation.MarkPointsToExternalAsHandled(r1);
 
   // Assert that the flags are no longer new
-  assert(!differencePropagation.PointeesEscapeIsNew(r0));
-  assert(!differencePropagation.PointsToExternalIsNew(r1));
+  EXPECT_FALSE(differencePropagation.PointeesEscapeIsNew(r0));
+  EXPECT_FALSE(differencePropagation.PointsToExternalIsNew(r1));
 
   // Act 6 - unify 0 and 1
   // After unification, any pointee or flag that is new to either node becomes new to the union
@@ -99,13 +96,9 @@ TestTracksDifferences()
   // a0 and a2 were still marked as new to node r1 at the time of unification.
   // a3 is not new to r0, but r1 has never seen it, so it must be regarded as new by the union.
   util::HashSet<PointerObjectIndex> subset{ a0, a2, a3 };
-  assert(subset.IsSubsetOf(differencePropagation.GetNewPointees(root)));
+  EXPECT_TRUE(subset.IsSubsetOf(differencePropagation.GetNewPointees(root)));
 
   // Neither flag has been seen by both nodes, so they are both new to the unification
-  assert(differencePropagation.PointeesEscapeIsNew(root));
-  assert(differencePropagation.PointsToExternalIsNew(root));
+  EXPECT_TRUE(differencePropagation.PointeesEscapeIsNew(root));
+  EXPECT_TRUE(differencePropagation.PointsToExternalIsNew(root));
 }
-
-JLM_UNIT_TEST_REGISTER(
-    "jlm/llvm/opt/alias-analyses/TestDifferencePropagation-TestTracksDifferences",
-    TestTracksDifferences)
