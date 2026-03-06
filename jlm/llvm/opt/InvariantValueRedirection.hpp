fc073daf27b220bd17594a2925fdfbc6a4679464
Nico Reissmann <nico.reissmann@gmail.com>
2026-03-06 05:57:58 +0100

    InvariantValueRedirection: Make transformation configurable

--- jlm/llvm/opt/InvariantValueRedirection.hpp
+++ jlm/llvm/opt/InvariantValueRedirection.hpp
@@ -23,32 +23,25 @@ namespace jlm::llvm
 
 class ThetaGammaPredicateCorrelation;
 
-/** \brief Invariant Value Redirection Optimization
+/** \brief Invariant Value Redirection
  *
- * Invariant Value Redirection (IVR) redirects invariant edges around gamma, theta, and call nodes.
- * It does this by diverting all users of invariant gamma and theta outputs to the origin of the
- * respective gamma and theta inputs. For call nodes, it redirects all users of invariant call
- * outputs to the origin of the respective call input. In case of nested nodes, the optimization
+ * Invariant Value Redirection (IVR) redirects invariant edges around gamma, theta, call, and load nodes.
+ * It does this by diverting all users of invariant outputs to the origin of the
+ * respective inputs. In case of nested nodes, the transformation
  * processes the innermost nodes first to ensure that the outputs of outer nodes are correctly
  * identified as invariant. Moreover, IVR processes a lambda node before all the lambda's call nodes
- * to ensure that the outputs of call nodes are correctly identified as invariant.
+ * to ensure that the outputs of call nodes are correctly identified as invariant. Nodes that become
+ * dead throughout the transformation are pruned from a region.
  *
- * ### Theta nodes
- * A loop variable is considered invariant if its post value is connected to its corresponding pre
- * value. All the users of the loop variables' output are diverted to the origin of the
- * corresponding input. See rvsdg::ThetaLoopVarIsInvariant() for more details.
- *
- * ### Gamma nodes
+ * ### Gamma output redirection
  * The output of a gamma node is considered invariant if all the corresponding region results are
  * connected to the arguments of a single gamma input. All the users of a gamma output are diverted
  * to the origin of this gamma input.
  *
- * ### Call nodes
- * The output of a call node is considered invariant if the respective result of the corresponding
- * lambda is connected to an argument of the lambda. All the users of a call output are diverted to
- * the origin of the call input corresponding to the lambda argument. Invariant Value Redirection
- * for call nodes works only on non-recursive direct calls as IVR needs to inspect the lambda body
- * in order to determine whether a value is simply routed through the lambda.
+ * ### Theta output redirection
+ * A loop variable is considered invariant if its post value is connected to its corresponding pre
+ * value. All the users of the loop variables' output are diverted to the origin of the
+ * corresponding input. See rvsdg::ThetaLoopVarIsInvariant() for more details.
  *
  * ### Theta nodes with a predicate that correlates with a gamma node
  * If the theta node has a gamma node in its subregion and for both nodes the predicates correlate,
@@ -72,36 +65,58 @@ class ThetaGammaPredicateCorrelation;
  * 2. If the loop variable's pre value is dead, then only the exit value of the loop variable is of
  * interest. This means that we can redirect the value from the respective entry variable of the
  * gamma node's exit subregion to the post value.
+ *
+ * ### Call nodes
+ * The output of a call node is considered invariant if the respective result of the corresponding
+ * lambda is connected to an argument of the lambda. All the users of a call output are diverted to
+ * the origin of the call input corresponding to the lambda argument. Invariant Value Redirection
+ * for call nodes works only on non-recursive direct calls as IVR needs to inspect the lambda body
+ * in order to determine whether a value is simply routed through the lambda.
+ *
+ * ### Load nodes
+ * The memory states of a load node can be diverted to their respective input's origin if the loaded
+ * value output of the load node is dead.
+ *
  */
 class InvariantValueRedirection final : public rvsdg::Transformation
 {
   class Statistics;
 
 public:
+  struct Configuration
+  {
+    bool enableGammaOutputRedirection = true;
+    bool enableThetaOutputRedirection = true;
+    bool enableThetaGammaCorrelationRedirection = true;
+    bool enableCallOutputRedirection = true;
+    bool enableLoadMemoryStateRedirection = true;
+  };
+
   ~InvariantValueRedirection() override;
 
-  InvariantValueRedirection()
-      : Transformation("InvariantValueRedirection")
+  explicit InvariantValueRedirection(Configuration configuration)
+      : Transformation("InvariantValueRedirection"),
+        configuration_(std::move(configuration))
   {}
 
   void
   Run(rvsdg::RvsdgModule & module, util::StatisticsCollector & statisticsCollector) override;
 
 private:
-  static void
-  RedirectInRootRegion(rvsdg::Graph & rvsdg);
+  void
+  redirectInRootRegion(rvsdg::Graph & rvsdg);
+
+  void
+  redirectInRegion(rvsdg::Region & region);
+
+  void
+  redirectInSubregions(rvsdg::StructuralNode & structuralNode);
 
   static void
-  RedirectInRegion(rvsdg::Region & region);
+  redirectGammaOutputs(rvsdg::GammaNode & gammaNode);
 
   static void
-  RedirectInSubregions(rvsdg::StructuralNode & structuralNode);
-
-  static void
-  RedirectGammaOutputs(rvsdg::GammaNode & gammaNode);
-
-  static void
-  RedirectThetaOutputs(rvsdg::ThetaNode & thetaNode);
+  redirectThetaOutputs(rvsdg::ThetaNode & thetaNode);
 
   /**
    * Redirects invariant loop variables from theta nodes of which thet predicate statically
@@ -113,7 +128,7 @@ private:
   redirectThetaGammaOutputs(rvsdg::ThetaNode & thetaNode);
 
   static void
-  RedirectCallOutputs(rvsdg::SimpleNode & callNode);
+  redirectCallOutputs(rvsdg::SimpleNode & callNode);
 
   /**
    * Redirects the load node's users of the memory state outputs to the origins' of the respective
@@ -123,6 +138,8 @@ private:
    */
   static void
   redirectLoadMemoryStates(rvsdg::SimpleNode & loadNode);
+
+  Configuration configuration_{};
 };
 
 }
