9efc1e93610e468262c2b41cd9e67f787280a9b2
HKrogstie <krogstie.havard@gmail.com>
2026-04-15 07:55:40 +0200

    Add json output for graph dumps (#1596)
    Adds the option of outputting json from the `GraphWriter` API. The json
    should take up much less space than the existing dot output.
    
    The [viewer](https://dot-tree-viz.vercel.app/) has been updated to
    support converting graphs from json into GraphViz, and does so on an
    ad-hoc basis.
    
    With this implementation, there is some dupliction between the dot
    output in C++, and the json->dot conversion in TypeScript in the viewer.
    At some point in the future it would be nice to only have a single path,
    but that can be left for later.
    
    Also, the ASCII output from the `GraphWriter` is never really used, so
    it could also probably be removed at some point.

--- jlm/tooling/Command.hpp
+++ jlm/tooling/Command.hpp
@@ -9,6 +9,7 @@
 #include <jlm/tooling/CommandGraph.hpp>
 #include <jlm/tooling/CommandLine.hpp>
 #include <jlm/util/file.hpp>
+#include <jlm/util/GraphWriter.hpp>
 
 #include <memory>
 #include <string>
@@ -416,9 +417,10 @@ private:
       util::StatisticsCollector & statisticsCollector);
 
   static void
-  PrintAsDot(
+  PrintAsGraphs(
       const llvm::LlvmRvsdgModule & rvsdgModule,
       const util::FilePath & outputFile,
+      jlm::util::graph::OutputFormat format,
       util::StatisticsCollector & statisticsCollector);
 
   [[nodiscard]] std::vector<std::shared_ptr<rvsdg::Transformation>>
