07075f01c467a2c047cd6a6805baf867f4801fdf
Nico Reissmann <nico.reissmann@gmail.com>
2026-05-01 15:53:52 +0200

    add memset operation

--- jlm/llvm/ir/operators/StdLibIntrinsicOperations.hpp
+++ jlm/llvm/ir/operators/StdLibIntrinsicOperations.hpp
@@ -3,8 +3,8 @@
  * See COPYING for terms of redistribution.
  */
 
-#ifndef JLM_LLVM_IR_OPERATORS_MEMCPY_HPP
-#define JLM_LLVM_IR_OPERATORS_MEMCPY_HPP
+#ifndef JLM_LLVM_IR_OPERATORS_STDLIBINTRINSICOPERATIONS_HPP
+#define JLM_LLVM_IR_OPERATORS_STDLIBINTRINSICOPERATIONS_HPP
 
 #include <jlm/llvm/ir/tac.hpp>
 #include <jlm/llvm/ir/types.hpp>
@@ -333,6 +333,188 @@ private:
   }
 };
 
+/**
+ * Abstract base class for memset operations.
+ *
+ * @see MemSetNonVolatileOperation
+ */
+class MemSetOperation : public rvsdg::SimpleOperation
+{
+protected:
+  MemSetOperation(
+      const std::vector<std::shared_ptr<const rvsdg::Type>> & operandTypes,
+      const std::vector<std::shared_ptr<const rvsdg::Type>> & resultTypes)
+      : SimpleOperation(operandTypes, resultTypes)
+  {
+    JLM_ASSERT(operandTypes.size() >= 4);
+
+    auto & dstAddressType = *operandTypes[0];
+    JLM_ASSERT(is<PointerType>(dstAddressType));
+
+    if (auto & valueType = *operandTypes[1]; valueType != *rvsdg::BitType::Create(8))
+    {
+      throw std::runtime_error("Expected 8 bit integer type.");
+    }
+
+    if (auto & lengthType = *operandTypes[2];
+        lengthType != *rvsdg::BitType::Create(32) && lengthType != *rvsdg::BitType::Create(64))
+    {
+      throw util::Error("Expected 32 bit or 64 bit integer type.");
+    }
+
+    if (auto & memoryStateType = *operandTypes.back(); !is<MemoryStateType>(memoryStateType))
+    {
+      throw util::Error("Number of memory states cannot be zero.");
+    }
+  }
+
+public:
+  /**
+   * @return The type of the length argument
+   */
+  [[nodiscard]] const rvsdg::BitType &
+  lengthType() const noexcept
+  {
+    const auto type = std::dynamic_pointer_cast<const rvsdg::BitType>(argument(2));
+    JLM_ASSERT(type != nullptr);
+    JLM_ASSERT(type->nbits() == 32 || type->nbits() == 64);
+    return *type;
+  }
+
+  /**
+   * @return Number of memory states
+   */
+  [[nodiscard]] virtual size_t
+  numMemoryStates() const noexcept = 0;
+
+  /**
+   * @param node a SimpleNode containing a MemSetOperation
+   * @return the input of \p node that points to the destination to fill.
+   */
+  [[nodiscard]] static rvsdg::Input &
+  destinationInput(const rvsdg::Node & node) noexcept
+  {
+    JLM_ASSERT(is<MemSetOperation>(&node));
+    const auto input = node.input(0);
+    JLM_ASSERT(is<PointerType>(input->Type()));
+    return *input;
+  }
+
+  /**
+   * @param node a SimpleNode containing a MemSetOperation
+   * @return the input of \p node that points to the byte value with which to fill the memory
+   */
+  [[nodiscard]] static rvsdg::Input &
+  valueInput(const rvsdg::Node & node) noexcept
+  {
+    JLM_ASSERT(is<MemSetOperation>(&node));
+    const auto input = node.input(1);
+    JLM_ASSERT(*input->Type() == *rvsdg::BitType::Create(8));
+    return *input;
+  }
+
+  /**
+   * @param node a SimpleNode containing a MemSetOperation
+   * @return the input of \p node that points to the number of bytes that need to be filled.
+   */
+  [[nodiscard]] static rvsdg::Input &
+  lengthInput(const rvsdg::Node & node) noexcept
+  {
+    JLM_ASSERT(is<MemSetOperation>(&node));
+    const auto input = node.input(2);
+    JLM_ASSERT(
+        *input->Type() == *rvsdg::BitType::Create(32)
+        || *input->Type() == *rvsdg::BitType::Create(64));
+    return *input;
+  }
+};
+
+/**
+ * Represents a non-volatile LLVM memset intrinsic
+ *
+ * See [LLVM Language Reference
+ * Manual](https://llvm.org/docs/LangRef.html#llvm-memset-inline-intrinsic) for more details.
+ */
+class MemSetNonVolatileOperation final : public MemSetOperation
+{
+public:
+  ~MemSetNonVolatileOperation() noexcept override;
+
+  MemSetNonVolatileOperation(std::shared_ptr<const rvsdg::Type> lengthType, size_t numMemoryStates)
+      : MemSetOperation(
+            createOperandTypes(std::move(lengthType), numMemoryStates),
+            createResultTypes(numMemoryStates))
+  {}
+
+  bool
+  operator==(const Operation & other) const noexcept override;
+
+  [[nodiscard]] std::string
+  debug_string() const override;
+
+  [[nodiscard]] std::unique_ptr<Operation>
+  copy() const override;
+
+  [[nodiscard]] size_t
+  numMemoryStates() const noexcept override;
+
+  static std::unique_ptr<ThreeAddressCode>
+  createTac(
+      const Variable & destination,
+      const Variable & value,
+      const Variable & length,
+      const std::vector<const Variable *> & memoryStates)
+  {
+    std::vector operands = { &destination, &value, &length };
+    operands.insert(operands.end(), memoryStates.begin(), memoryStates.end());
+
+    auto operation =
+        std::make_unique<MemCpyNonVolatileOperation>(length.Type(), memoryStates.size());
+    return ThreeAddressCode::create(std::move(operation), operands);
+  }
+
+  static rvsdg::SimpleNode &
+  createNode(
+      rvsdg::Output & destination,
+      rvsdg::Output & value,
+      rvsdg::Output & length,
+      const std::vector<rvsdg::Output *> & memoryStates)
+  {
+    std::vector operands = { &destination, &value, &length };
+    operands.insert(operands.end(), memoryStates.begin(), memoryStates.end());
+
+    return rvsdg::CreateOpNode<MemSetNonVolatileOperation>(
+        operands,
+        length.Type(),
+        memoryStates.size());
+  }
+
+private:
+  static std::vector<std::shared_ptr<const rvsdg::Type>>
+  createOperandTypes(
+      const std::shared_ptr<const rvsdg::Type> & lengthType,
+      const size_t numMemoryStates)
+  {
+    const auto pointerType = PointerType::Create();
+    const auto & valueType = rvsdg::BitType::Create(8);
+    std::vector<std::shared_ptr<const rvsdg::Type>> types = { pointerType, valueType, lengthType };
+    types.insert(types.end(), numMemoryStates, MemoryStateType::Create());
+    return types;
+  }
+
+  static std::vector<std::shared_ptr<const rvsdg::Type>>
+  createResultTypes(size_t numMemoryStates)
+  {
+    // The memset() standard C library call has as return type void*, but LLVM models the
+    // function call nevertheless as:
+    // call void @llvm.memset.p0.p0.i64(ptr align 4 %7, ptr align 4 %8, i64 %9, i1 false)
+    //
+    // LLVM simply hands in register %7 (dest pointer) to all users of the return type of memset().
+    // Thus, we only need state types as result types of the operation here.
+    return { numMemoryStates, MemoryStateType::Create() };
+  }
+};
+
 }
 
-#endif // JLM_LLVM_IR_OPERATORS_MEMCPY_HPP
+#endif
