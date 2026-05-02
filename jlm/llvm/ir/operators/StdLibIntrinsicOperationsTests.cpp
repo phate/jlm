c389ed21c66324ac17601ccbf0144d40805b6f2e
Nico Reissmann <nico.reissmann@gmail.com>
2026-05-02 11:09:42 +0200

    operation test

--- jlm/llvm/ir/operators/StdLibIntrinsicOperationsTests.cpp
+++ jlm/llvm/ir/operators/StdLibIntrinsicOperationsTests.cpp
@@ -10,7 +10,7 @@
 #include <jlm/rvsdg/TestOperations.hpp>
 #include <jlm/rvsdg/TestType.hpp>
 
-TEST(MemCpyOperationTests, operationEquality)
+TEST(StdLibIntrinsicOperationsTests, memcpyOperationEqualityTest)
 {
   using namespace jlm::llvm;
   using namespace jlm::rvsdg;
@@ -30,7 +30,7 @@ TEST(MemCpyOperationTests, operationEquality)
   EXPECT_NE(operation1, operation3); // number of memory states differs
 }
 
-TEST(MemCpyOperationTests, accessors)
+TEST(StdLibIntrinsicOperationsTests, memcpyAccessorsTest)
 {
   using namespace jlm::llvm;
 
@@ -56,7 +56,7 @@ TEST(MemCpyOperationTests, accessors)
   EXPECT_EQ(MemCpyOperation::countInput(memCpy).origin(), constant100.output(0));
 }
 
-TEST(MemCpyOperationTests, mapMemoryStateInputToOutput)
+TEST(StdLibIntrinsicOperationsTests, memcpyMapMemoryStateInputToOutput)
 {
   using namespace jlm::llvm;
 
@@ -104,7 +104,7 @@ TEST(MemCpyOperationTests, mapMemoryStateInputToOutput)
       memCpyVolatile.output(2));
 }
 
-TEST(MemCpyOperationTests, mapMemoryStateOutputToInput)
+TEST(StdLibIntrinsicOperationsTests, memcpyMapMemoryStateOutputToInput)
 {
   using namespace jlm::llvm;
 
@@ -149,5 +149,118 @@ TEST(MemCpyOperationTests, mapMemoryStateOutputToInput)
       &memState1);
   EXPECT_EQ(
       MemCpyOperation::mapMemoryStateOutputToInput(*memCpyVolatile.output(2)).origin(),
+      &memState2);
+}
+
+TEST(StdLibIntrinsicOperationsTests, memsetOperationEquality)
+{
+  using namespace jlm::llvm;
+  using namespace jlm::rvsdg;
+
+  // Arrange
+  auto valueType = TestType::createValueType();
+  auto bit32Type = BitType::Create(32);
+  auto bit64Type = BitType::Create(64);
+
+  MemSetNonVolatileOperation operation1(bit32Type, 1);
+  MemSetNonVolatileOperation operation2(bit64Type, 4);
+  TestOperation operation3({ valueType }, { valueType });
+
+  // Act & Assert
+  EXPECT_EQ(operation1, operation1);
+  EXPECT_NE(operation1, operation2); // length type differs
+  EXPECT_NE(operation1, operation3); // number of memory states differs
+}
+
+TEST(StdLibIntrinsicOperationsTests, memsetAccessorsTest)
+{
+  using namespace jlm::llvm;
+
+  // Arrange
+  auto pointerType = PointerType::Create();
+  auto bit8Type = jlm::rvsdg::BitType::Create(8);
+  auto bit32Type = jlm::rvsdg::BitType::Create(32);
+  auto memStateType = MemoryStateType::Create();
+
+  jlm::rvsdg::Graph graph;
+  auto & address = jlm::rvsdg::GraphImport::Create(graph, pointerType, "address1");
+  auto & value = jlm::rvsdg::GraphImport::Create(graph, bit8Type, "value");
+  auto & memState = jlm::rvsdg::GraphImport::Create(graph, memStateType, "memState");
+  auto & constant100 = IntegerConstantOperation::Create(graph.GetRootRegion(), 32, 100);
+
+  auto & memsetNode = jlm::rvsdg::CreateOpNode<MemSetNonVolatileOperation>(
+      { &address, &value, constant100.output(0), &memState },
+      bit32Type,
+      1);
+
+  // Act & Assert
+  EXPECT_EQ(MemSetOperation::destinationInput(memsetNode).origin(), &address);
+  EXPECT_EQ(MemSetOperation::valueInput(memsetNode).origin(), &value);
+  EXPECT_EQ(MemSetOperation::lengthInput(memsetNode).origin(), constant100.output(0));
+}
+
+TEST(StdLibIntrinsicOperationsTests, memsetMapMemoryStateInputToOutput)
+{
+  using namespace jlm::llvm;
+
+  // Arrange
+  auto pointerType = PointerType::Create();
+  auto bit8Type = jlm::rvsdg::BitType::Create(8);
+  auto bit32Type = jlm::rvsdg::BitType::Create(32);
+  auto memStateType = MemoryStateType::Create();
+
+  jlm::rvsdg::Graph graph;
+  auto & address = jlm::rvsdg::GraphImport::Create(graph, pointerType, "address1");
+  auto & value = jlm::rvsdg::GraphImport::Create(graph, bit8Type, "value");
+  auto & memState1 = jlm::rvsdg::GraphImport::Create(graph, memStateType, "memState1");
+  auto & memState2 = jlm::rvsdg::GraphImport::Create(graph, memStateType, "memState2");
+
+  auto & constant100 = IntegerConstantOperation::Create(graph.GetRootRegion(), 32, 100);
+
+  auto & memsetNode = MemSetNonVolatileOperation::createNode(
+      address,
+      value,
+      *constant100.output(0),
+      { &memState1, &memState2 });
+
+  // Act & Assert
+  EXPECT_EQ(
+      &MemSetOperation::mapMemoryStateInputToOutput(*memsetNode.input(3)),
+      memsetNode.output(0));
+  EXPECT_EQ(
+      &MemSetOperation::mapMemoryStateInputToOutput(*memsetNode.input(4)),
+      memsetNode.output(1));
+}
+
+TEST(StdLibIntrinsicOperationsTests, memsetMapMemoryStateOutputToInput)
+{
+  using namespace jlm::llvm;
+
+  // Arrange
+  auto pointerType = PointerType::Create();
+  auto bit8Type = jlm::rvsdg::BitType::Create(8);
+  auto bit32Type = jlm::rvsdg::BitType::Create(32);
+  auto memStateType = MemoryStateType::Create();
+
+  jlm::rvsdg::Graph graph;
+  auto & address = jlm::rvsdg::GraphImport::Create(graph, pointerType, "address1");
+  auto & value = jlm::rvsdg::GraphImport::Create(graph, bit8Type, "value");
+  auto & memState1 = jlm::rvsdg::GraphImport::Create(graph, memStateType, "memState1");
+  auto & memState2 = jlm::rvsdg::GraphImport::Create(graph, memStateType, "memState2");
+
+  auto & constant100 = IntegerConstantOperation::Create(graph.GetRootRegion(), 32, 100);
+
+  auto & memsetNode = MemSetNonVolatileOperation::createNode(
+      address,
+      value,
+      *constant100.output(0),
+      { &memState1, &memState2 });
+
+  // Act & Assert
+  EXPECT_EQ(
+      MemSetOperation::mapMemoryStateOutputToInput(*memsetNode.output(0)).origin(),
+      &memState1);
+  EXPECT_EQ(
+      MemSetOperation::mapMemoryStateOutputToInput(*memsetNode.output(1)).origin(),
       &memState2);
 }
