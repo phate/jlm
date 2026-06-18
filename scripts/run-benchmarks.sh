581e8f27e31cfe76eb1136de24374b76ce62095f
Håvard Krogstie <krogstie.havard@gmail.com>
2026-06-18 16:00:06 +0200

    make benchmarks CI job fail when the benchmarking fails

--- scripts/run-benchmarks.sh
+++ scripts/run-benchmarks.sh
@@ -3,7 +3,7 @@ set -eu +x
 
 # URL to the benchmark git repository and the commit to be used
 GIT_REPOSITORY=https://github.com/haved/jlm-benchmark.git
-GIT_COMMIT=5e0451d4ba7282b2a152787aef98de09403bf4cb
+GIT_COMMIT=50bf014fe8dacfaad7bbbfdd620b5b839518153c
 
 # Get the absolute path to this script and set default JLM paths
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
@@ -20,8 +20,8 @@ else
 fi
 
 APT_INSTALL_DEPS=false
-CLEAN=false
-BENCHMARK=""
+CLEAN_RUNS=false
+RUN_OPTIONS=""
 
 function commit()
 {
@@ -35,12 +35,12 @@ function usage()
 	echo "  --path PATH           The path where to place the benchmarks."
 	echo "                        Default=[${BENCHMARK_DIR}]"
 	echo "  --apt-install-deps    For CI runner or Ubuntu 24. Installs apt package dependencies before running."
+	echo "  --ci                  Use a benchmark configuration intended for running and validating in CI."
  	echo "  --parallel #THREADS   The number of threads to run in parallel."
 	echo "                        Default=[${NUM_PARALLEL_THREADS}]"
 	echo "  --benchmark BENCH     Only extract and build a specific benchamrk."
-	echo "                        Default=[ALL]"
-	echo "                        BENCH=[polybench|spec|emacs|ghostscript|gdb|sendmail]"
-	echo "  --clean               Delete extracted sources and build files."
+	echo "                        Default=[ALL]. See list of options in benchmark's run.sh"
+	echo "  --clean-runs          Delete build files and statistics from previous runs."
 	echo "  --get-commit-hash     Prints the commit hash used for the build."
 	echo "  --help                Prints this message and stops."
 }
@@ -60,6 +60,10 @@ while [[ "$#" -ge 1 ]] ; do
 			APT_INSTALL_DEPS=true
 			shift
 			;;
+		--ci)
+			RUN_OPTIONS="${RUN_OPTIONS} --ci"
+			shift
+			;;
 		--parallel)
 			shift
 			NUM_PARALLEL_THREADS=$1
@@ -67,7 +71,7 @@ while [[ "$#" -ge 1 ]] ; do
 			;;
 		--benchmark)
 			shift
-			BENCHMARK="--$1"
+			RUN_OPTIONS="${RUN_OPTIONS} --$1"
 			shift
 			;;
 		--get-commit-hash)
@@ -103,9 +107,9 @@ if [[ "${APT_INSTALL_DEPS}" = true ]]; then
 	sudo ./apt-install-dependencies.sh
 fi
 
-if [ "${CLEAN}" = true ]; then
-	./run.sh --clean
-	exit  0
+if [ "${CLEAN_RUNS}" = true ]; then
+	./run.sh clean-runs
+	exit 0
 fi
 
-./run.sh --jlm-opt "${JLM_ROOT_DIR}/build/jlm-opt" --llvm-config "${LLVMCONFIG}" --parallel "${NUM_PARALLEL_THREADS}" ${BENCHMARK}
+./run.sh --jlm-opt "${JLM_ROOT_DIR}/build/jlm-opt" --llvm-config "${LLVMCONFIG}" --parallel "${NUM_PARALLEL_THREADS}" ${RUN_OPTIONS}
