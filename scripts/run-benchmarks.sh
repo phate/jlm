6551c3d45b67206c006523512fc47728a5516d60
Håvard Krogstie <krogstie.havard@gmail.com>
2026-06-19 16:57:04 +0200

    update benchmark hash to remove absolute paths from sources.json

--- scripts/run-benchmarks.sh
+++ scripts/run-benchmarks.sh
@@ -3,7 +3,7 @@ set -eu +x
 
 # URL to the benchmark git repository and the commit to be used
 GIT_REPOSITORY=https://github.com/haved/jlm-benchmark.git
-GIT_COMMIT=48d89bd28a2075976ea7f1dc0b4278fc0bf398b7
+GIT_COMMIT=989c9f8d55504affe88b6c474b05ad9c6a954c8c
 
 # Get the absolute path to this script and set default JLM paths
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
