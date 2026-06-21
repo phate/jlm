cfda78eacb3c69f568b86c37078339c8364d03dc
Mega User <mega@example.com>
2026-06-21 13:06:52 +0200

    Tidy

--- jlm/hls/backend/rhls2firrtl/RhlsToFirrtlConverterTests.cpp
+++ jlm/hls/backend/rhls2firrtl/RhlsToFirrtlConverterTests.cpp
@@ -747,7 +747,7 @@ TEST_F(FirrtlTestBase, MemoryStateMergeOperation)
   auto & arg0 = *Lambda_->GetFunctionArguments()[0];
   auto & arg1 = *Lambda_->GetFunctionArguments()[1];
   auto * mergeOutput =
-      MemoryStateMergeOperation::Create(std::vector<rvsdg::Output *>{ &arg0, &arg1 });
+      MemoryStateMergeOperation::Create(std::vector<jlm::rvsdg::Output *>{ &arg0, &arg1 });
   Lambda_->finalize({ mergeOutput });
 
   TestableRhlsToFirrtlConverter converter;
