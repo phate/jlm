9747f9ff224d452af53ae2a63b39b5956ba30c4a
Nico Reissmann <nico.reissmann@gmail.com>
2026-07-09 19:53:06 +0200

    LLVM: Add constant folding for SExtOperation

--- jlm/llvm/ir/operators/ConversionOperationsTests.cpp
+++ jlm/llvm/ir/operators/ConversionOperationsTests.cpp
@@ -6,13 +6,16 @@
 #include <gtest/gtest.h>
 
 #include <jlm/llvm/ir/operators/ConversionOperations.hpp>
+#include <jlm/llvm/ir/operators/IntegerOperations.hpp>
 #include <jlm/llvm/ir/operators/operators.hpp>
-#include <jlm/rvsdg/bitstring/arithmetic.hpp>
 #include <jlm/rvsdg/bitstring/constant.hpp>
 #include <jlm/rvsdg/bitstring/value-representation.hpp>
 #include <jlm/rvsdg/NodeNormalization.hpp>
 #include <jlm/rvsdg/simple-node.hpp>
 #include <jlm/rvsdg/view.hpp>
+
+namespace jlm::llvm
+{
 
 TEST(ConversionOperationsTests, testSextZextConstantReductions)
 {
@@ -26,7 +29,7 @@ TEST(ConversionOperationsTests, testSextZextConstantReductions)
   // the 8-bit value 130 is -126 when interpreted as a signed value
   auto & x = BitConstantOperation::create(graph.GetRootRegion(), BitValueRepresentation(8, 130));
   auto & sext = jlm::llvm::SExtOperation::create(32, x);
-  auto & zext = jlm::llvm::ZExtOperation::create(32, x);
+  auto & zext = ZExtOperation::create(32, x);
   auto & sextExport = GraphExport::Create(sext, "sext");
   auto & zextExport = GraphExport::Create(zext, "zext");
 
@@ -60,4 +63,50 @@ TEST(ConversionOperationsTests, testSextZextConstantReductions)
     EXPECT_EQ(op->value().to_int(), 130u);
     EXPECT_EQ(op->value().nbits(), 32u);
   }
+}
+
+TEST(ConversionOperationsTests, SExtConstantFolding)
+{
+  using namespace jlm::rvsdg;
+
+  // Arrange
+  Graph graph;
+
+  auto & zero =
+      IntegerConstantOperation::Create(graph.GetRootRegion(), BitValueRepresentation(1, 0));
+  auto & one =
+      IntegerConstantOperation::Create(graph.GetRootRegion(), BitValueRepresentation(1, 1));
+
+  auto & node1 = SExtOperation::createNode(32, *zero.output(0));
+  auto & node2 = SExtOperation::createNode(32, *one.output(0));
+
+  auto & x1 = GraphExport::Create(*node1.output(0), "x1");
+  auto & x2 = GraphExport::Create(*node2.output(0), "x2");
+
+  view(graph, stdout);
+
+  // Act
+  ReduceNode<SExtOperation>(SExtOperation::foldConstant, node1);
+  ReduceNode<SExtOperation>(SExtOperation::foldConstant, node2);
+
+  graph.PruneNodes();
+
+  view(graph, stdout);
+
+  // Assert
+  {
+    auto [_, op] = TryGetSimpleNodeAndOptionalOp<IntegerConstantOperation>(*x1.origin());
+    EXPECT_TRUE(op);
+    EXPECT_EQ(op->Representation().to_uint(), 0u);
+    EXPECT_EQ(op->Representation().nbits(), 32u);
+  }
+
+  {
+    auto [_, op] = TryGetSimpleNodeAndOptionalOp<IntegerConstantOperation>(*x2.origin());
+    EXPECT_TRUE(op);
+    EXPECT_EQ(op->Representation().to_int(), -1);
+    EXPECT_EQ(op->Representation().nbits(), 32u);
+  }
+}
+
 }
