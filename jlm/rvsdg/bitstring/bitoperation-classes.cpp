e65a81cf462fcf304e57d6eaac68aac4786886c7
Nico Reissmann <nico.reissmann@gmail.com>
2026-07-22 13:35:28 +0200

    RVSDG: update constant folding for BitCompareOperation class

--- jlm/rvsdg/bitstring/bitoperation-classes.cpp
+++ jlm/rvsdg/bitstring/bitoperation-classes.cpp
@@ -78,52 +78,53 @@ BitCompareOperation::~BitCompareOperation() noexcept = default;
 
 binop_reduction_path_t
 BitCompareOperation::can_reduce_operand_pair(
-    const jlm::rvsdg::Output * arg1,
-    const jlm::rvsdg::Output * arg2) const noexcept
+    const Output *, const Output *) const noexcept
 {
-  auto & tracedOperand1 = traceOutputIntraProcedurally(*arg1);
-  auto [constantNode1, constantOperation1] =
-      rvsdg::TryGetSimpleNodeAndOptionalOp<BitConstantOperation>(tracedOperand1);
-  auto & tracedOperand2 = traceOutputIntraProcedurally(*arg2);
-  auto [constantNode2, constantOperation2] =
-      rvsdg::TryGetSimpleNodeAndOptionalOp<BitConstantOperation>(tracedOperand2);
-
-  BitValueRepresentation arg1_repr = constantOperation1
-                                       ? constantOperation1->value()
-                                       : BitValueRepresentation::repeat(type().nbits(), 'D');
-  BitValueRepresentation arg2_repr = constantOperation2
-                                       ? constantOperation2->value()
-                                       : BitValueRepresentation::repeat(type().nbits(), 'D');
-
-  switch (reduce_constants(arg1_repr, arg2_repr))
-  {
-  case compare_result::static_false:
-    return 1;
-  case compare_result::static_true:
-    return 2;
-  case compare_result::undecidable:
-    return binop_reduction_none;
-  }
-
   return binop_reduction_none;
 }
 
-jlm::rvsdg::Output *
+Output *
 BitCompareOperation::reduce_operand_pair(
-    binop_reduction_path_t path,
-    jlm::rvsdg::Output * arg1,
-    jlm::rvsdg::Output *) const
+    binop_reduction_path_t, Output *, Output *) const
 {
-  if (path == 1)
-  {
-    return &BitConstantOperation::create(*arg1->region(), BitValueRepresentation("0"));
-  }
-  if (path == 2)
-  {
-    return &BitConstantOperation::create(*arg1->region(), BitValueRepresentation("1"));
-  }
-
   return nullptr;
+}
+
+std::optional<std::vector<Output *>>
+BitCompareOperation::foldConstants(
+    const BitCompareOperation & operation,
+    const std::vector<Output *> & operands)
+{
+  JLM_ASSERT(operands.size() == 2);
+  auto & operand1 = *operands[0];
+  auto & operand2 = *operands[1];
+
+  const auto & tracedOperand1 = traceOutputIntraProcedurally(operand1);
+  auto [constantNode1, constantOperation1] =
+      rvsdg::TryGetSimpleNodeAndOptionalOp<BitConstantOperation>(tracedOperand1);
+
+  const auto & tracedOperand2 = traceOutputIntraProcedurally(operand2);
+  auto [constantNode2, constantOperation2] =
+      rvsdg::TryGetSimpleNodeAndOptionalOp<BitConstantOperation>(tracedOperand2);
+
+  const BitValueRepresentation representation1 =
+      constantOperation1 ? constantOperation1->value()
+                         : BitValueRepresentation::repeat(operation.type().nbits(), 'D');
+  const BitValueRepresentation representation2 =
+      constantOperation2 ? constantOperation2->value()
+                         : BitValueRepresentation::repeat(operation.type().nbits(), 'D');
+
+  switch (operation.reduce_constants(representation1, representation2))
+  {
+  case compare_result::static_false:
+    return std::vector(
+        { &BitConstantOperation::create(*operand1.region(), BitValueRepresentation("0")) });
+  case compare_result::static_true:
+    return std::vector(
+        { &BitConstantOperation::create(*operand1.region(), BitValueRepresentation("1")) });
+  default:
+    return std::nullopt;
+  }
 }
 
 }
