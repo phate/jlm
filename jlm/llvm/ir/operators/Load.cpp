5892b6bb3ff902c9c809cd32c11d899c394a5ac7
Nico Reissmann <nico.reissmann@gmail.com>
2026-08-12 06:21:30 +0200

    more

--- jlm/llvm/ir/operators/Load.cpp
+++ jlm/llvm/ir/operators/Load.cpp
@@ -356,8 +356,7 @@ getAllocationSizeInBytes(const rvsdg::Output & output)
   }
 
   // FIXME: It should also work without isConstant()
-  if (const auto llvmImport = dynamic_cast<const LlvmGraphImport *>(&output);
-      llvmImport && llvmImport->isConstant())
+  if (const auto llvmImport = dynamic_cast<const LlvmGraphImport *>(&output))
   {
     return GetTypeAllocSize(*llvmImport->ValueType());
   }
