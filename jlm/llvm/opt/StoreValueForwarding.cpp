e62062754c488e21330121dfb9fe555bed52f071
Nico Reissmann <nico.reissmann@gmail.com>
2026-08-12 11:22:57 +0200

    more

--- jlm/llvm/opt/StoreValueForwarding.cpp
+++ jlm/llvm/opt/StoreValueForwarding.cpp
@@ -1182,8 +1182,7 @@ copyDeltaElement(
         [&](const GetElementPtrOperation &)
         {
           JLM_ASSERT(elementOffsetInBytes == 0);
-          JLM_ASSERT(0 && "Here we are");
-          return static_cast<rvsdg::Output *>(nullptr);
+          return &copyDeltaRegionSlice(output, targetRegion);
         },
         [&](const ConstantAggregateZeroOperation &) -> rvsdg::Output *
         {
