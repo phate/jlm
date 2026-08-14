4ef81eca7cb6fc9d1d2aee401cab4fb1ea011ba8
Nico Reissmann <nico.reissmann@gmail.com>
2026-08-14 21:04:56 +0200

    LLVM: add support for llvm.fabs intrinsic

--- jlm/llvm/backend/IpGraphToLlvmConverter.cpp
+++ jlm/llvm/backend/IpGraphToLlvmConverter.cpp
@@ -1480,6 +1480,15 @@ IpGraphToLlvmConverter::convert_operation(
         { type },
         { multiplier, multiplicand, summand });
   }
+  if (is<FAbsOperation>(op))
+  {
+    auto operand1 = Context_->value(arguments[0]);
+    auto operand2 = Context_->value(arguments[1]);
+
+    auto type =
+        Context_->GetTypeConverter().ConvertJlmType(arguments[0]->type(), builder.getContext());
+    return builder.CreateIntrinsic(::llvm::Intrinsic::fabs, { type }, { operand1, operand2 });
+  }
 
   JLM_UNREACHABLE(util::strfmt("Unhandled operation type: ", op.debug_string()).c_str());
 }
