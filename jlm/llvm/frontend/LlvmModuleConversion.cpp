398b115e4014dcb8991d99a471e4c434baee378a
Nico Reissmann <nico.reissmann@gmail.com>
2026-08-16 20:44:29 +0200

    LLVM: add support for llvm.trunc intrinsic (#1833)

--- jlm/llvm/frontend/LlvmModuleConversion.cpp
+++ jlm/llvm/frontend/LlvmModuleConversion.cpp
@@ -1326,6 +1326,15 @@ convertCeilIntrinsic(const ::llvm::CallInst & instruction, tacsvector_t & tacs, 
 }
 
 static const Variable *
+convertTruncIntrinsic(const ::llvm::CallInst & instruction, tacsvector_t & tacs, Context & context)
+{
+  const auto operand = ConvertValue(instruction.getArgOperand(0), tacs, context);
+  tacs.push_back(TruncIntrinsicOperation::createTac(*operand));
+
+  return tacs.back()->result(0);
+}
+
+static const Variable *
 convertFloorIntrinsic(const ::llvm::CallInst & instruction, tacsvector_t & tacs, Context & context)
 {
   const auto operand = ConvertValue(instruction.getArgOperand(0), tacs, context);
@@ -1745,6 +1754,8 @@ convertIntrinsicInstruction(
     return convertSMinIntrinsic(intrinsicInstruction, threeAddressCodes, context);
   case ::llvm::Intrinsic::ssub_with_overflow:
     return convertSSubWithOverflowIntrinsic(intrinsicInstruction, threeAddressCodes, context);
+  case ::llvm::Intrinsic::trunc:
+    return convertTruncIntrinsic(intrinsicInstruction, threeAddressCodes, context);
   case ::llvm::Intrinsic::uadd_with_overflow:
     return convertUAddWithOverflowIntrinsic(intrinsicInstruction, threeAddressCodes, context);
   case ::llvm::Intrinsic::umax:
