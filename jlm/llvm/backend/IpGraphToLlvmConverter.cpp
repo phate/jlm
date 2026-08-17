f7bc8983c3214310ab4c22eabae4b52d1247b017
Nico Reissmann <nico.reissmann@gmail.com>
2026-08-17 07:42:11 +0200

    LLVM: add support for llvm.copysign intrinsic (#1835)

--- jlm/llvm/backend/IpGraphToLlvmConverter.cpp
+++ jlm/llvm/backend/IpGraphToLlvmConverter.cpp
@@ -1707,6 +1707,15 @@ IpGraphToLlvmConverter::convert_operation(
         { type },
         { operand1, operand2 });
   }
+  if (is<CopysignOperation>(op))
+  {
+    auto operand1 = Context_->value(arguments[0]);
+    auto operand2 = Context_->value(arguments[1]);
+
+    auto type =
+        Context_->GetTypeConverter().ConvertJlmType(arguments[0]->type(), builder.getContext());
+    return builder.CreateIntrinsic(::llvm::Intrinsic::copysign, { type }, { operand1, operand2 });
+  }
 
   JLM_UNREACHABLE(util::strfmt("Unhandled operation type: ", op.debug_string()).c_str());
 }
