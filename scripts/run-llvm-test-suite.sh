6ca1951f6deb7484b1b13675cca98385e1946ed6
Nico Reissmann <nico.reissmann@gmail.com>
2026-08-26 21:36:38 +0200

    Update LLVM test suite commit (#1856)

--- scripts/run-llvm-test-suite.sh
+++ scripts/run-llvm-test-suite.sh
@@ -3,7 +3,7 @@ set -eu
 
 # URL to the benchmark git repository and the commit to be used
 GIT_REPOSITORY=https://github.com/phate/llvm-test-suite.git
-GIT_COMMIT=3d72f37219f5c6c5c2f964f95823df8db5c903d7
+GIT_COMMIT=19d7452565b86f6c0cac9a1b72b763bf99f71ca4
 
 # Get the absolute path to this script and set default JLM paths
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
