ffe6832d34234a4c10c2fe2dabeadca2761198af
Magnus Sjalander <magnus.sjalander@ntnu.no>
2026-09-13 00:31:57 +0200

    MLIR: Handle context variables for delta

--- jlm/mlir/frontend/MlirToJlmConverter.cpp
+++ jlm/mlir/frontend/MlirToJlmConverter.cpp
@@ -978,6 +978,12 @@ MlirToJlmConverter::ConvertOperation(
             mlirDeltaNode.getConstant(),
             4)); // FIXME: the MLIR delta node does not support the alignment attribute
 
+    // Add context variables from inputs to delta's subregion
+    for (size_t i = 0; i < inputs.size(); ++i)
+    {
+      rvsdgDeltaNode->AddContextVar(*inputs[i]);
+    }
+
     auto outputVector = ConvertRegion(mlirDeltaNode.getRegion(), *rvsdgDeltaNode->subregion());
 
     if (outputVector.size() != 1)
